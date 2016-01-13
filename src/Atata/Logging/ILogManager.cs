6d4aa2a460211fc4e46b4d1882cae6bfde4ffca9
EvgeniyShunevich <eshunevich@gmail.com>
2016-01-13 23:34:30 +0200

    Update logging functionality; Add SimpleLogManager

--- src/Atata/Logging/ILogManager.cs
+++ src/Atata/Logging/ILogManager.cs
@@ -7,6 +7,6 @@ namespace Atata
         void Info(string message, params object[] args);
         void Warn(string message, params object[] args);
         void Error(string message, Exception excepton);
-        void Screenshot(string title = null, int waitMilliseconds = 0);
+        void Screenshot(string title = null);
     }
 }
---
+++ src/Atata/Logging/SimpleLogManager.cs
@@ -0,0 +1,49 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Text;
+
+namespace Atata
+{
+    public class SimpleLogManager : LogManagerBase
+    {
+        private Action<string> writeLineAction;
+
+        public SimpleLogManager(Action<string> writeLineAction, IWebDriver driver = null, string screenshotsFolderPath = null)
+            : base(driver, screenshotsFolderPath)
+        {
+            if (writeLineAction == null)
+                throw new ArgumentNullException("writeLineAction");
+
+            this.writeLineAction = writeLineAction;
+        }
+
+        public override void Info(string message, params object[] args)
+        {
+            Log("INFO", message, args);
+        }
+
+        public override void Warn(string message, params object[] args)
+        {
+            Log("WARN", message, args);
+        }
+
+        public override void Error(string message, Exception excepton)
+        {
+            Log("ERROR", string.Format("{0} {1}", message, excepton));
+        }
+
+        private void Log(string logLevel, string message, params object[] args)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            builder.
+                Append(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.ffff")).
+                Append(" ").
+                Append(logLevel).
+                Append(" ").
+                Append(message);
+
+            writeLineAction(builder.ToString());
+        }
+    }
+}
