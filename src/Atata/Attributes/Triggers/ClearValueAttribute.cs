4e816368b844649e2fb313153e8f15552d142c87
EvgeniyShunevich <eshunevich@gmail.com>
2016-01-28 21:17:43 +0200

    Add ClearValueAttribute trigger

---
+++ src/Atata/Attributes/Triggers/ClearValueAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace Atata
+{
+    public class ClearValueAttribute : TriggerAttribute
+    {
+        public ClearValueAttribute(TriggerEvent on = TriggerEvent.BeforeSet, TriggerPriority priority = TriggerPriority.Medium, TriggerScope appliesTo = TriggerScope.Self)
+            : base(on, priority, appliesTo)
+        {
+        }
+
+        public override void Run(TriggerContext context)
+        {
+            context.ComponentScopeFinder(false).Clear();
+        }
+    }
+}
