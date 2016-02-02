a2b91d381306f850e09342363e681e1f25ad6473
YevgeniyShunevych <eshunevich@gmail.com>
2016-02-02 11:29:39 +0200

    Add LogInfoAttribute trigger

---
+++ src/Atata/Attributes/Triggers/LogInfoAttribute.cs
@@ -0,0 +1,18 @@
+﻿namespace Atata
+{
+    public class LogInfoAttribute : TriggerAttribute
+    {
+        public LogInfoAttribute(string message, TriggerEvent on = TriggerEvent.AfterAnyAction, TriggerPriority priority = TriggerPriority.Medium, TriggerScope appliesTo = TriggerScope.Self)
+            : base(on, priority, appliesTo)
+        {
+            Message = message;
+        }
+
+        public string Message { get; private set; }
+
+        public override void Run(TriggerContext context)
+        {
+            context.Log.Info(Message);
+        }
+    }
+}
