6cfcd31f5355d8856fd47e71563b9056b65356b8
YevgeniyShunevych <eshunevich@gmail.com>
2016-02-09 14:05:13 +0200

    Add ScrollDown trigger

---
+++ src/Atata/Attributes/Triggers/ScrollDownAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace Atata
+{
+    public class ScrollDownAttribute : TriggerAttribute
+    {
+        public ScrollDownAttribute(TriggerEvents on = TriggerEvents.BeforeAnyAction, TriggerPriority priority = TriggerPriority.Medium, TriggerScope appliesTo = TriggerScope.Self)
+            : base(on, priority, appliesTo)
+        {
+        }
+
+        public override void Run(TriggerContext context)
+        {
+            context.Driver.ExecuteScript("window.scrollTo(0,document.body.scrollHeight);");
+        }
+    }
+}
