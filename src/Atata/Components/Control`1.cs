20fca0fe05b043f9c559232a4b3bbb0665c47856
YevgeniyShunevych <eshunevich@gmail.com>
2016-02-14 11:12:49 +0200

    Add VerifyEnabled and VerifyDisabled methods to Control

--- src/Atata/Components/Control`1.cs
+++ src/Atata/Components/Control`1.cs
@@ -24,5 +24,26 @@
         {
             Click();
         }
+
+        public TOwner VerifyEnabled()
+        {
+            Log.StartVerificationSection("{0} component is enabled", ComponentName);
+            Assert.That(IsEnabled(), "Expected {0} component to be enabled", ComponentName);
+            Log.EndSection();
+            return Owner;
+        }
+
+        public TOwner VerifyDisabled()
+        {
+            Log.StartVerificationSection("{0} component is disabled", ComponentName);
+            Assert.That(!IsEnabled(), "Expected {0} component to be disabled", ComponentName);
+            Log.EndSection();
+            return Owner;
+        }
+
+        public virtual bool IsEnabled()
+        {
+            return Scope.Enabled;
+        }
     }
 }
