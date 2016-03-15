1f7909117f1fd7d565b8a6bc2c51003191034802
YevgeniyShunevych <eshunevich@gmail.com>
2016-03-15 11:06:08 +0200

    Add parallelism to Tests project

--- src/Atata.Tests/Properties/AssemblyInfo.cs
+++ src/Atata.Tests/Properties/AssemblyInfo.cs
@@ -1,5 +1,10 @@
-﻿using System.Reflection;
+﻿using NUnit.Framework;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Atata.Tests")]
 [assembly: Guid("9d0aa4f2-4987-4395-be95-76abc329b7a0")]
+
+[assembly: LevelOfParallelism(4)]
+[assembly: Parallelizable(ParallelScope.Fixtures)]
+[assembly: Atata.Culture("en-us")]
