5af31fb69973f9d2512f74fa7c7dcb8fee27ab29
YevgeniyShunevych <eshunevich@gmail.com>
2016-03-16 12:41:00 +0200

    Add numeric types support to TermResolver

--- src/Atata/TermResolver.cs
+++ src/Atata/TermResolver.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Reflection;
 
@@ -8,6 +10,69 @@ namespace Atata
     {
         private const TermFormat DefaultFormat = TermFormat.Title;
         private const TermMatch DefaultMatch = TermMatch.Equals;
+
+        private static readonly Dictionary<Type, TermConverter> TypeTermConverters;
+
+        static TermResolver()
+        {
+            TypeTermConverters = new Dictionary<Type, TermConverter>();
+
+            RegisterStandardConverters();
+        }
+
+        private static void RegisterStandardConverters()
+        {
+            RegisterNumericConverter(sbyte.Parse);
+            RegisterNumericConverter(byte.Parse);
+            RegisterNumericConverter(short.Parse);
+            RegisterNumericConverter(ushort.Parse);
+            RegisterNumericConverter(int.Parse);
+            RegisterNumericConverter(uint.Parse);
+            RegisterNumericConverter(long.Parse);
+            RegisterNumericConverter(ulong.Parse);
+            RegisterNumericConverter(float.Parse);
+            RegisterNumericConverter(double.Parse);
+            RegisterNumericConverter(decimal.Parse);
+        }
+
+        private static void RegisterNumericConverter<T>(Func<string, NumberStyles, IFormatProvider, T> fromStringConverter)
+            where T : IFormattable
+        {
+            RegisterConverter(
+                typeof(T),
+                (s, to) => fromStringConverter(s, NumberStyles.Any, to.Culture),
+                (v, to) => ((T)v).ToString(to.StringFormat, to.Culture));
+        }
+
+        public static void RegisterConverter<T>(
+            Func<string, TermOptions, T> fromStringConverter,
+            Func<T, TermOptions, string> toStringConverter = null)
+        {
+            if (fromStringConverter == null)
+                throw new ArgumentNullException("fromStringConverter");
+
+            Func<string, TermOptions, object> castedFromStringConverter = (s, to) => fromStringConverter(s, to);
+            Func<object, TermOptions, string> castedToStringConverter = null;
+            if (toStringConverter != null)
+                castedToStringConverter = (v, to) => toStringConverter((T)v, to);
+
+            RegisterConverter(typeof(T), castedFromStringConverter, castedToStringConverter);
+        }
+
+        public static void RegisterConverter(
+            Type type,
+            Func<string, TermOptions, object> fromStringConverter,
+            Func<object, TermOptions, string> toStringConverter = null)
+        {
+            if (fromStringConverter == null)
+                throw new ArgumentNullException("fromStringConverter");
+
+            TypeTermConverters[type] = new TermConverter
+            {
+                FromStringConverter = fromStringConverter,
+                ToStringConverter = toStringConverter
+            };
+        }
 
         public static string ToString(object value, TermOptions termOptions = null)
         {
@@ -20,10 +85,18 @@ namespace Atata
 
         public static string[] GetTerms(object value, TermOptions termOptions = null)
         {
+            if (value == null)
+                throw new ArgumentNullException("value");
+
+            termOptions = termOptions ?? TermOptions.CreateDefault();
+            TermConverter termConverter;
+
             if (value is string)
                 return new[] { (string)value };
             else if (value is Enum)
                 return GetEnumTerms((Enum)value, termOptions);
+            else if (TypeTermConverters.TryGetValue(value.GetType(), out termConverter) && termConverter.ToStringConverter != null)
+                return new[] { termConverter.ToStringConverter(value, termOptions) };
             else
                 return new[] { value.ToString() };
         }
@@ -42,12 +115,16 @@ namespace Atata
 
         public static object FromString(string value, Type destinationType, TermOptions termOptions = null)
         {
+            termOptions = termOptions ?? TermOptions.CreateDefault();
             destinationType = Nullable.GetUnderlyingType(destinationType) ?? destinationType;
+            TermConverter termConverter;
 
             if (destinationType.IsEnum)
                 return StringToEnum(value, destinationType, termOptions);
+            else if (TypeTermConverters.TryGetValue(destinationType, out termConverter))
+                return termConverter.FromStringConverter(value, termOptions);
             else
-                return Convert.ChangeType(value, destinationType);
+                return Convert.ChangeType(value, destinationType, termOptions.Culture);
         }
 
         public static object StringToEnum(string value, Type enumType, TermOptions termOptions = null)
@@ -119,6 +196,12 @@ namespace Atata
             return termSettings != null && termSettings.Match != TermMatch.Inherit
                 ? termSettings.Match
                 : (TermMatch?)null;
+        }
+
+        private class TermConverter
+        {
+            public Func<string, TermOptions, object> FromStringConverter { get; set; }
+            public Func<object, TermOptions, string> ToStringConverter { get; set; }
         }
     }
 }
