76b357b0df60de7d42cdbbfdef9b37226ccca7ec
YevgeniyShunevych <eshunevich@gmail.com>
2016-03-28 22:50:29 +0300

    Refactor TermResolverTest

--- src/Atata.Tests/TermResolverTest.cs
+++ src/Atata.Tests/TermResolverTest.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System.Globalization;
 
 namespace Atata.Tests
 {
@@ -17,32 +18,43 @@ namespace Atata.Tests
             D
         }
 
-        [TestCase("test string")]
-        [TestCase(50)]
-        [TestCase(1234.56f)]
-        [TestCase(1234.56, "C2", "$1,234.56")]
-        [TestCase(1234.5, "asw {0:N1}_", "asw 1,234.5_")]
-        [TestCase(0.25, "P0", "25 %")]
-        [TestCase(-0.257f, "tax {0:P1}", "tax -25.7 %")]
-        [TestCase(15, "Percent: {0}%")]
-        [TestCase(Options.B)]
-        [TestCase(Options.D, null, "--d--")]
-        [TestCase(Options.A, null, ">>A:")]
-        [TestCase(Options.C, "D", "2")]
-        [TestCase(Options.C, "{0:D}.", "2.")]
-        [TestCase(Options.C, "X", "00000002")]
-        [TestCase(Options.A, "_{0:G}_", "_A_")]
-        public void TermResolver_StringFormat(object value, string format = "Before {0} after", string expectedFormattedValue = null)
+        [TestCaseSource(typeof(TermResolverStringFormatTestCaseSource))]
+        public void TermResolver_StringFormat(object value, string format, string expectedFormattedValue)
         {
             TermOptions options = new TermOptions { StringFormat = format };
             string formatted = TermResolver.ToString(value, options);
 
-            if (expectedFormattedValue == null)
-                expectedFormattedValue = string.Format(format, value);
             Assert.That(formatted, Is.EqualTo(expectedFormattedValue));
 
             object unformatted = TermResolver.FromString(formatted, value.GetType(), options);
             Assert.That(unformatted, Is.EqualTo(value));
+        }
+
+        private class TermResolverStringFormatTestCaseSource : TestCaseDataSource
+        {
+            public TermResolverStringFormatTestCaseSource()
+            {
+                Add("test string");
+                Add(50);
+                Add(1234.56f);
+                Add(1234.56, "C2", "$1,234.56");
+                Add(1234.5, "asw {0:N1}_", "asw 1,234.5_");
+                Add(0.25, "P0", "25 %");
+                Add(-0.257f, "tax {0:P1}", "tax -25.7 %");
+                Add(15, "Percent: {0}%");
+                Add(Options.B);
+                Add(Options.D, null, "--d--");
+                Add(Options.A, null, ">>A:");
+                Add(Options.C, "D", "2");
+                Add(Options.C, "{0:D}.", "2.");
+                Add(Options.C, "X", "00000002");
+                Add(Options.A, "_{0:G}_", "_A_");
+            }
+
+            private void Add(object value, string format = "<{0}>", string expectedFormattedValue = null)
+            {
+                base.Add(value, format, expectedFormattedValue ?? string.Format(new CultureInfo("en-us"), format, value));
+            }
         }
     }
 }
---
+++ src/Atata.Tests/TestCaseDataSource.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Atata.Tests
+{
+    public abstract class TestCaseDataSource : IEnumerable
+    {
+        private readonly List<TestCaseData> items = new List<TestCaseData>();
+
+        protected void Add(params object[] arguments)
+        {
+            items.Add(new TestCaseData(arguments));
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return items.GetEnumerator();
+        }
+    }
+}
