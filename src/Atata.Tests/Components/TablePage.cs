1365f0815aedaa295fcd598d2d719f3c429a4ebd
YevgeniyShunevych <eshunevich@gmail.com>
2016-04-22 17:57:12 +0300

    Add 2 table tests

---
+++ src/Atata.Tests/Components/TablePage.cs
@@ -0,0 +1,20 @@
+﻿using _ = Atata.Tests.TablePage;
+
+namespace Atata.Tests
+{
+    [NavigateTo("http://localhost:50549/Table.html")]
+    [VerifyTitle("Table")]
+    public class TablePage : Page<_>
+    {
+        public Table<_> SimpleTable { get; private set; }
+
+        public Table<UserTableRow, _> ComplexTable { get; private set; }
+
+        public class UserTableRow : TableRow<_>
+        {
+            public Text<_> FirstName { get; private set; }
+
+            public Text<_> LastName { get; private set; }
+        }
+    }
+}
---
+++ src/Atata.Tests/TableTest.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+
+namespace Atata.Tests
+{
+    [TestFixture]
+    public class TableTest : TestBase
+    {
+        private TablePage page;
+
+        protected override void OnSetUp()
+        {
+            page = Go.To<TablePage>();
+        }
+
+        [Test]
+        public void Table_Simple()
+        {
+            page.
+                SimpleTable.VerifyColumns("First Name", "Last Name").
+                SimpleTable.FirstRow().VerifyContent("John", TermMatch.Contains).
+                Do(x => x.SimpleTable.Row("Jack"), x =>
+                {
+                    x.VerifyExists();
+                    x.VerifyContent("Jameson", TermMatch.Contains);
+                });
+        }
+
+        [Test]
+        public void Table_Complex()
+        {
+            page.
+                ComplexTable.VerifyColumns("First Name", "Last Name").
+                ComplexTable.FirstRow().FirstName.VerifyEquals("John").
+                Do(x => x.ComplexTable.Row(r => r.FirstName == "Jack"), x =>
+                {
+                    x.VerifyExists();
+                    x.LastName.VerifyEquals("Jameson");
+                });
+        }
+    }
+}
