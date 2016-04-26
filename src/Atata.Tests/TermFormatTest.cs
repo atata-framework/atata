34e9d383b577e6a2898c7ca8199cd4e08d3a57a2
YevgeniyShunevych <eshunevich@gmail.com>
2016-04-26 17:09:05 +0300

    Update TermFormatExtensions; Add TermFormat tests

---
+++ src/Atata.Tests/TermFormatTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace Atata.Tests
+{
+    [TestFixture]
+    public class TermFormatTest
+    {
+        [TestCase("SimpleText", ExpectedResult = "Simple Text")]
+        [TestCase("HTMLText", ExpectedResult = "HTML Text")]
+        [TestCase("IsHTMLText?", ExpectedResult = "Is HTML Text")]
+        [TestCase("I_O-P", ExpectedResult = "I O P")]
+        [TestCase("SomeUIProp", ExpectedResult = "Some UI Prop")]
+        [TestCase("SomeIProp", ExpectedResult = "Some I Prop")]
+        [TestCase("HTML5", ExpectedResult = "HTML 5")]
+        [TestCase("T5Y", ExpectedResult = "T 5 Y")]
+        [TestCase("Ta57YuD", ExpectedResult = "Ta 57 Yu D")]
+        [TestCase("text", ExpectedResult = "Text")]
+        [TestCase("150text", ExpectedResult = "150 Text")]
+        [TestCase("*-+=#@&^a%()", ExpectedResult = "A")]
+        public string TermFormat_Title(string value)
+        {
+            return TermFormat.Title.ApplyTo(value);
+        }
+    }
+}
