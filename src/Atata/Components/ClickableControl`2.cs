f19e719cf0247f6d9393c6e22ecae6ab91ac0ce1
YevgeniyShunevych <eshunevich@gmail.com>
2016-04-29 13:02:46 +0300

    Add NavigationPageObjectCreator property to ClickableControl

--- src/Atata/Components/ClickableControl`2.cs
+++ src/Atata/Components/ClickableControl`2.cs
@@ -1,18 +1,29 @@
-﻿namespace Atata
+﻿using System;
+
+namespace Atata
 {
     [ControlDefinition("*", ComponentTypeName = "clickable control", IgnoreNameEndings = "Button,Link")]
     public class ClickableControl<TNavigateTo, TOwner> : Control<TOwner>
         where TNavigateTo : PageObject<TNavigateTo>
         where TOwner : PageObject<TOwner>
     {
+        internal Func<TNavigateTo> NavigationPageObjectCreator { get; set; }
+
         public new TNavigateTo Click()
         {
             base.Click();
 
             if (typeof(TOwner) == typeof(TNavigateTo))
+            {
                 return (TNavigateTo)(object)Owner;
+            }
             else
-                return Go.To<TNavigateTo>(navigate: false, temporarily: IsGoTemporarily());
+            {
+                return Go.To(
+                    NavigationPageObjectCreator != null ? NavigationPageObjectCreator() : null,
+                    navigate: false,
+                    temporarily: IsGoTemporarily());
+            }
         }
 
         private bool IsGoTemporarily()
