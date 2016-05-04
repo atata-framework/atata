31f6ee40d6670044a8b4ba02e518259145fb860a
YevgeniyShunevych <eshunevich@gmail.com>
2016-05-04 18:38:12 +0300

    Add UIComponentValueProvider`2 class

---
+++ src/Atata/ValueProvision/UIComponentValueProvider`2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Atata
+{
+    public class UIComponentValueProvider<TValue, TOwner> : IUIComponentValueProvider<TValue, TOwner>
+        where TOwner : PageObject<TOwner>
+    {
+        private readonly UIComponent<TOwner> component;
+        private readonly Func<UIComponent<TOwner>, TValue> valueGetFunction;
+        private readonly string providerName;
+
+        public UIComponentValueProvider(UIComponent<TOwner> component, Func<UIComponent<TOwner>, TValue> valueGetFunction, string providerName)
+        {
+            if (component == null)
+                throw new ArgumentNullException("component");
+            if (valueGetFunction == null)
+                throw new ArgumentNullException("valueGetFunction");
+            if (providerName == null)
+                throw new ArgumentNullException("providerName");
+
+            this.component = component;
+            this.valueGetFunction = valueGetFunction;
+            this.providerName = providerName;
+        }
+
+        string IUIComponentValueProvider<TValue, TOwner>.ComponentFullName
+        {
+            get { return component.ComponentFullName; }
+        }
+
+        TOwner IUIComponentValueProvider<TValue, TOwner>.Owner
+        {
+            get { return component.Owner; }
+        }
+
+        string IUIComponentValueProvider<TValue, TOwner>.ProviderName
+        {
+            get { return providerName; }
+        }
+
+        string IUIComponentValueProvider<TValue, TOwner>.ConvertValueToString(TValue value)
+        {
+            return TermResolver.ToString(value);
+        }
+
+        public TValue Get()
+        {
+            return valueGetFunction(component);
+        }
+    }
+}
