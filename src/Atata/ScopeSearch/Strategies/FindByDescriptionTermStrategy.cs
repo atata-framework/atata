5598564734df096eed86a5f8cade69ba35e4f77f
YevgeniyShunevych <eshunevich@gmail.com>
2016-06-07 17:12:08 +0300

    Add FindByDescriptionTermStrategy

---
+++ src/Atata/ScopeSearch/Strategies/FindByDescriptionTermStrategy.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace Atata
+{
+    public class FindByDescriptionTermStrategy : XPathComponentScopeLocateStrategy
+    {
+        public FindByDescriptionTermStrategy()
+            : base(useIndex: IndexUsage.None)
+        {
+        }
+
+        protected override void BuildXPath(StringBuilder builder, ComponentScopeLocateOptions options)
+        {
+            string termCondition = options.GetTermsXPathCondition();
+
+            builder.Insert(0, "dl/dt[{0}]]{1}/following-sibling::dd//".FormatWith(termCondition, options.GetPositionWrappedXPathConditionOrNull()));
+        }
+    }
+}
