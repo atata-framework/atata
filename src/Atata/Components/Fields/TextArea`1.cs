5bc542437127d3804d1c82a7b308fe98cce8891b
YevgeniyShunevych <eshunevich@gmail.com>
2016-06-14 18:26:20 +0300

    Add TextArea`1 control

---
+++ src/Atata/Components/Fields/TextArea`1.cs
@@ -0,0 +1,17 @@
+﻿namespace Atata
+{
+    [ControlDefinition("textarea", IgnoreNameEndings = "TextArea")]
+    public class TextArea<_> : EditableField<string, _>
+        where _ : PageObject<_>
+    {
+        protected override string GetValue()
+        {
+            return Scope.GetValue();
+        }
+
+        protected override void SetValue(string value)
+        {
+            Scope.FillInWith(value);
+        }
+    }
+}
