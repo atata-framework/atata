12ae986c517c579902481b31545394204c894b58
YevgeniyShunevych <eshunevich@gmail.com>
2016-06-20 13:48:27 +0300

    Update Content and Text component's type names

--- src/Atata/Components/Fields/Content/Content`2.cs
+++ src/Atata/Components/Fields/Content/Content`2.cs
@@ -5,6 +5,7 @@
     /// </summary>
     /// <typeparam name="T">The type of the content.</typeparam>
     /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
+    [ControlDefinition(ComponentTypeName = "element")]
     public class Content<T, TOwner> : Field<T, TOwner>
         where TOwner : PageObject<TOwner>
     {
--- src/Atata/Components/Fields/Content/Text`1.cs
+++ src/Atata/Components/Fields/Content/Text`1.cs
@@ -4,6 +4,7 @@
     /// Represents any element containing text content.
     /// </summary>
     /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
+    [ControlDefinition(ComponentTypeName = "text element")]
     public class Text<TOwner> : Content<string, TOwner>
         where TOwner : PageObject<TOwner>
     {
