8a21e20288eca383ef334e7c18dc646e571a9d95
YevgeniyShunevych <eshunevich@gmail.com>
2016-07-02 16:21:47 +0300

    Add KendoNumericTextBox`2 and KendoNumericTextBox`1 controls

---
+++ src/Atata.KendoUI/KendoNumericTextBox`1.cs
@@ -0,0 +1,7 @@
+﻿namespace Atata.KendoUI
+{
+    public class KendoNumericTextBox<TOwner> : KendoNumericTextBox<decimal?, TOwner>
+        where TOwner : PageObject<TOwner>
+    {
+    }
+}
---
+++ src/Atata.KendoUI/KendoNumericTextBox`2.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+
+namespace Atata.KendoUI
+{
+    // TODO: Fix IdXPathFormat. Element used by IdXPathFormat is invisilble.
+    [ControlDefinition(
+        "span",
+        ContainingClass = "k-numerictextbox",
+        ComponentTypeName = "numeric text box",
+        IdXPathFormat = "span[span/input[2][@id='{0}']]")]
+    public class KendoNumericTextBox<T, TOwner> : EditableField<T, TOwner>
+        where TOwner : PageObject<TOwner>
+    {
+        protected override T GetValue()
+        {
+            string valueAsString = Scope.Get(By.CssSelector("input.k-input[data-role='numerictextbox']").Input().OfAnyVisibility()).GetValue();
+            return ConvertStringToValue(valueAsString);
+        }
+
+        protected override void SetValue(T value)
+        {
+            Scope.Get(By.CssSelector("input.k-formatted-value.k-input").Input()).Click();
+
+            Scope.Get(By.CssSelector("input.k-input")).FillInWith(value.ToString());
+        }
+    }
+}
