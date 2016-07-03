c263b4aa0c6a8462b946b22b75eb2bdc737f64e4
YevgeniyShunevych <eshunevich@gmail.com>
2016-07-03 16:20:01 +0300

    Remove ControlDefinition attribute from Text`1 control

--- src/Atata/Components/Fields/Content/Text`1.cs
+++ src/Atata/Components/Fields/Content/Text`1.cs
@@ -4,7 +4,6 @@
     /// Represents any element containing text content.
     /// </summary>
     /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
-    [ControlDefinition(ComponentTypeName = "text element")]
     public class Text<TOwner> : Content<string, TOwner>
         where TOwner : PageObject<TOwner>
     {
