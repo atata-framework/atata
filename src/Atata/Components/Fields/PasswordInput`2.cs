2ecbf7106f585fd4e4bb0e90c34860c3c343dcea
YevgeniyShunevych <eshunevich@gmail.com>
2016-07-08 15:41:31 +0300

    Add PasswordInput`2 control

--- src/Atata/Components/Fields/PasswordInput`2.cs
+++ src/Atata/Components/Fields/PasswordInput`2.cs
@@ -1,11 +1,11 @@
 ﻿namespace Atata
 {
     /// <summary>
-    /// Represents the text input control (&lt;input type="text"&gt;).
+    /// Represents the password input control (&lt;input type="password"&gt;).
     /// </summary>
     /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
-    [ControlDefinition("input[@type='text' or not(@type)]")]
-    public class TextInput<TOwner> : Input<string, TOwner>
+    [ControlDefinition("input[@type='password']")]
+    public class PasswordInput<TOwner> : Input<string, TOwner>
         where TOwner : PageObject<TOwner>
     {
     }
