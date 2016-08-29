bf7d2fd0cdf9f8db77611331aa700146d55ba3bd
YevgeniyShunevych <eshunevich@gmail.com>
2016-08-29 18:11:08 +0300

    Add NUnitTestContextLogConsumer

---
+++ src/Atata/Logging/NUnitTestContextLogConsumer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+
+namespace Atata
+{
+    public class NUnitTestContextLogConsumer : TextOutputLogConsumer
+    {
+        private readonly MethodInfo writeMethod;
+
+        public NUnitTestContextLogConsumer()
+        {
+            Type type = Type.GetType("NUnit.Framework.TestContext,nunit.framework", true);
+            writeMethod = type.GetMethod("WriteLine", new[] { typeof(string) });
+        }
+
+        protected override void Write(string completeMessage)
+        {
+            writeMethod.Invoke(null, new[] { completeMessage });
+        }
+    }
+}
