5c8f444e3944d319cb4a3c46381e8dd147b7ebd1
YevgeniyShunevych <eshunevich@gmail.com>
2016-09-09 21:56:42 +0300

    Add ExtendedBy class

---
+++ src/Atata.WebDriverExtras/ExtendedBy.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.ObjectModel;
+using OpenQA.Selenium;
+
+namespace Atata
+{
+    internal class ExtendedBy : By
+    {
+        private readonly By by;
+
+        internal ExtendedBy(By by)
+        {
+            this.by = (by as ExtendedBy)?.by ?? by;
+
+            Description = this.by.ToString();
+        }
+
+        internal string ElementName { get; set; }
+
+        internal string ElementKind { get; set; }
+
+        internal SearchOptions Options { get; set; }
+
+        public override IWebElement FindElement(ISearchContext context)
+        {
+            return by.FindElement(context);
+        }
+
+        public override ReadOnlyCollection<IWebElement> FindElements(ISearchContext context)
+        {
+            return by.FindElements(context);
+        }
+
+        public string GetElementNameWithKind()
+        {
+            bool hasName = !string.IsNullOrWhiteSpace(ElementName);
+            bool hasKind = !string.IsNullOrWhiteSpace(ElementKind);
+
+            if (hasName && hasKind)
+                return $"\"{ElementName}\" {ElementKind}";
+            else if (hasName)
+                return ElementName;
+            else if (hasKind)
+                return ElementKind;
+            else
+                return null;
+        }
+
+        public override string ToString()
+        {
+            return by.ToString();
+        }
+    }
+}
