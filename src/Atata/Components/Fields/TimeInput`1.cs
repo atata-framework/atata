b8a3ee2e0f2fe818aafcda50bac7f6a9b1353211
YevgeniyShunevych <eshunevich@gmail.com>
2016-09-27 18:09:07 +0300

    Update TimeInput`1 XML comments

--- src/Atata/Components/Fields/TimeInput`1.cs
+++ src/Atata/Components/Fields/TimeInput`1.cs
@@ -3,7 +3,7 @@
 namespace Atata
 {
     /// <summary>
-    /// Represents the time input control.
+    /// Represents the time input control. By default is being searched by the label. Handles any input element with type="time", type="text" or without the type attribute defined.
     /// </summary>
     /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
     [ControlDefinition("input[@type='text' or @type='time' or not(@type)]")]
