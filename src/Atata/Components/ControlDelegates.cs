24a712e01453a2b984cf20e118a74536a0fd6595
YevgeniyShunevych <eshunevich@gmail.com>
2016-09-29 18:16:44 +0300

    Update XML comments

--- src/Atata/Components/ControlDelegates.cs
+++ src/Atata/Components/ControlDelegates.cs
@@ -22,6 +22,12 @@
     public delegate TOwner Button<TOwner>()
         where TOwner : PageObject<TOwner>;
 
+    /// <summary>
+    /// Represents the <see cref="ButtonControl{TNavigateTo, TOwner}"/> delegate. By default is being searched by the content and value (button by content text and input by value attribute). Handles any input element with type="button", type="submit", type="reset" or button element.
+    /// </summary>
+    /// <typeparam name="TNavigateTo">The type of the page object to navigate to.</typeparam>
+    /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
+    /// <returns>The instance of the <typeparamref name="TNavigateTo"/> page object.</returns>
     public delegate TNavigateTo Button<TNavigateTo, TOwner>()
         where TNavigateTo : PageObject<TNavigateTo>
         where TOwner : PageObject<TOwner>;
--- src/Atata/Extensions/INavigableExtensions.cs
+++ src/Atata/Extensions/INavigableExtensions.cs
@@ -4,6 +4,13 @@ namespace Atata
 {
     public static class INavigableExtensions
     {
+        /// <summary>
+        /// Clicks the control and performs the navigation to the page object of <typeparamref name="TNavigateTo"/> type.
+        /// </summary>
+        /// <typeparam name="TNavigateTo">The type of the page object to navigate to.</typeparam>
+        /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
+        /// <param name="navigableControl">The navigable control.</param>
+        /// <returns>The instance of <typeparamref name="TNavigateTo"/>.</returns>
         public static TNavigateTo ClickAndGo<TNavigateTo, TOwner>(this INavigable<TNavigateTo, TOwner> navigableControl)
             where TNavigateTo : PageObject<TNavigateTo>
             where TOwner : PageObject<TOwner>
