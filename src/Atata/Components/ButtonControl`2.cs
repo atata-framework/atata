1e08d8c543791f924106d4c83869e5a825d7023d
YevgeniyShunevych <eshunevich@gmail.com>
2016-09-29 17:50:45 +0300

    Update Button XML comments

--- src/Atata/Components/ButtonControl`2.cs
+++ src/Atata/Components/ButtonControl`2.cs
@@ -1,5 +1,12 @@
 ﻿namespace Atata
 {
+    /// <summary>
+    /// Represents the button control. By default is being searched by the content and value (button by content text and input by value attribute). Handles any input element with type="button", type="submit", type="reset" or button element.
+    /// </summary>
+    /// <typeparam name="TNavigateTo">The type of the page object to navigate to.</typeparam>
+    /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
+    /// <seealso cref="Atata.ButtonControl{TOwner}" />
+    /// <seealso cref="Atata.INavigable{TNavigateTo, TOwner}" />
     public class ButtonControl<TNavigateTo, TOwner> : ButtonControl<TOwner>, INavigable<TNavigateTo, TOwner>
         where TNavigateTo : PageObject<TNavigateTo>
         where TOwner : PageObject<TOwner>
--- src/Atata/Components/ControlDelegates.cs
+++ src/Atata/Components/ControlDelegates.cs
@@ -14,6 +14,11 @@
         where TNavigateTo : PageObject<TNavigateTo>
         where TOwner : PageObject<TOwner>;
 
+    /// <summary>
+    /// Represents the <see cref="ButtonControl{TOwner}"/> delegate. By default is being searched by the content and value (button by content text and input by value attribute). Handles any input element with type="button", type="submit", type="reset" or button element.
+    /// </summary>
+    /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
+    /// <returns>The instance of the owner page object.</returns>
     public delegate TOwner Button<TOwner>()
         where TOwner : PageObject<TOwner>;
 
