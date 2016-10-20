8e243b1dffdcc342603a569550b1b20544f5254c
YevgeniyShunevych <eshunevich@gmail.com>
2016-10-20 12:33:07 +0300

    Rename WaitUntilJQueryAjaxAttribute to WaitForJQueryAjaxAttribute

---
+++ src/Atata/Attributes/Triggers/WaitForJQueryAjaxAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Atata
+{
+    /// <summary>
+    /// Indicates that the waiting should be performed until the jQuery AJAX is completed. By default occurs after the click.
+    /// </summary>
+    public class WaitForJQueryAjaxAttribute : TriggerAttribute
+    {
+        public WaitForJQueryAjaxAttribute(TriggerEvents on = TriggerEvents.AfterClick, TriggerPriority priority = TriggerPriority.Medium)
+            : base(on, priority)
+        {
+        }
+
+        public override void Execute<TOwner>(TriggerContext<TOwner> context)
+        {
+            bool completed = context.Driver.Try().Until(
+                x => (bool)context.Driver.ExecuteScript("return jQuery.active == 0"));
+
+            if (!completed)
+                throw new Exception("Timed out waiting for the jQuery AJAX call to complete.");
+        }
+    }
+}
