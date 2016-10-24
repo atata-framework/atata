7ec65d8fbfce9bda2a334106a091ce4d6dcc7a2b
YevgeniyShunevych <eshunevich@gmail.com>
2016-10-24 16:21:41 +0300

    Add Find_ByNameAndIndex test

---
+++ src/Atata.Tests/Components/FindingPage.cs
@@ -0,0 +1,13 @@
+﻿using _ = Atata.Tests.FindingPage;
+
+namespace Atata.Tests
+{
+    [NavigateTo("Finding.html")]
+    [VerifyTitle]
+    [VerifyH1]
+    public class FindingPage : Page<_>
+    {
+        [FindByName("radio-options", Index = 2)]
+        public RadioButton<_> OptionC { get; private set; }
+    }
+}
---
+++ src/Atata.Tests/FindingTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Atata.Tests
+{
+    public class FindingTest : AutoTest
+    {
+        private FindingPage page;
+
+        protected override void OnSetUp()
+        {
+            page = Go.To<FindingPage>();
+        }
+
+        [Test]
+        public void Find_ByNameAndIndex()
+        {
+            Assert.That(page.OptionC.Attributes["value"], Is.EqualTo("OptionC"));
+        }
+    }
+}
