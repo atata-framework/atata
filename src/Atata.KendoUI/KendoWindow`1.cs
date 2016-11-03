5de7693539ecbde3b000b476efd124185a78e020
YevgeniyShunevych <eshunevich@gmail.com>
2016-11-03 11:32:53 +0200

    Update KendoWindow`1

--- src/Atata.KendoUI/KendoWindow`1.cs
+++ src/Atata.KendoUI/KendoWindow`1.cs
@@ -2,8 +2,12 @@
 {
     [PageObjectDefinition("div", ContainingClass = "k-window", ComponentTypeName = "window", IgnoreNameEndings = "PopupWindow,Window,Popup")]
     [WindowTitleElementDefinition(ContainingClass = "k-window-title")]
-    public abstract class KendoWindow<T> : PopupWindow<T>
-        where T : KendoWindow<T>
+    public abstract class KendoWindow<TOwner> : PopupWindow<TOwner>
+        where TOwner : KendoWindow<TOwner>
     {
+        protected KendoWindow(params string[] windowTitleValues)
+            : base(windowTitleValues)
+        {
+        }
     }
 }
