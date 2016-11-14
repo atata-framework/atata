57adac5ac804b12d51a5a5e67a3fad9f0d34c63f
YevgeniyShunevych <eshunevich@gmail.com>
2016-11-14 18:31:16 +0200

    Relocate control search attributes

---
+++ src/Atata/Attributes/ControlSearch/FindItemAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Atata
+{
+    [AttributeUsage(AttributeTargets.Property, Inherited = true)]
+    public abstract class FindItemAttribute : Attribute
+    {
+        public abstract IItemElementFindStrategy CreateStrategy(UIComponentMetadata metadata);
+    }
+}
