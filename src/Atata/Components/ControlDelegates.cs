22afc8af5e9a1ab4e11b352c1cf1052ef32d4afe
YevgeniyShunevych <eshunevich@gmail.com>
2016-11-14 18:34:10 +0200

    Update XML comments

--- src/Atata/Components/ControlDelegates.cs
+++ src/Atata/Components/ControlDelegates.cs
@@ -1,21 +1,21 @@
 ﻿namespace Atata
 {
     /// <summary>
-    /// Represents the <see cref="Clickable{TOwner}"/> delegate.
+    /// Represents the <see cref="ClickableControl{TOwner}"/> delegate.
     /// </summary>
     /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
     /// <returns>The instance of the owner page object.</returns>
-    /// <seealso cref="Clickable{TOwner}" />
+    /// <seealso cref="ClickableControl{TOwner}" />
     public delegate TOwner Clickable<TOwner>()
         where TOwner : PageObject<TOwner>;
 
     /// <summary>
-    /// Represents the <see cref="Clickable{TNavigateTo, TOwner}"/> delegate.
+    /// Represents the <see cref="ClickableControl{TNavigateTo, TOwner}"/> delegate.
     /// </summary>
     /// <typeparam name="TNavigateTo">The type of the page object to navigate to.</typeparam>
     /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
     /// <returns>The instance of the owner page object.</returns>
-    /// <seealso cref="Clickable{TNavigateTo, TOwner}" />
+    /// <seealso cref="ClickableControl{TNavigateTo, TOwner}" />
     public delegate TNavigateTo Clickable<TNavigateTo, TOwner>()
         where TNavigateTo : PageObject<TNavigateTo>
         where TOwner : PageObject<TOwner>;
