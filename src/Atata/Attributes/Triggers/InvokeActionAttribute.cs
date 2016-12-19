5fbc6b2b3d4c470b88d7d1293ca1ed04173e2c5a
YevgeniyShunevych <eshunevich@gmail.com>
2016-12-19 17:14:02 +0200

    Add InvokeActionAttribute trigger

---
+++ src/Atata/Attributes/Triggers/InvokeActionAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Atata
+{
+    /// <summary>
+    /// Defines the action to invoke on the specified event.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Class | AttributeTargets.Interface, AllowMultiple = true)]
+    public class InvokeActionAttribute : TriggerAttribute
+    {
+        public InvokeActionAttribute(Action action, TriggerEvents on, TriggerPriority priority = TriggerPriority.Medium)
+            : base(on, priority)
+        {
+            Action = action.CheckNotNull(nameof(action));
+        }
+
+        /// <summary>
+        /// Gets the action delegate.
+        /// </summary>
+        public Action Action { get; private set; }
+
+        protected internal override void Execute<TOwner>(TriggerContext<TOwner> context)
+        {
+            Action();
+        }
+    }
+}
