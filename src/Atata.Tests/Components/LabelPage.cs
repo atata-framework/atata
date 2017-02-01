c944e4ac30ca17c324b970207fe0b7d988020c45
YevgeniyShunevych <eshunevich@gmail.com>
2017-02-01 13:05:28 +0200

    Add Label test

---
+++ src/Atata.Tests/Components/LabelPage.cs
@@ -0,0 +1,19 @@
+﻿using _ = Atata.Tests.LabelPage;
+
+namespace Atata.Tests
+{
+    [Url("Label.html")]
+    [VerifyTitle]
+    [VerifyH1]
+    public class LabelPage : Page<_>
+    {
+        public Label<_> FirstNameLabel { get; private set; }
+
+        [Term(Format = "{0}*")]
+        [Format("{0}*")]
+        public Label<_> LastNameLabel { get; private set; }
+
+        [FindByAttribute("for", "last-name")]
+        public Label<_> LastNameByForLabel { get; private set; }
+    }
+}
