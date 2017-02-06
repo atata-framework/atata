0bc909ae2f5e5d1db82ca39e04238e001accc12c
YevgeniyShunevych <eshunevich@gmail.com>
2017-02-06 15:12:49 +0200

    Update XML comments of Label`1

--- src/Atata/Components/Fields/Content/Label`1.cs
+++ src/Atata/Components/Fields/Content/Label`1.cs
@@ -1,7 +1,7 @@
 ﻿namespace Atata
 {
     /// <summary>
-    /// Represents the &lt;label&gt; tag. Default search is performed by the content.
+    /// Represents the &lt;label&gt; element. Default search is performed by the content.
     /// </summary>
     /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
     [ControlDefinition("label", ComponentTypeName = "label", IgnoreNameEndings = "Label")]
