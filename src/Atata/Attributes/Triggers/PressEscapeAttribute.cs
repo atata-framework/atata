bfdbb410db7a71db043dea05b3bec6eb966ded29
YevgeniyShunevych <eshunevich@gmail.com>
2017-02-09 14:13:57 +0200

    Add PressEscapeAttribute trigger

---
+++ src/Atata/Attributes/Triggers/PressEscapeAttribute.cs
@@ -0,0 +1,13 @@
+﻿namespace Atata
+{
+    /// <summary>
+    /// Indicates that the Escape key should be pressed on the specified event. By default occurs after the set.
+    /// </summary>
+    public class PressEscapeAttribute : PressKeysAttribute
+    {
+        public PressEscapeAttribute(TriggerEvents on = TriggerEvents.AfterSet, TriggerPriority priority = TriggerPriority.Medium)
+            : base(OpenQA.Selenium.Keys.Escape, on, priority)
+        {
+        }
+    }
+}
