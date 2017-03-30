eabc203965b7978a9ad55719d046866dc8da2f99
YevgeniyShunevych <eshunevich@gmail.com>
2017-03-30 12:32:55 +0300

    Add Should_Contain test

--- src/Atata.Tests/ShouldTest.cs
+++ src/Atata.Tests/ShouldTest.cs
@@ -7,6 +7,7 @@ namespace Atata.Tests
         private const string Country1Name = "England";
         private const string Country2Name = "France";
         private const string Country3Name = "Germany";
+        private const string MissingCountryName = "Missing";
 
         [Test]
         public void Should_BeEquivalent()
@@ -20,7 +21,7 @@ namespace Atata.Tests
             Assert.Throws<AssertionException>(() =>
                 should.BeEquivalent(Country1Name, Country2Name));
 
-            should.Not.BeEquivalent(Country1Name, Country2Name, Country3Name, "other");
+            should.Not.BeEquivalent(Country1Name, Country2Name, Country3Name, MissingCountryName);
 
             Assert.Throws<AssertionException>(() =>
                 should.Not.BeEquivalent(Country3Name, Country1Name, Country2Name));
@@ -40,12 +41,32 @@ namespace Atata.Tests
             Assert.Throws<AssertionException>(() =>
                 should.EqualSequence(Country1Name));
 
-            should.Not.EqualSequence(Country1Name, Country2Name, Country3Name, "other");
+            should.Not.EqualSequence(Country1Name, Country2Name, Country3Name, MissingCountryName);
             should.Not.EqualSequence(Country3Name, Country1Name, Country2Name);
             should.Not.EqualSequence(Country1Name);
 
             Assert.Throws<AssertionException>(() =>
                 should.Not.EqualSequence(Country1Name, Country2Name, Country3Name));
+        }
+
+        [Test]
+        public void Should_Contain()
+        {
+            var should = Go.To<TablePage>().
+                CountryTable.Rows.SelectData(x => x.Country).Should.AtOnce;
+
+            should.Contain(Country1Name, Country2Name, Country3Name);
+            should.Contain(Country2Name, Country1Name);
+
+            Assert.Throws<AssertionException>(() =>
+                should.Contain(Country1Name, MissingCountryName));
+
+            should.Not.Contain(MissingCountryName);
+
+            Assert.Throws<AssertionException>(() =>
+                should.Not.Contain(Country1Name, MissingCountryName));
+            Assert.Throws<AssertionException>(() =>
+                should.Not.Contain(Country3Name, Country1Name, Country2Name));
         }
     }
 }
