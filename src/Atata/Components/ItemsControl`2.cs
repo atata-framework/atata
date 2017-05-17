6daa1c0a6d5dfe6851ffac6ce4db4d8c80f48043
YevgeniyShunevych <eshunevich@gmail.com>
2017-05-17 10:01:39 +0300

    Update XML comments of ItemsControl`2

--- src/Atata/Components/ItemsControl`2.cs
+++ src/Atata/Components/ItemsControl`2.cs
@@ -1,7 +1,7 @@
 ﻿namespace Atata
 {
     /// <summary>
-    /// Represents the items control (a set of any control of <typeparamref name="TItem"/> type).
+    /// Represents the items control (a control containing a set of any control of <typeparamref name="TItem"/> type). Default search finds the first occurring element.
     /// </summary>
     /// <typeparam name="TItem">The type of the item control.</typeparam>
     /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
