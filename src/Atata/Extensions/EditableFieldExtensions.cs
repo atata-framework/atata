e0303561501a12e040b7ccf63fe7f43b8309978c
YevgeniyShunevych <eshunevich@gmail.com>
2017-05-18 14:32:07 +0300

    #26 Add SetRandom extension method for EditableField`2 with nullable data type

--- src/Atata/Extensions/EditableFieldExtensions.cs
+++ src/Atata/Extensions/EditableFieldExtensions.cs
@@ -12,5 +12,28 @@
 
             return field.Owner;
         }
+
+        public static TOwner SetRandom<TOwner>(this EditableField<decimal?, TOwner> field, out int value)
+            where TOwner : PageObject<TOwner>
+        {
+            decimal? decimalValue;
+            field.SetRandom(out decimalValue);
+
+            value = (int)decimalValue;
+
+            return field.Owner;
+        }
+
+        public static TOwner SetRandom<TData, TOwner>(this EditableField<TData?, TOwner> field, out TData value)
+            where TData : struct
+            where TOwner : PageObject<TOwner>
+        {
+            TData? nullableValue;
+            field.SetRandom(out nullableValue);
+
+            value = (TData)nullableValue;
+
+            return field.Owner;
+        }
     }
 }
