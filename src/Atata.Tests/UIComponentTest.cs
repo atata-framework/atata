84167515761b5771fe97f363b675527d5e60f086
YevgeniyShunevych <eshunevich@gmail.com>
2017-05-31 09:54:29 +0300

    Add UIComponent_ComponentSize test

---
+++ src/Atata.Tests/UIComponentTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Atata.Tests
+{
+    public class UIComponentTest : AutoTest
+    {
+        [Test]
+        public void UIComponent_ComponentSize()
+        {
+            int height;
+
+            Go.To<InputPage>().
+                TextInput.ComponentSize.Width.Should.BeGreater(20).
+                TextInput.ComponentSize.Height.Should.BeInRange(10, 100).
+                TextInput.ComponentSize.Height.Get(out height).
+                TextInput.ComponentSize.Height.Should.Equal(height);
+        }
+    }
+}
