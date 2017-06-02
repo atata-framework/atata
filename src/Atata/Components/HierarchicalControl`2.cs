1b4879487aed1dc516ceecf6243e48ac1adbd94b
YevgeniyShunevych <eshunevich@gmail.com>
2017-06-02 17:53:04 +0300

    #37 Add HierarchicalControl`2 and HierarchicalItem`2 controls

---
+++ src/Atata/Components/HierarchicalControl`2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Atata
+{
+    public class HierarchicalControl<TItem, TOwner> : Control<TOwner>
+        where TItem : HierarchicalItem<TItem, TOwner>
+        where TOwner : PageObject<TOwner>
+    {
+        /// <summary>
+        /// Gets the children <see cref="ControlList{TItem, TOwner}"/> instance.
+        /// </summary>
+        [FindSettings(OuterXPath = "./")]
+        public ControlList<TItem, TOwner> Children { get; private set; }
+
+        /// <summary>
+        /// Gets the descendants (all items at any level of hierarchy) <see cref="ControlList{TItem, TOwner}"/> instance.
+        /// </summary>
+        [FindSettings(OuterXPath = ".//")]
+        public ControlList<TItem, TOwner> Descendants { get; private set; }
+
+        /// <summary>
+        /// Gets the child item at the specified index.
+        /// </summary>
+        /// <param name="index">The zero-based index of the item to get.</param>
+        /// <returns>The child item at the specified index.</returns>
+        public TItem this[int index] => Children[index];
+
+        /// <summary>
+        /// Gets the child item that matches the conditions defined by the specified predicate expression.
+        /// </summary>
+        /// <param name="predicateExpression">The predicate expression to test each item.</param>
+        /// <returns>The first child item that matches the conditions of the specified predicate.</returns>
+        public TItem this[Expression<Func<TItem, bool>> predicateExpression] => Children[predicateExpression];
+    }
+}
---
+++ src/Atata/Components/HierarchicalItem`2.cs
@@ -0,0 +1,17 @@
+﻿namespace Atata
+{
+    public class HierarchicalItem<TItem, TOwner> : HierarchicalControl<TItem, TOwner>
+        where TItem : HierarchicalItem<TItem, TOwner>
+        where TOwner : PageObject<TOwner>
+    {
+        [FindFirst(OuterXPath = "ancestor::")]
+        public new TItem Parent { get; private set; }
+
+        public DataProvider<bool, TOwner> HasParent => GetOrCreateDataProvider("has parent", OnHasParent);
+
+        protected virtual bool OnHasParent()
+        {
+            return Parent.Exists(SearchOptions.SafelyAtOnce());
+        }
+    }
+}
