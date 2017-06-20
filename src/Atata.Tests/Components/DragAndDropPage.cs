25e7382aa0dddfbe0f129e86a4b91041bd7b8bec
YevgeniyShunevych <eshunevich@gmail.com>
2017-06-20 15:58:27 +0300

    Update WebDriver.ChromeDriver.win32 to v2.30.0.0; Add Control_DragAndDrop excluded test

---
+++ src/Atata.Tests/Components/DragAndDropPage.cs
@@ -0,0 +1,19 @@
+﻿namespace Atata.Tests.Components
+{
+    using _ = DragAndDropPage;
+
+    [Url("DragAndDrop.html")]
+    public class DragAndDropPage : Page<_>
+    {
+        [FindById]
+        public ItemsControl<DragItem, _> DropContainer { get; private set; }
+
+        [FindById]
+        public ItemsControl<DragItem, _> DragItems { get; private set; }
+
+        [ControlDefinition("span", ContainingClass = "drag-item")]
+        public class DragItem : Control<_>
+        {
+        }
+    }
+}
---
+++ src/Atata.Tests/ControlTests.cs
@@ -0,0 +1,19 @@
+﻿using Atata.Tests.Components;
+
+namespace Atata.Tests
+{
+    public class ControlTests : UITestFixture
+    {
+        public void Control_DragAndDrop()
+        {
+            Go.To<DragAndDropPage>().
+                DropContainer.Items.Should.BeEmpty().
+                DragItems.Items.Should.HaveCount(2).
+                DragItems[x => x.Content == "Drag item 1"].DragAndDropTo(x => x.DropContainer).
+                DragItems[0].DragAndDropTo(x => x.DropContainer).
+                DropContainer.Items.Should.HaveCount(2).
+                DragItems.Items.Should.BeEmpty().
+                DropContainer[1].Content.Should.Equal("Drag item 2");
+        }
+    }
+}
