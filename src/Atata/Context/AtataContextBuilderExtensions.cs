ed89682aeaf930d0f10bc8a4196fe2bd3cc576b3
YevgeniyShunevych <eshunevich@gmail.com>
2017-08-30 15:28:57 +0300

    #61 Enhance FileScreenshotConsumer

--- src/Atata/Context/AtataContextBuilderExtensions.cs
+++ src/Atata/Context/AtataContextBuilderExtensions.cs
@@ -249,7 +249,7 @@ namespace Atata
         }
 
         /// <summary>
-        /// Specifies the image format of the log consumer.
+        /// Specifies the image format of the file screenshot consumer.
         /// </summary>
         /// <param name="builder">The builder.</param>
         /// <param name="imageFormat">The image format.</param>
@@ -262,19 +262,21 @@ namespace Atata
         }
 
         /// <summary>
-        /// Specifies the image format of the log consumer.
+        /// Specifies the image format of the file screenshot consumer.
         /// </summary>
+        /// <typeparam name="TConsumer">The type of the file screenshot consumer.</typeparam>
         /// <param name="builder">The builder.</param>
         /// <param name="imageFormat">The image format.</param>
         /// <returns>The <see cref="AtataContextBuilder{FileScreenshotConsumer}"/> instance.</returns>
-        public static AtataContextBuilder<FileScreenshotConsumer> With(this AtataContextBuilder<FileScreenshotConsumer> builder, ScreenshotImageFormat imageFormat)
+        public static AtataContextBuilder<TConsumer> With<TConsumer>(this AtataContextBuilder<TConsumer> builder, ScreenshotImageFormat imageFormat)
+            where TConsumer : FileScreenshotConsumerBase
         {
             builder.Context.ImageFormat = imageFormat;
             return builder;
         }
 
         /// <summary>
-        /// Specifies the folder path builder of the log consumer.
+        /// Specifies the folder path builder of the file screenshot consumer.
         /// </summary>
         /// <param name="builder">The builder.</param>
         /// <param name="folderPathBuilder">The folder path builder function.</param>
@@ -286,7 +288,19 @@ namespace Atata
         }
 
         /// <summary>
-        /// Specifies the file name builder of the log consumer.
+        /// Specifies the folder path of the file screenshot consumer.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <param name="folderPath">The folder path.</param>
+        /// <returns>The <see cref="AtataContextBuilder{FileScreenshotConsumer}"/> instance.</returns>
+        public static AtataContextBuilder<FileScreenshotConsumer> WithFolderPath(this AtataContextBuilder<FileScreenshotConsumer> builder, string folderPath)
+        {
+            builder.Context.FolderPath = folderPath;
+            return builder;
+        }
+
+        /// <summary>
+        /// Specifies the file name builder of the file screenshot consumer.
         /// </summary>
         /// <param name="builder">The builder.</param>
         /// <param name="fileNameBuilder">The file name builder function that takes an instance of <see cref="ScreenshotInfo"/>.</param>
@@ -298,7 +312,19 @@ namespace Atata
         }
 
         /// <summary>
-        /// Specifies the file path builder of the log consumer.
+        /// Specifies the file name of the file screenshot consumer.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <param name="fileName">The file name.</param>
+        /// <returns>The <see cref="AtataContextBuilder{FileScreenshotConsumer}"/> instance.</returns>
+        public static AtataContextBuilder<FileScreenshotConsumer> WithFileName(this AtataContextBuilder<FileScreenshotConsumer> builder, string fileName)
+        {
+            builder.Context.FileName = fileName;
+            return builder;
+        }
+
+        /// <summary>
+        /// Specifies the file path builder of the file screenshot consumer.
         /// </summary>
         /// <param name="builder">The builder.</param>
         /// <param name="filePathBuilder">The file path builder function that takes an instance of <see cref="ScreenshotInfo"/>.</param>
@@ -306,6 +332,18 @@ namespace Atata
         public static AtataContextBuilder<FileScreenshotConsumer> WithFilePath(this AtataContextBuilder<FileScreenshotConsumer> builder, Func<ScreenshotInfo, string> filePathBuilder)
         {
             builder.Context.FilePathBuilder = filePathBuilder;
+            return builder;
+        }
+
+        /// <summary>
+        /// Specifies the file path of the file screenshot consumer.
+        /// </summary>
+        /// <param name="builder">The builder.</param>
+        /// <param name="filePath">The file path.</param>
+        /// <returns>The <see cref="AtataContextBuilder{FileScreenshotConsumer}"/> instance.</returns>
+        public static AtataContextBuilder<FileScreenshotConsumer> WithFilePath(this AtataContextBuilder<FileScreenshotConsumer> builder, string filePath)
+        {
+            builder.Context.FilePath = filePath;
             return builder;
         }
 
--- src/Atata/Logging/FileScreenshotConsumer.cs
+++ src/Atata/Logging/FileScreenshotConsumer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Atata
 {
@@ -9,11 +11,46 @@ namespace Atata
     /// <seealso cref="Atata.IScreenshotConsumer" />
     public class FileScreenshotConsumer : FileScreenshotConsumerBase
     {
+        private static readonly Dictionary<string, Func<ScreenshotInfo, object>> PathVariableFactoryMap;
+
+        static FileScreenshotConsumer()
+        {
+            PathVariableFactoryMap = new Dictionary<string, Func<ScreenshotInfo, object>>
+            {
+                ["build-start"] = scr => AtataContext.BuildStart,
+
+                ["test-name"] = scr => AtataContext.Current.TestName,
+                ["test-start"] = scr => AtataContext.Current.TestStart,
+
+                ["screenshot-number"] = scr => scr.Number,
+                ["screenshot-title"] = scr => scr.Title,
+                ["screenshot-pageobjectname"] = scr => scr.PageObjectName,
+                ["screenshot-pageobjecttypename"] = scr => scr.PageObjectTypeName,
+                ["screenshot-pageobjectfullname"] = scr => scr.PageObjectFullName
+            };
+        }
+
         public Func<string> FolderPathBuilder { get; set; }
 
         public Func<ScreenshotInfo, string> FileNameBuilder { get; set; }
 
         public Func<ScreenshotInfo, string> FilePathBuilder { get; set; }
+
+        public string FolderPath { get; set; }
+
+        public string FileName { get; set; }
+
+        public string FilePath { get; set; }
+
+        public static string FormatPath(string format, ScreenshotInfo screenshotInfo)
+        {
+            for (int i = 0; i < PathVariableFactoryMap.Count; i++)
+            {
+                format = format.Replace("{" + PathVariableFactoryMap.Keys.ElementAt(i), $"{{{i}}}");
+            }
+
+            return format.FormatWith(PathVariableFactoryMap.Values.Select(factory => factory(screenshotInfo)).ToArray());
+        }
 
         /// <summary>
         /// Builds the path of the file without the extension.
@@ -23,15 +60,21 @@ namespace Atata
         protected override string BuildFilePath(ScreenshotInfo screenshotInfo)
         {
             if (FilePathBuilder != null)
-                return FilePathBuilder(screenshotInfo);
+                return FilePathBuilder(screenshotInfo).SanitizeForPath();
+            else if (!string.IsNullOrWhiteSpace(FilePath))
+                return FormatPath(FilePath, screenshotInfo).SanitizeForPath();
 
             string folderPath = FolderPathBuilder?.Invoke()
-                ?? $@"Logs\{AtataContext.BuildStart:yyyy-MM-dd HH_mm_ss}\{AtataContext.Current.TestName}";
+                ?? (!string.IsNullOrWhiteSpace(FolderPath)
+                    ? FormatPath(FolderPath, screenshotInfo)
+                    : $@"Logs\{AtataContext.BuildStart:yyyy-MM-dd HH_mm_ss}\{AtataContext.Current.TestName}");
 
             folderPath = folderPath.SanitizeForPath();
 
             string fileName = FileNameBuilder?.Invoke(screenshotInfo)
-                ?? $"{screenshotInfo.Number:D2} - {screenshotInfo.PageObjectFullName}{screenshotInfo.Title?.Prepend(" - ")}";
+                ?? (!string.IsNullOrWhiteSpace(FileName)
+                    ? FormatPath(FileName, screenshotInfo)
+                    : $"{screenshotInfo.Number:D2} - {screenshotInfo.PageObjectFullName}{screenshotInfo.Title?.Prepend(" - ")}");
 
             fileName = fileName.SanitizeForFileName();
 
