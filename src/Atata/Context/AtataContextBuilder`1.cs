6d7c1262d54beade5452973512966b17e3a6d3f1
YevgeniyShunevych <eshunevich@gmail.com>
2017-09-04 10:30:09 +0300

    Add XML comments to AtataContextBuilder`1

--- src/Atata/Context/AtataContextBuilder`1.cs
+++ src/Atata/Context/AtataContextBuilder`1.cs
@@ -10,8 +10,16 @@ namespace Atata
             Context = context;
         }
 
+        /// <summary>
+        /// Gets the context.
+        /// </summary>
         public TContext Context { get; private set; }
 
+        /// <summary>
+        /// Specifies the properties map for the context.
+        /// </summary>
+        /// <param name="propertiesMap">The properties map.</param>
+        /// <returns>The same builder instance.</returns>
         public AtataContextBuilder<TContext> WithProperties(Dictionary<string, object> propertiesMap)
         {
             propertiesMap.CheckNotNull(nameof(propertiesMap));
