f3ec82ae7d364f545dd5ddd2046f377bd054b699
YevgeniyShunevych <eshunevich@gmail.com>
2017-09-18 12:43:00 +0300

    Add Configuration_Mixed, Configuration_MultiDriver and Configuration_Clear tests

---
+++ src/Atata.Tests/ConfigurationTests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Atata.Tests
+{
+    [TestFixture]
+    public class ConfigurationTests
+    {
+        [Test]
+        public void Configuration_Mixed()
+        {
+            var globalContext = AtataContext.GlobalConfiguration.BuildingContext;
+
+            Assert.That(globalContext.TestNameFactory, Is.Null);
+            Assert.That(globalContext.DriverFactories, Is.Empty);
+            Assert.That(globalContext.LogConsumers, Is.Empty);
+            Assert.That(globalContext.ScreenshotConsumers, Is.Empty);
+            Assert.That(globalContext.BaseUrl, Is.Null);
+            Assert.That(globalContext.CleanUpActions, Is.Empty);
+
+            AtataContext.GlobalConfiguration.
+                UseNUnitTestName().
+                AddNUnitTestContextLogging().
+                LogNUnitError();
+
+            var currentContext = AtataContext.Configure().
+                UsePhantomJS().
+                UseBaseUrl("http://localhost:50549").
+                TakeScreenshotOnNUnitError().
+                AddScreenshotFileSaving().
+                UseRetryTimeout(TimeSpan.FromSeconds(100)).
+                UseRetryInterval(TimeSpan.FromSeconds(1)).
+                BuildingContext;
+
+            Assert.That(globalContext.TestNameFactory(), Is.EqualTo(nameof(Configuration_Mixed)));
+            Assert.That(globalContext.DriverFactories, Is.Empty);
+            Assert.That(globalContext.LogConsumers, Has.Count.EqualTo(1));
+            Assert.That(globalContext.LogConsumers.First().Consumer, Is.TypeOf<NUnitTestContextLogConsumer>());
+            Assert.That(globalContext.ScreenshotConsumers, Is.Empty);
+            Assert.That(globalContext.BaseUrl, Is.Null);
+            Assert.That(globalContext.CleanUpActions, Has.Count.EqualTo(1));
+
+            Assert.That(currentContext.DriverFactories, Has.Count.EqualTo(1));
+            Assert.That(currentContext.DriverFactoryToUse.Alias, Is.EqualTo(DriverAliases.PhantomJS));
+            Assert.That(currentContext.LogConsumers, Has.Count.EqualTo(1));
+            Assert.That(currentContext.ScreenshotConsumers, Has.Count.EqualTo(1));
+            Assert.That(currentContext.ScreenshotConsumers.First(), Is.TypeOf<FileScreenshotConsumer>());
+            Assert.That(currentContext.BaseUrl, Is.EqualTo("http://localhost:50549"));
+            Assert.That(currentContext.CleanUpActions, Has.Count.EqualTo(2));
+            Assert.That(currentContext.RetryTimeout, Is.EqualTo(TimeSpan.FromSeconds(100)));
+            Assert.That(currentContext.RetryInterval, Is.EqualTo(TimeSpan.FromSeconds(1)));
+        }
+
+        [Test]
+        public void Configuration_MultiDriver()
+        {
+            var contextBuilder = AtataContext.Configure().
+                UsePhantomJS().
+                UseChrome();
+
+            var context = contextBuilder.BuildingContext;
+
+            Assert.That(context.DriverFactories, Has.Count.EqualTo(2));
+            Assert.That(context.DriverFactoryToUse.Alias, Is.EqualTo(DriverAliases.Chrome));
+
+            contextBuilder.
+                UseFirefox();
+
+            Assert.That(context.DriverFactories, Has.Count.EqualTo(3));
+            Assert.That(context.DriverFactoryToUse.Alias, Is.EqualTo(DriverAliases.Firefox));
+
+            contextBuilder.
+                UseDriver(DriverAliases.PhantomJS);
+
+            Assert.That(context.DriverFactories, Has.Count.EqualTo(3));
+            Assert.That(context.DriverFactoryToUse.Alias, Is.EqualTo(DriverAliases.PhantomJS));
+
+            contextBuilder.
+                UseDriver(DriverAliases.InternetExplorer);
+
+            Assert.That(context.DriverFactories, Has.Count.EqualTo(4));
+            Assert.That(context.DriverFactoryToUse.Alias, Is.EqualTo(DriverAliases.InternetExplorer));
+        }
+
+        [Test]
+        public void Configuration_Clear()
+        {
+            var context = AtataContext.Configure().
+                UsePhantomJS().
+                UseBaseUrl("http://localhost:50549").
+                TakeScreenshotOnNUnitError().
+                AddScreenshotFileSaving().
+                UseRetryTimeout(TimeSpan.FromSeconds(100)).
+                UseRetryInterval(TimeSpan.FromSeconds(1)).
+                Clear().
+                BuildingContext;
+
+            Assert.That(context.TestNameFactory, Is.Null);
+            Assert.That(context.DriverFactories, Is.Empty);
+            Assert.That(context.DriverFactoryToUse, Is.Null);
+            Assert.That(context.LogConsumers, Is.Empty);
+            Assert.That(context.ScreenshotConsumers, Is.Empty);
+            Assert.That(context.BaseUrl, Is.Null);
+            Assert.That(context.CleanUpActions, Is.Empty);
+            Assert.That(context.RetryTimeout, Is.EqualTo(TimeSpan.FromSeconds(10)));
+            Assert.That(context.RetryInterval, Is.EqualTo(TimeSpan.FromSeconds(0.5)));
+        }
+    }
+}
