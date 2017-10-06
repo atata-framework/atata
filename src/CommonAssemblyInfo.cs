e1a04df744bb319b4241ed5b9ac4ad4614861d22
YevgeniyShunevych <eshunevich@gmail.com>
2017-10-06 12:57:05 +0300

    Update Atata.sln and CommonAssemblyInfo.cs

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Yevgeniy Shunevych 2017")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.14.0")]
-[assembly: AssemblyFileVersion("0.14.0")]
+[assembly: AssemblyVersion("0.15.0")]
+[assembly: AssemblyFileVersion("0.15.0")]
