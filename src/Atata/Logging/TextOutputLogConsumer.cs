0f048f257907653bba0770b8f57d6f6e4df3fed7
YevgeniyShunevych <eshunevich@gmail.com>
2017-12-05 16:14:44 +0200

    #120 Update TextOutputLogConsumer

--- src/Atata/Logging/TextOutputLogConsumer.cs
+++ src/Atata/Logging/TextOutputLogConsumer.cs
@@ -1,12 +1,36 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 using System.Text;
 
 namespace Atata
 {
-    public abstract class TextOutputLogConsumer : ILogConsumer
+    /// <summary>
+    /// Represents text output log consumer.
+    /// Is used for regular text logging.
+    /// Action to write text to some source can be passed via constructor.
+    /// Also it is possible to define custom class inherited from <see cref="TextOutputLogConsumer"/> and override <see cref="Write(string)"/> method.
+    /// </summary>
+    public class TextOutputLogConsumer : ILogConsumer
     {
+        private readonly Action<string> writeAction;
+
+        public TextOutputLogConsumer()
+        {
+        }
+
+        public TextOutputLogConsumer(Action<string> writeAction)
+        {
+            this.writeAction = writeAction;
+        }
+
+        /// <summary>
+        /// Gets or sets the text parts separator. The default value is " ".
+        /// </summary>
         public string Separator { get; set; } = " ";
 
+        /// <summary>
+        /// Gets or sets the timestamp format. The default value is "yyyy-MM-dd HH:mm:ss.ffff".
+        /// </summary>
         public string TimestampFormat { get; set; } = "yyyy-MM-dd HH:mm:ss.ffff";
 
         public void Log(LogEventInfo eventInfo)
@@ -15,7 +39,10 @@ namespace Atata
             Write(completeMessage);
         }
 
-        protected abstract void Write(string completeMessage);
+        protected virtual void Write(string completeMessage)
+        {
+            writeAction?.Invoke(completeMessage);
+        }
 
         private string BuildCompleteMessage(LogEventInfo eventInfo)
         {
