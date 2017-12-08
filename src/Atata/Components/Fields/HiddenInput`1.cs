42aeda6c4b838cba228edac659ae975d65235036
YevgeniyShunevych <eshunevich@gmail.com>
2017-12-08 12:36:48 +0200

    #123 Change HiddenInput<T, TOwner> control finding; Add XML comments to HiddenInput<TOwner>

--- src/Atata/Components/Fields/HiddenInput`1.cs
+++ src/Atata/Components/Fields/HiddenInput`1.cs
@@ -1,5 +1,10 @@
 ﻿namespace Atata
 {
+    /// <summary>
+    /// Represents the hidden input control (&lt;input type="hidden"&gt;) with text data.
+    /// Default search finds the first occurring &lt;input type="hidden"&gt; element.
+    /// </summary>
+    /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
     public class HiddenInput<TOwner> : HiddenInput<string, TOwner>
         where TOwner : PageObject<TOwner>
     {
--- src/Atata/Components/Fields/HiddenInput`2.cs
+++ src/Atata/Components/Fields/HiddenInput`2.cs
@@ -1,11 +1,13 @@
 ﻿namespace Atata
 {
     /// <summary>
-    /// Represents the hidden input control (&lt;input type="hidden"&gt;). Default search finds the first occurring &lt;input type="hidden"&gt; element.
+    /// Represents the hidden input control (&lt;input type="hidden"&gt;).
+    /// Default search finds the first occurring &lt;input type="hidden"&gt; element.
     /// </summary>
     /// <typeparam name="T">The type of the control's data.</typeparam>
     /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
     [ControlDefinition("input[@type='hidden']", Visibility = Visibility.Hidden)]
+    [ControlFinding(typeof(FindFirstAttribute))]
     public class HiddenInput<T, TOwner> : Input<T, TOwner>
         where TOwner : PageObject<TOwner>
     {
