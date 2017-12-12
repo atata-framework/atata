237c98318d1f89e3b1d3a8af9e422883edb27889
YevgeniyShunevych <eshunevich@gmail.com>
2017-12-12 10:48:56 +0200

    Add XML comments to IPropertySettings

--- src/Atata/IPropertySettings.cs
+++ src/Atata/IPropertySettings.cs
@@ -1,7 +1,13 @@
 ﻿namespace Atata
 {
+    /// <summary>
+    /// Defines functionality that provides the properties bag.
+    /// </summary>
     public interface IPropertySettings
     {
+        /// <summary>
+        /// Gets the properties bag.
+        /// </summary>
         PropertyBag Properties { get; }
     }
 }
