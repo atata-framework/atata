3fd332bd9cc1691adb797f4b7bca62e4d74a51a6
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2018-02-26 17:42:32 +0200

    #146 Add more specific exceptions to AtataMapper

---
+++ src/Atata/Mapping/AtataMapper.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Atata
+{
+    public static class AtataMapper
+    {
+        public static void Map(Dictionary<string, object> propertiesMap, object destination)
+        {
+            Type destinationType = destination.GetType();
+
+            foreach (var item in propertiesMap)
+            {
+                Map(item.Key, item.Value, destination, destinationType);
+            }
+        }
+
+        public static void Map(string propertyName, object propertyValue, object destination)
+        {
+            Map(propertyName, propertyValue, destination, destination.GetType());
+        }
+
+        private static void Map(string propertyName, object propertyValue, object destination, Type destinationType)
+        {
+            PropertyInfo property = destinationType.GetProperty(
+                propertyName,
+                BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.IgnoreCase);
+
+            if (property == null)
+                throw new MappingException($"Failed to map \"{propertyName}\" property for {destinationType.FullName} type. Property is not found.");
+
+            Type propertyValueType = propertyValue?.GetType();
+            Type propertyType = property.PropertyType;
+            Type underlyingPropertyType = Nullable.GetUnderlyingType(propertyType) ?? propertyType;
+
+            try
+            {
+                object valueToSet = propertyValue != null && !(propertyType.IsAssignableFrom(propertyValueType) || underlyingPropertyType.IsAssignableFrom(propertyValueType))
+                    ? ConvertValue(propertyValue, underlyingPropertyType)
+                    : propertyValue;
+
+                property.SetValue(destination, valueToSet, null);
+            }
+            catch (Exception exception)
+            {
+                string additionalMessage = propertyValue == null
+                    ? $"Property null value cannot be converted to {propertyType} type."
+                    : $"Property \"{propertyValue}\" value of {propertyValueType} type cannot be converted to {propertyType} type.";
+
+                throw new MappingException($"Failed to map \"{propertyName}\" property for {destinationType.FullName} type. {additionalMessage}", exception);
+            }
+        }
+
+        private static object ConvertValue(object sourceValue, Type destinationType)
+        {
+            if (destinationType.IsEnum)
+                return ConvertToEnum(destinationType, sourceValue);
+            else if (destinationType == typeof(TimeSpan))
+                return ConvertToTimeSpan(sourceValue);
+            else
+                return sourceValue;
+        }
+
+        private static object ConvertToEnum(Type enumType, object value)
+        {
+            if (value is string stringValue)
+                return Enum.Parse(enumType, stringValue, true);
+            else
+                return Enum.ToObject(enumType, value);
+        }
+
+        private static TimeSpan ConvertToTimeSpan(object value)
+        {
+            if (value is double || value is int || value is float)
+                return TimeSpan.FromSeconds(Convert.ToDouble(value));
+            else
+                return TimeSpan.Parse(value.ToString());
+        }
+    }
+}
---
+++ src/Atata/Mapping/MappingException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Atata
+{
+    [Serializable]
+    public class MappingException : Exception
+    {
+        public MappingException()
+        {
+        }
+
+        public MappingException(string message)
+            : base(message)
+        {
+        }
+
+        public MappingException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected MappingException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
