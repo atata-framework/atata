b004f7751c818d085e94a939c87b77303c91ca67
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2018-03-01 19:36:05 +0200

    #149 Add FrameSetPage<TOwner> base page object class

---
+++ src/Atata/Components/FrameSetPage`1.cs
@@ -0,0 +1,14 @@
+﻿namespace Atata
+{
+    /// <summary>
+    /// Represents the frameset-based HTML page.
+    /// Uses the root &lt;frameset&gt; tag as a scope.
+    /// </summary>
+    /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
+    /// <seealso cref="PageObject{T}" />
+    [PageObjectDefinition("frameset", ComponentTypeName = "page", IgnoreNameEndings = "Page,PageObject")]
+    public abstract class FrameSetPage<TOwner> : PageObject<TOwner>
+        where TOwner : FrameSetPage<TOwner>
+    {
+    }
+}
