7a31791074ac61e7768c0d14708f13101773102f
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2018-03-01 19:36:43 +0200

    Add FrameSetPage test

---
+++ src/Atata.Tests/Components/FrameSetPage.cs
@@ -0,0 +1,18 @@
+﻿namespace Atata.Tests
+{
+    using _ = FrameSetPage;
+
+    [Url("FrameSet.html")]
+    [VerifyTitle(TermCase.Pascal)]
+    public class FrameSetPage : FrameSetPage<_>
+    {
+        [FindByIndex(0)]
+        public Frame<FrameInner1Page, _> Frame1 { get; private set; }
+
+        [FindByIndex(1)]
+        public Frame<FrameInner2Page, _> Frame2 { get; private set; }
+
+        [FindByIndex(2)]
+        public Frame<FrameInner1Page, _> Frame3 { get; private set; }
+    }
+}
---
+++ src/Atata.Tests/FrameSetTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace Atata.Tests
+{
+    public class FrameSetTests : UITestFixture
+    {
+        private FrameSetPage page;
+
+        protected override void OnSetUp()
+        {
+            page = Go.To<FrameSetPage>();
+        }
+
+        [Test]
+        public void FrameSetPage()
+        {
+            page.
+                Frame1.SwitchTo().
+                    Header.Should.Equal("Frame Inner 1").
+                    TextInput.Should.Exist().
+                    SwitchToRoot<FrameSetPage>().
+                Frame2.DoWithin(x => x.
+                    Header.Should.Equal("Frame Inner 2").
+                    Select.Should.Equal(1)).
+                Frame3.SwitchTo().
+                    Header.Should.Equal("Frame Inner 1").
+                    TextInput.Set("123").
+                    TextInput.Should.Equal("123");
+        }
+    }
+}
