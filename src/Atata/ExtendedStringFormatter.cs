84802dc6785c8e53585709cf6a4163cf0e147fc5
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2018-03-06 09:53:43 +0200

    #150 FileScreenshotConsumer doesn't handle path variables; #151 Set default format for path variables of DateTime type for FileScreenshotConsumer; #152 Add support of inner format of string path variables for FileScreenshotConsumer; Add XML comments to FileScreenshotConsumer

---
+++ src/Atata/ExtendedStringFormatter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace Atata
+{
+    public class ExtendedStringFormatter : IFormatProvider, ICustomFormatter
+    {
+        private const string InnerFormatValueIndicator = "*";
+
+        public static ExtendedStringFormatter Default { get; } = new ExtendedStringFormatter();
+
+        public object GetFormat(Type formatType)
+        {
+            if (formatType == typeof(ICustomFormatter))
+                return this;
+            else
+                return null;
+        }
+
+        public string Format(string format, object arg, IFormatProvider formatProvider)
+        {
+            if (!Equals(formatProvider))
+                return null;
+
+            if (arg == null)
+                return string.Empty;
+
+            if (arg is string argString && !string.IsNullOrWhiteSpace(format))
+                return FormatInnerString(format, argString);
+            else if (arg is IFormattable argFormattable)
+                return argFormattable.ToString(format, CultureInfo.CurrentCulture);
+            else
+                return arg.ToString();
+        }
+
+        private static string FormatInnerString(string format, string argument)
+        {
+            string normalizedFormat = format.
+                Replace(InnerFormatValueIndicator, "{0}").
+                Replace("{0}{0}", InnerFormatValueIndicator);
+
+            return string.Format(normalizedFormat, argument);
+        }
+    }
+}
--- src/Atata/Logging/FileScreenshotConsumer.cs
+++ src/Atata/Logging/FileScreenshotConsumer.cs
@@ -7,10 +7,21 @@ namespace Atata
 {
     /// <summary>
     /// Represents the screenshot consumer that saves the screenshot to the file.
+    /// By default uses <c>"Logs\{build-start}\{test-name}"</c> as folder path format,
+    /// <c>"{screenshot-number:D2} - {screenshot-pageobjectfullname}{screenshot-title: - *}"</c> as file name format
+    /// and <c>Png</c> as image format.
+    /// Example of screenshot file path using default settings: <c>"Logs\2018-03-03 14_34_04\SampleTest\01 - Home page - Screenshot title.png"</c>.
+    /// Available path variables: <c>{build-start}</c>, <c>{test-name}</c>, <c>{test-start}</c>, <c>{driver-alias}</c>, <c>{screenshot-number}</c>, <c>{screenshot-title}</c>, <c>{screenshot-pageobjectname}</c>, <c>{screenshot-pageobjecttypename}</c>, <c>{screenshot-pageobjectfullname}</c>.
+    /// Path variables support the formatting.
     /// </summary>
-    /// <seealso cref="Atata.IScreenshotConsumer" />
+    /// <seealso cref="IScreenshotConsumer" />
     public class FileScreenshotConsumer : FileScreenshotConsumerBase
     {
+        /// <summary>
+        /// The default DateTime format for "build-start" and "test-start" path variables is "yyyy-MM-dd HH_mm_ss".
+        /// </summary>
+        public const string DefaultDateTimeFormat = "yyyy-MM-dd HH_mm_ss";
+
         private static readonly Dictionary<string, Func<ScreenshotInfo, object>> PathVariableFactoryMap;
 
         static FileScreenshotConsumer()
@@ -32,26 +43,54 @@ namespace Atata
             };
         }
 
+        /// <summary>
+        /// Gets or sets the builder of the folder path.
+        /// </summary>
         public Func<string> FolderPathBuilder { get; set; }
 
+        /// <summary>
+        /// Gets or sets the builder of the file name.
+        /// </summary>
         public Func<ScreenshotInfo, string> FileNameBuilder { get; set; }
 
+        /// <summary>
+        /// Gets or sets the builder of the file path.
+        /// </summary>
         public Func<ScreenshotInfo, string> FilePathBuilder { get; set; }
 
+        /// <summary>
+        /// Gets or sets the folder path.
+        /// </summary>
         public string FolderPath { get; set; }
 
+        /// <summary>
+        /// Gets or sets the file name.
+        /// </summary>
         public string FileName { get; set; }
 
+        /// <summary>
+        /// Gets or sets the file path.
+        /// </summary>
         public string FilePath { get; set; }
 
+        /// <summary>
+        /// Formats the screenshot file path using path variables.
+        /// </summary>
+        /// <param name="format">The format.</param>
+        /// <param name="screenshotInfo">The screenshot information.</param>
+        /// <returns>The formatted file path format.</returns>
         public static string FormatPath(string format, ScreenshotInfo screenshotInfo)
         {
+            format = format.
+                Replace("{build-start}", $"{{build-start:{DefaultDateTimeFormat}}}").
+                Replace("{test-start}", $"{{test-start:{DefaultDateTimeFormat}}}");
+
             for (int i = 0; i < PathVariableFactoryMap.Count; i++)
             {
-                format = format.Replace("{" + PathVariableFactoryMap.Keys.ElementAt(i), $"{{{i}}}");
+                format = format.Replace("{" + PathVariableFactoryMap.Keys.ElementAt(i), $"{{{i}");
             }
 
-            return format.FormatWith(PathVariableFactoryMap.Values.Select(factory => factory(screenshotInfo)).ToArray());
+            return string.Format(ExtendedStringFormatter.Default, format, PathVariableFactoryMap.Values.Select(factory => factory(screenshotInfo)).ToArray());
         }
 
         /// <summary>
@@ -69,7 +108,7 @@ namespace Atata
             string folderPath = FolderPathBuilder?.Invoke()
                 ?? (!string.IsNullOrWhiteSpace(FolderPath)
                     ? FormatPath(FolderPath, screenshotInfo)
-                    : $@"Logs\{AtataContext.BuildStart:yyyy-MM-dd HH_mm_ss}\{AtataContext.Current.TestName}");
+                    : $@"Logs\{AtataContext.BuildStart.Value.ToString(DefaultDateTimeFormat)}\{AtataContext.Current.TestName}");
 
             folderPath = folderPath.SanitizeForPath();
 
