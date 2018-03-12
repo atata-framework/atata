d8982f0f9b3f841ef822dd0a6dfd4189c55e466d
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2018-03-12 12:13:33 +0200

    Add tests: DefaultTimeout_BaseRetryTimeout_ElementFind, DefaultTimeout_BaseRetryTimeout_Waiting and DefaultTimeout_BaseRetryTimeout_Verification; Update DefaultTimeout_WaitingTimeout test

--- src/Atata.Tests/DefaultTimeoutTests.cs
+++ src/Atata.Tests/DefaultTimeoutTests.cs
@@ -1,14 +1,58 @@
 ﻿using System;
 using NUnit.Framework;
+using OpenQA.Selenium;
 
 namespace Atata.Tests
 {
     public class DefaultTimeoutTests : UITestFixtureBase
     {
         [Test]
-        public void DefaultTimeout_Waiting()
+        public void DefaultTimeout_BaseRetryTimeout_ElementFind()
         {
             ConfigureBaseAtataContext().
+                UseBaseRetryTimeout(TimeSpan.FromSeconds(2)).
+                Build();
+
+            var page = Go.To<WaitingPage>();
+
+            using (StopwatchAsserter.Within(2))
+                page.MissingControl.GetScope();
+        }
+
+        [Test]
+        public void DefaultTimeout_BaseRetryTimeout_Waiting()
+        {
+            ConfigureBaseAtataContext().
+                UseBaseRetryTimeout(TimeSpan.FromSeconds(2)).
+                Build();
+
+            var page = Go.To<WaitingPage>();
+
+            using (StopwatchAsserter.Within(2))
+                page.MissingControl.Wait(Until.Visible, new WaitOptions { ThrowOnPresenceFailure = false });
+        }
+
+        [Test]
+        public void DefaultTimeout_BaseRetryTimeout_Verification()
+        {
+            ConfigureBaseAtataContext().
+                UseBaseRetryTimeout(TimeSpan.FromSeconds(2)).
+                Build();
+
+            var page = Go.To<WaitingPage>();
+
+            Assert.Throws<NoSuchElementException>(() =>
+            {
+                using (StopwatchAsserter.Within(2))
+                    page.MissingControl.Should.BeEnabled();
+            });
+        }
+
+        [Test]
+        public void DefaultTimeout_WaitingTimeout()
+        {
+            ConfigureBaseAtataContext().
+                UseBaseRetryTimeout(TimeSpan.FromSeconds(1)).
                 UseWaitingTimeout(TimeSpan.FromSeconds(3)).
                 Build();
 
