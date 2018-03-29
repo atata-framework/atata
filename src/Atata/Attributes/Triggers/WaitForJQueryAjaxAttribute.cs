f9c8cda663f76a41f49779396823bb955e3324d6
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2018-03-29 11:02:59 +0300

    #163 Inherit WaitForJQueryAjaxAttribute from WaitForScriptAttribute

--- src/Atata/Attributes/Triggers/WaitForJQueryAjaxAttribute.cs
+++ src/Atata/Attributes/Triggers/WaitForJQueryAjaxAttribute.cs
@@ -1,24 +1,20 @@
-﻿using System;
-
-namespace Atata
+﻿namespace Atata
 {
     /// <summary>
-    /// Indicates that the waiting should be performed until the jQuery AJAX is completed. By default occurs after the click.
+    /// Indicates that the waiting should be performed until the jQuery AJAX is completed.
+    /// By default occurs after the click.
     /// </summary>
-    public class WaitForJQueryAjaxAttribute : TriggerAttribute
+    public class WaitForJQueryAjaxAttribute : WaitForScriptAttribute
     {
         public WaitForJQueryAjaxAttribute(TriggerEvents on = TriggerEvents.AfterClick, TriggerPriority priority = TriggerPriority.Medium)
             : base(on, priority)
         {
         }
 
-        protected internal override void Execute<TOwner>(TriggerContext<TOwner> context)
-        {
-            bool completed = context.Driver.Try().Until(
-                x => (bool)context.Driver.ExecuteScript("return jQuery.active == 0"));
+        protected override string BuildReportMessage<TOwner>(TriggerContext<TOwner> context)
+            => "Wait for jQuery AJAX execution";
 
-            if (!completed)
-                throw new TimeoutException("Timed out waiting for the jQuery AJAX call to complete.");
-        }
+        protected override string BuildScript<TOwner>(TriggerContext<TOwner> context)
+            => "return jQuery.active == 0";
     }
 }
