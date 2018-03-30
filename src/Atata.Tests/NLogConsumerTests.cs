9d09f75d76f56daf5358e075c11b2d3cc9b079f6
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2018-03-30 14:59:04 +0300

    Add NLogConsumer test

---
+++ src/Atata.Tests/NLogConsumerTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace Atata.Tests
+{
+    public class NLogConsumerTests : UITestFixtureBase
+    {
+        [Test]
+        public void NLogConsumer()
+        {
+            ConfigureBaseAtataContext().
+                AddNLogLogging().
+                Build();
+
+            string testMessage = Guid.NewGuid().ToString();
+
+            AtataContext.Current.Log.Info(testMessage);
+
+            string filePath = Path.Combine(
+                AppDomain.CurrentDomain.BaseDirectory,
+                "Logs",
+                AtataContext.BuildStart.Value.ToString(FileScreenshotConsumer.DefaultDateTimeFormat),
+                AtataContext.Current.TestName,
+                $"{AtataContext.Current.TestName}.log");
+
+            FileAssert.Exists(filePath);
+
+            string fileContent = File.ReadAllText(filePath);
+
+            Assert.That(fileContent, Does.Contain(testMessage));
+        }
+    }
+}
