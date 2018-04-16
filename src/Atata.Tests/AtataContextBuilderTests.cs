57019b872ad395b02dce6c37b65bce5c0308c584
Yevgeniy Shunevych <yevgeniy.shunevych@gmail.com>
2018-04-16 12:50:29 +0300

    Add AtataContextBuilder_OnBuilding and AtataContextBuilder_OnBuilding_WithoutDriver tests

--- src/Atata.Tests/AtataContextBuilderTests.cs
+++ src/Atata.Tests/AtataContextBuilderTests.cs
@@ -15,16 +15,40 @@ namespace Atata.Tests
         }
 
         [Test]
+        public void AtataContextBuilder_OnBuilding()
+        {
+            int executionsCount = 0;
+
+            AtataContext.Configure().
+                UseChrome().
+                OnBuilding(() => executionsCount++).
+                Build();
+
+            Assert.That(executionsCount, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void AtataContextBuilder_OnBuilding_WithoutDriver()
+        {
+            int executionsCount = 0;
+
+            Assert.Throws<InvalidOperationException>(() =>
+                AtataContext.Configure().
+                    UseDriver(() => null).
+                    OnBuilding(() => executionsCount++).
+                    Build());
+
+            Assert.That(executionsCount, Is.EqualTo(1));
+        }
+
+        [Test]
         public void AtataContextBuilder_OnDriverCreated()
         {
             int executionsCount = 0;
 
             AtataContext.Configure().
                 UseChrome().
-                OnDriverCreated(driver =>
-                {
-                    executionsCount++;
-                }).
+                OnDriverCreated(driver => executionsCount++).
                 Build();
 
             Assert.That(executionsCount, Is.EqualTo(1));
@@ -37,10 +61,7 @@ namespace Atata.Tests
 
             AtataContext.Configure().
                 UseChrome().
-                OnDriverCreated(() =>
-                {
-                    executionsCount++;
-                }).
+                OnDriverCreated(() => executionsCount++).
                 Build();
 
             AtataContext.Current.RestartDriver();
