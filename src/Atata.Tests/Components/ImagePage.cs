1629f54a72ac2eb586072e4ed3d261800bc10cfe
Yevgeniy Shunevych <yevgeniy.shunevych@gmail.com>
2018-08-17 17:13:24 +0300

    #185 Add Image<TOwner> control

---
+++ src/Atata.Tests/Components/ImagePage.cs
@@ -0,0 +1,15 @@
+﻿namespace Atata.Tests
+{
+    using _ = ImagePage;
+
+    [Url("Image.html")]
+    [VerifyTitle]
+    public class ImagePage : Page<_>
+    {
+        [FindById]
+        public Image<_> LoadedImage { get; private set; }
+
+        [FindById]
+        public Image<_> NotLoadedImage { get; private set; }
+    }
+}
