5f068732f1b97c8620ab3b0112b6954826602d6c
Yaroslav Samus <yaroslav.samus@gmail.com>
2018-09-18 22:58:51 +0300

    Add RightClickParentAttribute trigger

---
+++ src/Atata/Attributes/Triggers/RightClickParentAttribute.cs
@@ -0,0 +1,18 @@
+﻿namespace Atata
+{
+    /// <summary>
+    /// Indicates that the right click on the parent component should occur on the specified event. By default occurs before any access to the component. Is useful for the context menu item controls.
+    /// </summary>
+    public class RightClickParentAttribute : TriggerAttribute
+    {
+        public RightClickParentAttribute(TriggerEvents on = TriggerEvents.BeforeAccess, TriggerPriority priority = TriggerPriority.Medium)
+            : base(on, priority)
+        {
+        }
+
+        protected internal override void Execute<TOwner>(TriggerContext<TOwner> context)
+        {
+            ((Control<TOwner>)context.Component.Parent).RightClick();
+        }
+    }
+}
