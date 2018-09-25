6762d9db2b02a14da81748499199925c1579fbf2
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2018-09-25 16:59:57 +0300

    #195 Add AttributeSettingsAttribute

---
+++ src/Atata/Attributes/AttributeSettingsAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Atata
+{
+    /// <summary>
+    /// Represents the base attribute settings class for other attributes.
+    /// </summary>
+    public abstract class AttributeSettingsAttribute : MulticastAttribute
+    {
+        /// <summary>
+        /// Gets or sets the target attribute types.
+        /// </summary>
+        public Type[] TargetAttributeTypes { get; set; }
+
+        /// <summary>
+        /// Gets or sets the target attribute type.
+        /// </summary>
+        public Type TargetAttributeType
+        {
+            get { return TargetAttributeTypes?.SingleOrDefault(); }
+            set { TargetAttributeTypes = new[] { value }; }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this instance has any target specified.
+        /// </summary>
+        public override bool IsTargetSpecified =>
+            base.IsTargetSpecified ||
+            (TargetAttributeTypes?.Any() ?? false);
+    }
+}
