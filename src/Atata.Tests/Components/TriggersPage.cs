2f86a4790a6861b7e50c0cb30506f1e438f3fcec
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2018-09-26 14:16:37 +0300

    Add [assembly: InternalsVisibleTo("Atata.Tests")] attribute to Atata project

--- src/Atata.Tests/Components/TriggersPage.cs
+++ src/Atata.Tests/Components/TriggersPage.cs
@@ -114,7 +114,7 @@ namespace Atata.Tests
 
             public string Message { get; private set; }
 
-            protected override void Execute<TOwner>(TriggerContext<TOwner> context)
+            protected internal override void Execute<TOwner>(TriggerContext<TOwner> context)
             {
                 context.Log.Info(Message);
             }
