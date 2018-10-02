6717cec2fc68f6bb37060d98e6d4f9d8ef6b8760
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2018-10-02 11:41:55 +0300

    #200 Make some members of UIComponent and UIComponent<TOwner> public; Add and update XML comments of UIComponent and UIComponent<TOwner>

--- src/Atata/Components/UIComponent.cs
+++ src/Atata/Components/UIComponent.cs
@@ -18,36 +18,57 @@ namespace Atata
             Log = AtataContext.Current.Log;
         }
 
-        protected internal UIComponent Owner { get; internal set; }
+        /// <summary>
+        /// Gets the owner component.
+        /// </summary>
+        public UIComponent Owner { get; internal set; }
 
         /// <summary>
         /// Gets the parent component.
         /// </summary>
-        protected internal UIComponent Parent { get; internal set; }
+        public UIComponent Parent { get; internal set; }
 
         protected internal ILogManager Log { get; internal set; }
 
         protected internal RemoteWebDriver Driver { get; internal set; }
 
-        protected internal ScopeSource ScopeSource { get; internal set; }
+        /// <summary>
+        /// Gets the source of the scope.
+        /// </summary>
+        public ScopeSource ScopeSource { get; internal set; }
 
         protected internal IScopeLocator ScopeLocator { get; internal set; }
 
         protected internal bool CacheScopeElement { get; set; }
 
-        protected internal string ComponentName { get; internal set; }
+        /// <summary>
+        /// Gets the name of the component.
+        /// </summary>
+        public string ComponentName { get; internal set; }
 
-        protected internal string ComponentTypeName { get; internal set; }
+        /// <summary>
+        /// Gets the name of the component type.
+        /// Returns the value of <see cref="UIComponentDefinitionAttribute.ComponentTypeName"/> property for control from <see cref="ControlDefinitionAttribute"/>
+        /// and for page object from <see cref="PageObjectDefinitionAttribute"/>.
+        /// </summary>
+        public string ComponentTypeName { get; internal set; }
 
-        protected internal string ComponentFullName
+        /// <summary>
+        /// Gets the full name of the component including parent component full name, own component name and own component type name.
+        /// </summary>
+        public string ComponentFullName
         {
             get { return BuildComponentFullName(); }
         }
 
-        protected internal UIComponentMetadata Metadata { get; internal set; }
+        /// <summary>
+        /// Gets the metadata of the component.
+        /// </summary>
+        public UIComponentMetadata Metadata { get; internal set; }
 
         /// <summary>
-        /// Gets the <see cref="IWebElement"/> instance that represents the scope HTML element. Also executes <see cref="TriggerEvents.BeforeAccess" /> and <see cref="TriggerEvents.AfterAccess" /> triggers.
+        /// Gets the <see cref="IWebElement"/> instance that represents the scope HTML element.
+        /// Also executes <see cref="TriggerEvents.BeforeAccess" /> and <see cref="TriggerEvents.AfterAccess" /> triggers.
         /// </summary>
         public IWebElement Scope
         {
@@ -61,9 +82,11 @@ namespace Atata
         }
 
         /// <summary>
-        /// Gets the <see cref="IWebElement"/> instance that represents the scope HTML element. Also executes <see cref="TriggerEvents.BeforeAccess" /> and <see cref="TriggerEvents.AfterAccess" /> triggers.
+        /// Gets the <see cref="IWebElement"/> instance that represents the scope HTML element.
+        /// Also executes <see cref="TriggerEvents.BeforeAccess" /> and <see cref="TriggerEvents.AfterAccess" /> triggers.
         /// </summary>
-        /// <param name="options">The options. If set to null, then it uses <c>SearchOptions.Safely()</c>.</param>
+        /// <param name="options">The search options.
+        /// If set to <c>null</c>, then it uses <c>SearchOptions.Safely()</c>.</param>
         /// <returns>The <see cref="IWebElement"/> instance of the scope.</returns>
         public IWebElement GetScope(SearchOptions options = null)
         {
@@ -108,6 +131,10 @@ namespace Atata
         {
         }
 
+        /// <summary>
+        /// Builds the full name of the component including parent component full name, own component name and own component type name.
+        /// </summary>
+        /// <returns>The full name of the component.</returns>
         protected virtual string BuildComponentFullName()
         {
             StringBuilder builder = new StringBuilder();
@@ -128,9 +155,10 @@ namespace Atata
         /// <summary>
         /// Determines whether the component exists.
         /// </summary>
-        /// <param name="options">The options. If set to null, then it uses <c>SearchOptions.Safely()</c>.</param>
-        /// <returns>true if the component exists; otherwise, false.</returns>
-        /// <exception cref="NoSuchElementException">The <paramref name="options"/> has IsSafely property equal to false value and the component doesn't exist.</exception>
+        /// <param name="options">The search options.
+        /// If set to <c>null</c>, then it uses <c>SearchOptions.Safely()</c>.</param>
+        /// <returns><c>true</c> if the component exists; otherwise, <c>false</c>.</returns>
+        /// <exception cref="NoSuchElementException">The <paramref name="options"/> has <see cref="SearchOptions.IsSafely"/> property equal to <c>false</c> value and the component doesn't exist.</exception>
         public bool Exists(SearchOptions options = null)
         {
             return GetScopeElement(options ?? SearchOptions.Safely()) != null;
@@ -139,9 +167,10 @@ namespace Atata
         /// <summary>
         /// Determines whether the component is missing.
         /// </summary>
-        /// <param name="options">The options. If set to null, then it uses <c>SearchOptions.Safely()</c>.</param>
-        /// <returns>true if the component is missing; otherwise, false.</returns>
-        /// <exception cref="NotMissingElementException">The <paramref name="options"/> has IsSafely property equal to false value and the component exists.</exception>
+        /// <param name="options">The search options.
+        /// If set to <c>null</c>, then it uses <c>SearchOptions.Safely()</c>.</param>
+        /// <returns><c>true</c> if the component is missing; otherwise, <c>false</c>.</returns>
+        /// <exception cref="NotMissingElementException">The <paramref name="options"/> has <see cref="SearchOptions.IsSafely"/> property equal to <c>false</c> value and the component exists.</exception>
         public bool Missing(SearchOptions options = null)
         {
             return OnMissing(options ?? SearchOptions.Safely());
@@ -152,6 +181,12 @@ namespace Atata
             return ScopeLocator.IsMissing(options);
         }
 
+        /// <summary>
+        /// Returns a <see cref="string"/> that represents this instance including <see cref="ComponentFullName"/> and <see cref="Scope"/> element details.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="string"/> that represents this instance.
+        /// </returns>
         public override string ToString()
         {
             StringBuilder builder = new StringBuilder(ComponentFullName);
@@ -163,6 +198,9 @@ namespace Atata
             return builder.ToString();
         }
 
+        /// <summary>
+        /// Cleans up the current instance.
+        /// </summary>
         protected internal abstract void CleanUp();
     }
 }
