ee746ba5e685fc08c42dac698dd201917f51a8de
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2018-10-02 11:38:57 +0300

    Add and update XML comments of IUIComponent<TOwner>

--- src/Atata/Components/IUIComponent`1.cs
+++ src/Atata/Components/IUIComponent`1.cs
@@ -3,11 +3,21 @@ using OpenQA.Selenium;
 
 namespace Atata
 {
+    /// <summary>
+    /// Represents an interface for UI component.
+    /// </summary>
+    /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
     public interface IUIComponent<TOwner>
         where TOwner : PageObject<TOwner>
     {
+        /// <summary>
+        /// Gets the owner page object.
+        /// </summary>
         TOwner Owner { get; }
 
+        /// <summary>
+        /// Gets the parent component.
+        /// </summary>
         IUIComponent<TOwner> Parent { get; }
 
         /// <summary>
@@ -15,18 +25,43 @@ namespace Atata
         /// </summary>
         DataProvider<bool, TOwner> IsVisible { get; }
 
+        /// <summary>
+        /// Gets the <see cref="DataProvider{TData, TOwner}"/> instance for the text content.
+        /// Gets content using <see cref="ContentSourceAttribute"/> or, by default, uses <see cref="IWebElement.Text"/> property of component scope <see cref="IWebElement"/> element.
+        /// </summary>
         DataProvider<string, TOwner> Content { get; }
 
+        /// <summary>
+        /// Gets the source of the scope.
+        /// </summary>
         ScopeSource ScopeSource { get; }
 
+        /// <summary>
+        /// Gets the <see cref="IWebElement"/> instance that represents the scope HTML element.
+        /// Also executes <see cref="TriggerEvents.BeforeAccess" /> and <see cref="TriggerEvents.AfterAccess" /> triggers.
+        /// </summary>
         IWebElement Scope { get; }
 
+        /// <summary>
+        /// Gets the scope locator.
+        /// </summary>
         IScopeLocator ScopeLocator { get; }
 
+        /// <summary>
+        /// Gets the name of the component.
+        /// </summary>
         string ComponentName { get; }
 
+        /// <summary>
+        /// Gets the name of the component type.
+        /// Returns the value of <see cref="UIComponentDefinitionAttribute.ComponentTypeName"/> property for control from <see cref="ControlDefinitionAttribute"/>
+        /// and for page object from <see cref="PageObjectDefinitionAttribute"/>.
+        /// </summary>
         string ComponentTypeName { get; }
 
+        /// <summary>
+        /// Gets the full name of the component including parent component full name, own component name and own component type name.
+        /// </summary>
         string ComponentFullName { get; }
 
         /// <summary>
@@ -39,25 +74,43 @@ namespace Atata
         /// </summary>
         UIComponentSizeProvider<TOwner> ComponentSize { get; }
 
+        /// <summary>
+        /// Gets the <see cref="UIComponentAttributeProvider{TOwner}"/> instance that provides an access to the scope element's attributes.
+        /// </summary>
         UIComponentAttributeProvider<TOwner> Attributes { get; }
 
+        /// <summary>
+        /// Gets the <see cref="UIComponentCssProvider{TOwner}"/> instance that provides an access to the scope element's CSS properties.
+        /// </summary>
         UIComponentCssProvider<TOwner> Css { get; }
 
+        /// <summary>
+        /// Gets the list of child controls.
+        /// </summary>
         UIComponentChildrenList<TOwner> Controls { get; }
 
+        /// <summary>
+        /// Gets the metadata of the component.
+        /// </summary>
         UIComponentMetadata Metadata { get; }
 
         /// <summary>
-        /// Gets the set of triggers. Provides the functionality to get/add/remove triggers dynamically.
+        /// Gets the set of triggers.
+        /// Provides the functionality to get/add/remove triggers dynamically.
         /// </summary>
         UIComponentTriggerSet<TOwner> Triggers { get; }
 
+        /// <summary>
+        /// Gets the verification provider that provides a set of verification extension methods.
+        /// </summary>
         UIComponentVerificationProvider<UIComponent<TOwner>, TOwner> Should { get; }
 
         /// <summary>
-        /// Gets the <see cref="IWebElement"/> instance that represents the scope HTML element. Also executes <see cref="TriggerEvents.BeforeAccess" /> and <see cref="TriggerEvents.AfterAccess" /> triggers.
+        /// Gets the <see cref="IWebElement"/> instance that represents the scope HTML element.
+        /// Also executes <see cref="TriggerEvents.BeforeAccess" /> and <see cref="TriggerEvents.AfterAccess" /> triggers.
         /// </summary>
-        /// <param name="options">The options. If set to null, then it uses <c>SearchOptions.Safely()</c>.</param>
+        /// <param name="options">The search options.
+        /// If set to <c>null</c>, then it uses <c>SearchOptions.Safely()</c>.</param>
         /// <returns>The <see cref="IWebElement"/> instance of the scope.</returns>
         IWebElement GetScope(SearchOptions options = null);
 
@@ -69,15 +122,46 @@ namespace Atata
         /// <returns>The instance of the owner page object.</returns>
         TOwner Wait(Until until, WaitOptions options = null);
 
+        /// <summary>
+        /// Determines whether the component exists.
+        /// </summary>
+        /// <param name="options">The search options.
+        /// If set to <c>null</c>, then it uses <c>SearchOptions.Safely()</c>.</param>
+        /// <returns><c>true</c> if the component exists; otherwise, <c>false</c>.</returns>
+        /// <exception cref="NoSuchElementException">The <paramref name="options"/> has <see cref="SearchOptions.IsSafely"/> property equal to <c>false</c> value and the component doesn't exist.</exception>
         bool Exists(SearchOptions options = null);
 
+        /// <summary>
+        /// Determines whether the component is missing.
+        /// </summary>
+        /// <param name="options">The search options.
+        /// If set to <c>null</c>, then it uses <c>SearchOptions.Safely()</c>.</param>
+        /// <returns><c>true</c> if the component is missing; otherwise, <c>false</c>.</returns>
+        /// <exception cref="NotMissingElementException">The <paramref name="options"/> has <see cref="SearchOptions.IsSafely"/> property equal to <c>false</c> value and the component exists.</exception>
         bool Missing(SearchOptions options = null);
 
+        /// <summary>
+        /// Gets the data provider by name or creates and stores a new instance with the specified <paramref name="providerName"/> and using <paramref name="valueGetFunction"/>.
+        /// </summary>
+        /// <typeparam name="TValue">The type of the value.</typeparam>
+        /// <param name="providerName">Name of the provider.</param>
+        /// <param name="valueGetFunction">The function that gets a value.</param>
+        /// <returns>A new instance of <see cref="DataProvider{TData, TOwner}"/> type or already stored one.</returns>
         DataProvider<TValue, TOwner> GetOrCreateDataProvider<TValue>(string providerName, Func<TValue> valueGetFunction);
 
+        /// <summary>
+        /// Gets the ancestor component of specified type.
+        /// </summary>
+        /// <typeparam name="TComponentToFind">The type of the component to find.</typeparam>
+        /// <returns>The component or <c>null</c> if not found.</returns>
         TComponentToFind GetAncestor<TComponentToFind>()
             where TComponentToFind : UIComponent<TOwner>;
 
+        /// <summary>
+        /// Gets the ancestor component of specified type or self.
+        /// </summary>
+        /// <typeparam name="TComponentToFind">The type of the component to find.</typeparam>
+        /// <returns>The component or <c>null</c> if not found.</returns>
         TComponentToFind GetAncestorOrSelf<TComponentToFind>()
             where TComponentToFind : UIComponent<TOwner>;
     }
