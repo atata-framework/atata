c7033dd178d34a75446f19fe0cc85b0f697e67c0
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2018-10-10 12:44:37 +0300

    Add XML comments to UrlAttribute

--- src/Atata/Attributes/UrlAttribute.cs
+++ src/Atata/Attributes/UrlAttribute.cs
@@ -2,6 +2,10 @@
 
 namespace Atata
 {
+    /// <summary>
+    /// Specifies the URL to navigate to during initialization of page object.
+    /// Applies to page object types.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class)]
     public class UrlAttribute : Attribute
     {
@@ -10,6 +14,9 @@ namespace Atata
             Url = url;
         }
 
+        /// <summary>
+        /// Gets the URL to navigate to.
+        /// </summary>
         public string Url { get; private set; }
     }
 }
