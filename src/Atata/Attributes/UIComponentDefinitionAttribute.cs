50158b74fe4c5e87d49fc8cd009173e96259569a
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2018-10-10 12:42:18 +0300

    Add XML comments to UIComponentDefinitionAttribute

--- src/Atata/Attributes/UIComponentDefinitionAttribute.cs
+++ src/Atata/Attributes/UIComponentDefinitionAttribute.cs
@@ -2,6 +2,9 @@
 
 namespace Atata
 {
+    /// <summary>
+    /// Represents the base attribute class for UI component (page object, control) definition.
+    /// </summary>
     public abstract class UIComponentDefinitionAttribute : ScopeDefinitionAttribute
     {
         protected UIComponentDefinitionAttribute(string scopeXPath = DefaultScopeXPath)
@@ -9,15 +12,32 @@ namespace Atata
         {
         }
 
+        /// <summary>
+        /// Gets or sets the name of the component type.
+        /// It is used in report log messages to describe the component type.
+        /// </summary>
         public string ComponentTypeName { get; set; }
 
+        /// <summary>
+        /// Gets or sets the property name endings to ignore/truncate.
+        /// Accepts a string containing a set of values separated by comma, for example <c>"Button,Link"</c>.
+        /// </summary>
         public string IgnoreNameEndings { get; set; }
 
+        /// <summary>
+        /// Gets the values of property name endings to ignore.
+        /// </summary>
+        /// <returns>An array of name endings to ignore.</returns>
         public string[] GetIgnoreNameEndingValues()
         {
             return IgnoreNameEndings != null ? IgnoreNameEndings.Split(',').Where(x => !string.IsNullOrWhiteSpace(x)).ToArray() : new string[0];
         }
 
+        /// <summary>
+        /// Normalizes the name considering value of <see cref="IgnoreNameEndings"/>.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <returns>Normalized name.</returns>
         public string NormalizeNameIgnoringEnding(string name)
         {
             string endingToIgnore = GetIgnoreNameEndingValues().
