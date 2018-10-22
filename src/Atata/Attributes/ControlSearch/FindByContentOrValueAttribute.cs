6eb6dc1e72b8f1a71076972609fcaa8ef8b0b085
Yaroslav Samus <yaroslav.samus@dfsco.com>
2018-10-22 14:00:52 +0300

    Update XML comments of FindByContentOrValueAttribute

--- src/Atata/Attributes/ControlSearch/FindByContentOrValueAttribute.cs
+++ src/Atata/Attributes/ControlSearch/FindByContentOrValueAttribute.cs
@@ -5,7 +5,7 @@ namespace Atata
     /// <summary>
     /// Specifies that a control should be found by the content text or value attribute.
     /// Finds the control that has the content or value attribute matching the specified term(s).
-    /// Uses <c>Title</c> as the default term case.
+    /// Uses <see cref="TermCase.Title"/> as the default term case.
     /// </summary>
     public class FindByContentOrValueAttribute : TermFindAttribute
     {
