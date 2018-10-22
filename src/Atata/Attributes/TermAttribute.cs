34a59849b5c55a56225309b2d1bf20ed2e565e05
Yaroslav Samus <yaroslav.samus@dfsco.com>
2018-10-22 14:37:14 +0300

    Update XML comments of TermAttribute

--- src/Atata/Attributes/TermAttribute.cs
+++ src/Atata/Attributes/TermAttribute.cs
@@ -42,7 +42,8 @@ namespace Atata
         }
 
         /// <summary>
-        /// Gets or sets a value indicating whether the name should be cut considering the IgnoreNameEndings property value of <see cref="ControlDefinitionAttribute"/> and <see cref="PageObjectDefinitionAttribute"/>. The default value is true.
+        /// Gets or sets a value indicating whether the name should be cut considering the IgnoreNameEndings property value of <see cref="ControlDefinitionAttribute"/> and <see cref="PageObjectDefinitionAttribute"/>.
+        /// The default value is <see langword="true"/>.
         /// </summary>
         public bool CutEnding
         {
