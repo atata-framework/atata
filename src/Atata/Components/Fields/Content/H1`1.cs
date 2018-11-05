b80832816589ca7a12e53785114ff57c129ab74f
Yaroslav Samus <yaroslav.samus@gmail.com>
2018-11-06 00:08:50 +0200

    Update XML comments of H1 component

--- src/Atata/Components/Fields/Content/H1`1.cs
+++ src/Atata/Components/Fields/Content/H1`1.cs
@@ -1,8 +1,8 @@
 ﻿namespace Atata
 {
     /// <summary>
-    /// Represents the &lt;h1&gt; heading tag.
-    /// Default search finds the first occurring &lt;h1&gt; element.
+    /// Represents the <c>h1</c>; heading tag.
+    /// Default search finds the first occurring <c>h1</c>; element.
     /// </summary>
     /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
     [ControlDefinition("h1", ComponentTypeName = "<h1> heading", IgnoreNameEndings = "Header,Heading")]
