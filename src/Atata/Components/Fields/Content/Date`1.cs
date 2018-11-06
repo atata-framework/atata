980985323318f28bc674f702d3b55083ebd3bbb6
Yaroslav Samus <yaroslav.samus@dfsco.com>
2018-11-06 11:45:06 +0200

    Update XML comments of Date component

--- src/Atata/Components/Fields/Content/Date`1.cs
+++ src/Atata/Components/Fields/Content/Date`1.cs
@@ -5,7 +5,7 @@ namespace Atata
     /// <summary>
     /// Represents any element containing date content.
     /// Default search finds the first occurring element.
-    /// The default format is <c>d</c> (short date pattern, e.g. <c>6/15/2009</c>).
+    /// The default format is <c>"d"</c> (short date pattern, e.g. <c>6/15/2009</c>).
     /// </summary>
     /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
     [Format("d")]
