4729db084211a1630f944d271034a30afb1aa25a
Yaroslav Samus <yaroslav.samus@dfsco.com>
2018-11-06 11:55:52 +0200

    Update XML comments of H5 component

--- src/Atata/Components/Fields/Content/H5`1.cs
+++ src/Atata/Components/Fields/Content/H5`1.cs
@@ -1,8 +1,8 @@
 ﻿namespace Atata
 {
     /// <summary>
-    /// Represents the <c>h5</c> heading tag.
-    /// Default search finds the first occurring <c>h5</c> element.
+    /// Represents the <c>&lt;h5&gt;</c> heading tag.
+    /// Default search finds the first occurring <c>&lt;h5&gt;</c> element.
     /// </summary>
     /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
     [ControlDefinition("h5", ComponentTypeName = "<h5> heading", IgnoreNameEndings = "Header,Heading")]
