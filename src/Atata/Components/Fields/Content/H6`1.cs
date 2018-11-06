6cdcfc2383d7f216b8649ae4f477cfa712ca6112
Yaroslav Samus <yaroslav.samus@dfsco.com>
2018-11-06 11:56:32 +0200

    Update XML comments of H6 component

--- src/Atata/Components/Fields/Content/H6`1.cs
+++ src/Atata/Components/Fields/Content/H6`1.cs
@@ -1,8 +1,8 @@
 ﻿namespace Atata
 {
     /// <summary>
-    /// Represents the <c>h6</c> heading tag.
-    /// Default search finds the first occurring <c>h6</c> element.
+    /// Represents the <c>&lt;h6&gt;</c> heading tag.
+    /// Default search finds the first occurring <c>&lt;h6&gt;</c> element.
     /// </summary>
     /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
     [ControlDefinition("h6", ComponentTypeName = "<h6> heading", IgnoreNameEndings = "Header,Heading")]
