9496cba604dca942397e628d0903237a904d88f4
Yaroslav Samus <yaroslav.samus@gmail.com>
2018-11-15 00:39:22 +0200

    Update XML comments of Button`1 component

--- src/Atata/Components/Button`1.cs
+++ src/Atata/Components/Button`1.cs
@@ -3,7 +3,7 @@
     /// <summary>
     /// Represents the button control.
     /// Default search is performed by the content and value (button by content text and input by value attribute).
-    /// Handles any input element with type="button", type="submit", type="reset" or button element.
+    /// Handles any input element with <c>type="button"</c>, <c>type="submit"</c>, <c>type="reset"</c> or <c>button</c> element.
     /// </summary>
     /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
     [ControlDefinition("*[self::input[@type='button' or @type='submit' or @type='reset'] or self::button]", ComponentTypeName = "button", IgnoreNameEndings = "Button")]
