1cdd3fd9491d804e829259e3ade3a2fa12225488
Yaroslav Samus <yaroslav.samus@gmail.com>
2018-11-15 00:32:59 +0200

    Update XML comments of UrlInput`1 component

--- src/Atata/Components/Fields/UrlInput`1.cs
+++ src/Atata/Components/Fields/UrlInput`1.cs
@@ -1,7 +1,7 @@
 ﻿namespace Atata
 {
     /// <summary>
-    /// Represents the URL input control (&lt;input type="url"&gt;).
+    /// Represents the URL input control (<c>&lt;input type="url"&gt;</c>).
     /// Default search is performed by the label.
     /// </summary>
     /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
