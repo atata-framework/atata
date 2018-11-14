f15e716762f70e23f7823fcff44d6eb3c34adc97
Yaroslav Samus <yaroslav.samus@gmail.com>
2018-11-15 00:28:01 +0200

    Update XML comments of RadioButtonList`2 and SearchInput`1 components

--- src/Atata/Components/Fields/SearchInput`1.cs
+++ src/Atata/Components/Fields/SearchInput`1.cs
@@ -1,7 +1,7 @@
 ﻿namespace Atata
 {
     /// <summary>
-    /// Represents the search input control (&lt;input type="search"&gt;).
+    /// Represents the search input control (<c>&lt;input type="search"&gt;</c>).
     /// Default search is performed by the label.
     /// </summary>
     /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
