7f1892abcd9758f83905b0cd3688695cb8720ea5
Yaroslav Samus <yaroslav.samus@gmail.com>
2018-11-16 00:19:11 +0200

    Update XML comments of Link`1 component

--- src/Atata/Components/Link`1.cs
+++ src/Atata/Components/Link`1.cs
@@ -1,7 +1,7 @@
 ﻿namespace Atata
 {
     /// <summary>
-    /// Represents the link control (&lt;a&gt;).
+    /// Represents the link control (<c>&lt;a&gt;</c>).
     /// Default search is performed by the content.
     /// </summary>
     /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
