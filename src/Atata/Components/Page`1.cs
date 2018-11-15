ded8c1c964e7ab0f5dd596f47b7eb9f6e30e7904
Yaroslav Samus <yaroslav.samus@gmail.com>
2018-11-16 00:21:36 +0200

    Update XML comments of Page`1 component

--- src/Atata/Components/Page`1.cs
+++ src/Atata/Components/Page`1.cs
@@ -2,7 +2,7 @@
 {
     /// <summary>
     /// Represents the whole HTML page and is the main base class to inherit for the pages.
-    /// Uses the &lt;body&gt; tag as a scope.
+    /// Uses the <c>&lt;body&gt;</c> tag as a scope.
     /// </summary>
     /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
     /// <seealso cref="PageObject{T}" />
