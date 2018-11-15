01baf47ba8bbe3870e5182e8c56accc7004738f9
Yaroslav Samus <yaroslav.samus@gmail.com>
2018-11-15 23:46:51 +0200

    Update XML comments of DateInput`1 component

--- src/Atata/Components/Fields/DateInput`1.cs
+++ src/Atata/Components/Fields/DateInput`1.cs
@@ -5,7 +5,7 @@ namespace Atata
     /// <summary>
     /// Represents the date input control.
     /// Default search is performed by the label.
-    /// The default format is <c>"d"</c> (short date pattern, e.g. 6/15/2009).
+    /// The default format is <c>"d"</c> (short date pattern, e.g. <c>6/15/2009</c>).
     /// Handles any input element with <c>type="date"</c>, <c>type="text"</c> or without the defined type attribute.
     /// </summary>
     /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
