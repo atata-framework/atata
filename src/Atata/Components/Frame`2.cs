4a2015ba447a5c5f6f07a29c11c98a81deb35274
Yaroslav Samus <yaroslav.samus@gmail.com>
2018-11-18 00:36:54 +0200

    Update XML comments of Frame`2 component

--- src/Atata/Components/Frame`2.cs
+++ src/Atata/Components/Frame`2.cs
@@ -17,7 +17,7 @@ namespace Atata
         /// </summary>
         /// <param name="framePageObject">The frame page object.</param>
         /// <param name="temporarily">
-        /// If set to <c>true</c> navigates temporarily preserving current page object state.
+        /// If set to <see langword="true"/> navigates temporarily preserving current page object state.
         /// If is not set, checks <see cref="GoTemporarilyAttribute"/>.</param>
         /// <returns>The instance of the frame page object.</returns>
         public TFramePageObject SwitchTo(TFramePageObject framePageObject = null, bool? temporarily = null)
@@ -30,7 +30,7 @@ namespace Atata
         /// </summary>
         /// <param name="action">The action to do in scope of frame.</param>
         /// <param name="temporarily">
-        /// If set to <c>true</c> navigates temporarily preserving current page object state.
+        /// If set to <see langword="true"/> navigates temporarily preserving current page object state.
         /// If is not set, checks <see cref="GoTemporarilyAttribute"/>.</param>
         /// <returns>The instance of the owner page object.</returns>
         public TOwner DoWithin(Action<TFramePageObject> action, bool? temporarily = null)
