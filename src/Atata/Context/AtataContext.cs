8d4f84b7b9acb973687689ad44e43d24a1450c63
Yaroslav Samus <yaroslav.samus@gmail.com>
2018-11-18 01:04:18 +0200

    Update XML comments of AtataContext class

--- src/Atata/Context/AtataContext.cs
+++ src/Atata/Context/AtataContext.cs
@@ -44,7 +44,7 @@ namespace Atata
 
         /// <summary>
         /// Gets or sets a value indicating whether the <see cref="Current"/> property use thread-static approach (value unique for each thread).
-        /// The default value is <c>true</c>.
+        /// The default value is <see langword="true"/>.
         /// </summary>
         public static bool IsThreadStatic
         {
@@ -101,63 +101,63 @@ namespace Atata
 
         /// <summary>
         /// Gets the base retry timeout.
-        /// The default value is 5 seconds.
+        /// The default value is <c>5</c> seconds.
         /// </summary>
         [Obsolete("Use BaseRetryTimeout instead.")] // Obsolete since v0.17.0.
         public TimeSpan RetryTimeout => BaseRetryTimeout;
 
         /// <summary>
         /// Gets the base retry interval.
-        /// The default value is 500 milliseconds.
+        /// The default value is <c>500</c> milliseconds.
         /// </summary>
         [Obsolete("Use BaseRetryInterval instead.")] // Obsolete since v0.17.0.
         public TimeSpan RetryInterval => BaseRetryInterval;
 
         /// <summary>
         /// Gets the base retry timeout.
-        /// The default value is 5 seconds.
+        /// The default value is <c>5</c> seconds.
         /// </summary>
         public TimeSpan BaseRetryTimeout { get; internal set; }
 
         /// <summary>
         /// Gets the base retry interval.
-        /// The default value is 500 milliseconds.
+        /// The default value is <c>500</c> milliseconds.
         /// </summary>
         public TimeSpan BaseRetryInterval { get; internal set; }
 
         /// <summary>
         /// Gets the element find timeout.
-        /// The default value is 5 seconds.
+        /// The default value is <c>5</c> seconds.
         /// </summary>
         public TimeSpan ElementFindTimeout { get; internal set; }
 
         /// <summary>
         /// Gets the element find retry interval.
-        /// The default value is 500 milliseconds.
+        /// The default value is <c>500</c> milliseconds.
         /// </summary>
         public TimeSpan ElementFindRetryInterval { get; internal set; }
 
         /// <summary>
         /// Gets the waiting timeout.
-        /// The default value is 5 seconds.
+        /// The default value is <c>5</c> seconds.
         /// </summary>
         public TimeSpan WaitingTimeout { get; internal set; }
 
         /// <summary>
         /// Gets the waiting retry interval.
-        /// The default value is 500 milliseconds.
+        /// The default value is <c>500</c> milliseconds.
         /// </summary>
         public TimeSpan WaitingRetryInterval { get; internal set; }
 
         /// <summary>
         /// Gets the verification timeout.
-        /// The default value is 5 seconds.
+        /// The default value is <c>5</c> seconds.
         /// </summary>
         public TimeSpan VerificationTimeout { get; internal set; }
 
         /// <summary>
         /// Gets the verification retry interval.
-        /// The default value is 500 milliseconds.
+        /// The default value is <c>500</c> milliseconds.
         /// </summary>
         public TimeSpan VerificationRetryInterval { get; internal set; }
 
@@ -230,7 +230,7 @@ namespace Atata
         /// <summary>
         /// Cleans up the test context.
         /// </summary>
-        /// <param name="quitDriver">if set to <c>true</c> quits WebDriver.</param>
+        /// <param name="quitDriver">if set to <see langword="true"/> quits WebDriver.</param>
         public void CleanUp(bool quitDriver = true)
         {
             if (disposed)
