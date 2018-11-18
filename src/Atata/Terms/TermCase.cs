ff82cf49d778feff8607c14ffdf3dd60c3e775bd
Yaroslav Samus <yaroslav.samus@gmail.com>
2018-11-19 00:55:01 +0200

    Update XML comments of TermCase

--- src/Atata/Terms/TermCase.cs
+++ src/Atata/Terms/TermCase.cs
@@ -11,77 +11,77 @@
         None,
 
         /// <summary>
-        /// Uses title case (e.g. "Some of the Terms").
+        /// Uses title case (e.g. <c>"Some of the Terms"</c>).
         /// </summary>
         Title,
 
         /// <summary>
-        /// Uses title case with all words capitalized (e.g. "Some Of The Terms").
+        /// Uses title case with all words capitalized (e.g. <c>"Some Of The Terms"</c>).
         /// </summary>
         Capitalized,
 
         /// <summary>
-        /// Uses sentence case (e.g. "Some term").
+        /// Uses sentence case (e.g. <c>"Some term"</c>).
         /// </summary>
         Sentence,
 
         /// <summary>
-        /// Uses mid-sentence case where the first word is not capitalized (e.g. "some term").
+        /// Uses mid-sentence case where the first word is not capitalized (e.g. <c>"some term"</c>).
         /// </summary>
         MidSentence,
 
         /// <summary>
-        /// Uses lower case (e.g. "some term").
+        /// Uses lower case (e.g. <c>"some term"</c>).
         /// </summary>
         Lower,
 
         /// <summary>
-        /// Uses lower case with words merging (e.g. "someterm").
+        /// Uses lower case with words merging (e.g. <c>"someterm"</c>).
         /// </summary>
         LowerMerged,
 
         /// <summary>
-        /// Uses upper case (e.g. "SOME TERM").
+        /// Uses upper case (e.g. <c>"SOME TERM"</c>).
         /// </summary>
         Upper,
 
         /// <summary>
-        /// Uses upper case with words merging (e.g. "SOMETERM").
+        /// Uses upper case with words merging (e.g. <c>"SOMETERM"</c>).
         /// </summary>
         UpperMerged,
 
         /// <summary>
-        /// Uses camel case (e.g. "someTerm").
+        /// Uses camel case (e.g. <c>"someTerm"</c>).
         /// </summary>
         Camel,
 
         /// <summary>
-        /// Uses pascal case (e.g. "SomeTerm").
+        /// Uses pascal case (e.g. <c>"SomeTerm"</c>).
         /// </summary>
         Pascal,
 
         /// <summary>
-        /// Uses dash ('-') and lower case (e.g. "some-term").
+        /// Uses dash ('-') and lower case (e.g. <c>"some-term"</c>).
         /// </summary>
         Kebab,
 
         /// <summary>
-        /// Uses hyphen ('‐') and lower case (e.g. "some‐term").
+        /// Uses hyphen ('‐') and lower case (e.g. <c>"some‐term"</c>).
         /// </summary>
         HyphenKebab,
 
         /// <summary>
-        /// Uses dash ('-') and pascal case (e.g. "Some-Term").
+        /// Uses dash ('-') and pascal case (e.g. <c>"Some-Term"</c>).
         /// </summary>
         PascalKebab,
 
         /// <summary>
-        /// Uses hyphen ('‐') and pascal case (e.g. "Some‐Term").
+        /// Uses hyphen ('‐') and pascal case (e.g. <c>"Some‐Term"</c>).
         /// </summary>
         PascalHyphenKebab,
 
         /// <summary>
-        /// Uses underscore ('_') and lower case (e.g. "some_term").
+        /// Uses underscore ('_') and lower case (e.g. <c>"some_term"</c>).
         /// </summary>
         Snake
     }
