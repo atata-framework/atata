3f34294645056777aece3da80f589030f863ab18
Yevgeniy Shunevych <yevgeniy.shunevych@gmail.com>
2018-11-20 14:30:26 +0200

    Update src/Atata/Components/Fields/NumberInput`1.cs
    Co-Authored-By: ysamus <yaroslav.samus@gmail.com>

--- src/Atata/Components/Fields/NumberInput`1.cs
+++ src/Atata/Components/Fields/NumberInput`1.cs
@@ -3,7 +3,7 @@
     /// <summary>
     /// Represents the number input control.
     /// Default search is performed by the label.
-    /// Handles any <c>input</c> element with <c>type="number"</c>, <c>type="text"</c> or without the defined <c>type</c> attribute.
+    /// Handles any <c>&lt;input&gt;</c> element with <c>type="number"</c>, <c>type="text"</c> or without the defined <c>type</c> attribute.
     /// </summary>
     /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
     [ControlDefinition("input[@type='number' or @type='text' or not(@type)]")]
