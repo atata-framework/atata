45dca9bd5a1b7b2cb832c995ad9d16382dd3b9d0
Yevgeniy Shunevych <yevgeniy.shunevych@gmail.com>
2018-11-20 14:31:49 +0200

    Update src/Atata/Components/Fields/TextInput`1.cs
    Co-Authored-By: ysamus <yaroslav.samus@gmail.com>

--- src/Atata/Components/Fields/TextInput`1.cs
+++ src/Atata/Components/Fields/TextInput`1.cs
@@ -3,7 +3,7 @@
     /// <summary>
     /// Represents the text input control (<c>&lt;input type="text"&gt;</c>).
     /// Default search is performed by the label.
-    /// Handles any input element with <c>type="text"</c> or without the defined <c>type</c> attribute.
+    /// Handles any <c>&lt;input&gt;</c> element with <c>type="text"</c> or without the defined <c>type</c> attribute.
     /// </summary>
     /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
     [ControlDefinition("input[@type='text' or not(@type)]")]
