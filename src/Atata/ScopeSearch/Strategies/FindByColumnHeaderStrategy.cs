0641f9ad4e6539901070acbb16c6c38510d633da
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2019-04-18 09:38:27 +0300

    #248 Add HeaderXPath property to FindByColumnHeaderStrategy; #249 Split FindByColumnHeaderStrategy.Find method; Add XML comments to FindByColumnHeaderStrategy

--- src/Atata/ScopeSearch/Strategies/FindByColumnHeaderStrategy.cs
+++ src/Atata/ScopeSearch/Strategies/FindByColumnHeaderStrategy.cs
@@ -3,30 +3,45 @@ using OpenQA.Selenium;
 
 namespace Atata
 {
+    /// <summary>
+    /// Represents a strategy that finds a control in a cell that corresponds the column searched by the column header text.
+    /// First finds the index of the column header and then finds the cell by this index.
+    /// </summary>
     public class FindByColumnHeaderStrategy : IComponentScopeLocateStrategy
     {
-        private readonly string headerXPath;
+        /// <summary>
+        /// The default XPath of the header, which is <c>"ancestor::table[1]//th"</c>.
+        /// </summary>
+        public const string DefaultHeaderXPath = "ancestor::table[1]//th";
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FindByColumnHeaderStrategy"/> class
+        /// using <see cref="DefaultHeaderXPath"/>.
+        /// </summary>
         public FindByColumnHeaderStrategy()
-            : this("(ancestor::table)[position() = last()]//th")
+            : this(DefaultHeaderXPath)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FindByColumnHeaderStrategy"/> class
+        /// using the specified <paramref name="headerXPath"/> argument value.
+        /// </summary>
+        /// <param name="headerXPath">The XPath of the header element.</param>
         public FindByColumnHeaderStrategy(string headerXPath)
         {
-            this.headerXPath = headerXPath;
+            HeaderXPath = headerXPath;
         }
+
+        /// <summary>
+        /// Gets or sets the XPath of the header element.
+        /// The default value is <c>"ancestor::table[1]//th"</c>.
+        /// </summary>
+        public string HeaderXPath { get; set; }
 
         public ComponentScopeLocateResult Find(IWebElement scope, ComponentScopeLocateOptions options, SearchOptions searchOptions)
         {
-            var headers = scope.GetAll(By.XPath(headerXPath).With(searchOptions).OfAnyVisibility());
-            var headerNamePredicate = options.Match.GetPredicate();
-
-            int? columnIndex = headers.
-                Select((x, i) => new { x.Text, Index = i }).
-                Where(x => options.Terms.Any(term => headerNamePredicate(x.Text, term))).
-                Select(x => (int?)x.Index).
-                FirstOrDefault();
+            int? columnIndex = GetColumnIndex(scope, options, searchOptions);
 
             if (columnIndex == null)
             {
@@ -36,7 +51,38 @@ namespace Atata
                     throw ExceptionFactory.CreateForNoSuchElement(options.GetTermsAsString(), searchContext: scope);
             }
 
-            return new FindByColumnIndexStrategy(columnIndex.Value).Find(scope, options, searchOptions);
+            IComponentScopeLocateStrategy nextStrategy = CreateColumnIndexStrategy(columnIndex.Value);
+            return new SequalComponentScopeLocateResult(scope, nextStrategy);
+        }
+
+        /// <summary>
+        /// Gets the index of the column.
+        /// </summary>
+        /// <param name="scope">The scope.</param>
+        /// <param name="options">The component scope locate options.</param>
+        /// <param name="searchOptions">The search options.</param>
+        /// <returns>The index of the column or <see langword="null"/> if not found.</returns>
+        protected virtual int? GetColumnIndex(IWebElement scope, ComponentScopeLocateOptions options, SearchOptions searchOptions)
+        {
+            var headers = scope.GetAll(By.XPath(HeaderXPath).With(searchOptions).OfAnyVisibility());
+            var headerNamePredicate = options.Match.GetPredicate();
+
+            return headers.
+                Select((x, i) => new { x.Text, Index = i }).
+                Where(x => options.Terms.Any(term => headerNamePredicate(x.Text, term))).
+                Select(x => (int?)x.Index).
+                FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Creates the strategy to find a component by the column index.
+        /// By default creates an instance of <see cref="FindByColumnIndexStrategy"/>.
+        /// </summary>
+        /// <param name="columnIndex">Index of the column.</param>
+        /// <returns>An instance of <see cref="FindByColumnIndexStrategy"/>.</returns>
+        protected virtual IComponentScopeLocateStrategy CreateColumnIndexStrategy(int columnIndex)
+        {
+            return new FindByColumnIndexStrategy(columnIndex);
         }
     }
 }
