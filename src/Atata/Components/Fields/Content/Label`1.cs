a83a3f65c18a265d4905061f84ceddd126b0cf2b
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2019-04-25 17:26:21 +0300

    #259 Add Label<T, TOwner> control

--- src/Atata/Components/Fields/Content/Label`1.cs
+++ src/Atata/Components/Fields/Content/Label`1.cs
@@ -5,9 +5,7 @@
     /// Default search is performed by the content.
     /// </summary>
     /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
-    [ControlDefinition("label", ComponentTypeName = "label", IgnoreNameEndings = "Label")]
-    [ControlFinding(FindTermBy.Content)]
-    public class Label<TOwner> : Content<string, TOwner>
+    public class Label<TOwner> : Label<string, TOwner>
         where TOwner : PageObject<TOwner>
     {
     }
---
+++ src/Atata/Components/Fields/Content/Label`2.cs
@@ -0,0 +1,15 @@
+﻿namespace Atata
+{
+    /// <summary>
+    /// Represents the label control (<c>&lt;label&gt;</c>).
+    /// Default search is performed by the content.
+    /// </summary>
+    /// <typeparam name="T">The type of the content.</typeparam>
+    /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
+    [ControlDefinition("label", ComponentTypeName = "label", IgnoreNameEndings = "Label")]
+    [ControlFinding(FindTermBy.Content)]
+    public class Label<T, TOwner> : Content<T, TOwner>
+        where TOwner : PageObject<TOwner>
+    {
+    }
+}
