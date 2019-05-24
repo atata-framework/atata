d572cf7e7c6d03459d44bf0b57322591c37e46ce
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2019-05-24 10:46:46 +0300

    #272 Add FindItemByRelativeElementContentAttribute

---
+++ src/Atata/Attributes/Behaviors/FindItemByRelativeElementContentAttribute.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Atata
+{
+    /// <summary>
+    /// Represents the behavior to find an item of <see cref="OptionList{T, TOwner}"/> control by relative element content using its XPath.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Class | AttributeTargets.Interface | AttributeTargets.Assembly, AllowMultiple = true)]
+    public class FindItemByRelativeElementContentAttribute : TermSettingsAttribute, IFindItemAttribute
+    {
+        public FindItemByRelativeElementContentAttribute(string relativeElementXPath)
+        {
+            RelativeElementXPath = relativeElementXPath;
+        }
+
+        public FindItemByRelativeElementContentAttribute(string relativeElementXPath, TermCase termCase)
+            : base(termCase)
+        {
+            RelativeElementXPath = relativeElementXPath;
+        }
+
+        public FindItemByRelativeElementContentAttribute(string relativeElementXPath, TermMatch match)
+            : base(match)
+        {
+            RelativeElementXPath = relativeElementXPath;
+        }
+
+        public FindItemByRelativeElementContentAttribute(string relativeElementXPath, TermMatch match, TermCase termCase)
+            : base(match, termCase)
+        {
+            RelativeElementXPath = relativeElementXPath;
+        }
+
+        /// <summary>
+        /// Gets the relative element XPath.
+        /// </summary>
+        public string RelativeElementXPath { get; }
+
+        public IItemElementFindStrategy CreateStrategy(UIComponent component, UIComponentMetadata metadata)
+        {
+            return new FindItemByRelativeElementContentStrategy(RelativeElementXPath);
+        }
+    }
+}
---
+++ src/Atata/ScopeSearch/Strategies/FindItemByRelativeElementContentStrategy.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+
+namespace Atata
+{
+    /// <summary>
+    /// Represents an item find strategy that finds the item by relative element content using its XPath.
+    /// </summary>
+    public class FindItemByRelativeElementContentStrategy : TermItemElementFindStrategy
+    {
+        public FindItemByRelativeElementContentStrategy(string relativeElementXPath)
+        {
+            RelativeElementXPath = relativeElementXPath;
+        }
+
+        public string RelativeElementXPath { get; }
+
+        public override string GetXPathCondition(object parameter, TermOptions termOptions)
+        {
+            return $"[{RelativeElementXPath}[{TermResolver.CreateXPathCondition(parameter, termOptions)}]]";
+        }
+
+        protected override string GetParameterAsString(IWebElement element)
+        {
+            return element.Get(By.XPath(RelativeElementXPath).AtOnce()).Text;
+        }
+    }
+}
