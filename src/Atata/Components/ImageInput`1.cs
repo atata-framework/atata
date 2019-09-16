f33fc68465c38352c196065faa2468639f8900bb
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2019-09-16 15:18:02 +0300

    #307 Add ImageInput<TOwner> control

---
+++ src/Atata/Components/ImageInput`1.cs
@@ -0,0 +1,23 @@
+﻿namespace Atata
+{
+    /// <summary>
+    /// Represents the image input control (<c>&lt;input type="image"&gt;</c>).
+    /// Default search is performed by <c>alt</c> attribute using <see cref="FindByAltAttribute"/>.
+    /// </summary>
+    /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
+    [ControlDefinition("input[@type='image']", ComponentTypeName = "image input")]
+    [ControlFinding(FindTermBy.Alt)]
+    public class ImageInput<TOwner> : Control<TOwner>
+        where TOwner : PageObject<TOwner>
+    {
+        /// <summary>
+        /// Gets the <see cref="DataProvider{TData, TOwner}"/> instance for the <c>src</c> attribute.
+        /// </summary>
+        public DataProvider<string, TOwner> Source => Attributes.Src;
+
+        /// <summary>
+        /// Gets the <see cref="DataProvider{TData, TOwner}"/> instance for the <c>alt</c> attribute.
+        /// </summary>
+        public DataProvider<string, TOwner> Alt => Attributes.Alt;
+    }
+}
