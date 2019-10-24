86094eb4743f1773082b8fb63e8dd100cacd1f72
Yevgeniy Shunevych <yevgeniy.shunevych@gmail.com>
2019-10-24 16:44:00 -0500

    #321 Add WaitUntilEnabledAttribute trigger

---
+++ src/Atata/Attributes/Triggers/WaitUntilEnabledAttribute.cs
@@ -0,0 +1,19 @@
+﻿namespace Atata
+{
+    /// <summary>
+    /// Specifies the waiting for the control to become enabled.
+    /// By default occurs before the click and before the set.
+    /// </summary>
+    public class WaitUntilEnabledAttribute : WaitingTriggerAttribute
+    {
+        public WaitUntilEnabledAttribute(TriggerEvents on = TriggerEvents.BeforeClick | TriggerEvents.BeforeSet, TriggerPriority priority = TriggerPriority.Medium)
+            : base(on, priority)
+        {
+        }
+
+        protected internal override void Execute<TOwner>(TriggerContext<TOwner> context)
+        {
+            ((Control<TOwner>)context.Component).WaitTo.Within(Timeout, RetryInterval).BeEnabled();
+        }
+    }
+}
