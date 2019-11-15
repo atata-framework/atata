1f512c0cd17a6a77a0546c71137f94ff23ea7850
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2019-11-15 12:02:11 +0200

    #328 Add FindByDescendantAttributeAttribute

---
+++ src/Atata/ScopeSearch/Strategies/FindByDescendantAttributeStrategy.cs
@@ -0,0 +1,18 @@
+﻿namespace Atata
+{
+    public class FindByDescendantAttributeStrategy : XPathComponentScopeLocateStrategy
+    {
+        private readonly string attributeName;
+
+        public FindByDescendantAttributeStrategy(string attributeName)
+        {
+            this.attributeName = attributeName;
+        }
+
+        protected override string Build(ComponentScopeXPathBuilder builder, ComponentScopeLocateOptions options)
+        {
+            return builder.
+                WrapWithIndex(x => x.OuterXPath.ComponentXPath[c => c.Descendant.Any[d => d.TermsConditionOf(attributeName)]]);
+        }
+    }
+}
