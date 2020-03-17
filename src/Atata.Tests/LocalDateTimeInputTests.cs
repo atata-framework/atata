f751a8eda170753c526670ab8e0289bc310c1db9
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2020-03-17 17:17:49 +0200

    #346 Add LocalDateTimeInput<TOwner> control

---
+++ src/Atata.Tests/LocalDateTimeInputTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Atata.Tests
+{
+    public class LocalDateTimeInputTests : UITestFixture
+    {
+        private const string ControlValueFormat = "yyyy-MM-ddTHH:mm";
+
+        private LocalDateTimeInputPage page;
+
+        protected override void OnSetUp()
+        {
+            page = Go.To<LocalDateTimeInputPage>();
+        }
+
+        [Test]
+        public void LocalDateTimeInput()
+        {
+            var control = page.Regular;
+            var outputControl = page.RegularOutput;
+
+            control.Should.BeNull();
+
+            var testValue = new DateTime(2019, 11, 27, 20, 45, 0);
+
+            control.Set(testValue);
+            control.Should.Equal(testValue);
+            outputControl.Should.Equal(testValue);
+
+            control.Clear();
+            control.Should.BeNull();
+            outputControl.Should.BeNull();
+
+            control.Append("invalid");
+            control.Should.BeNull();
+            outputControl.Should.BeNull();
+
+            testValue = new DateTime(2011, 1, 2, 8, 00, 0);
+            control.Set(testValue);
+            control.Should.Equal(testValue);
+            outputControl.Should.Equal(testValue);
+        }
+    }
+}
---
+++ src/Atata/Components/Fields/LocalDateTimeInput`1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Atata
+{
+    /// <summary>
+    /// Represents the local date/time input control (<c>&lt;input type="datetime-local"&gt;</c>).
+    /// Default search is performed by the label.
+    /// The default format is <c>"yyyy-MM-ddTHH:mm"</c>.
+    /// </summary>
+    /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
+    [ControlDefinition("input[@type='datetime-local']", ComponentTypeName = "local date/time input")]
+    [Format("g")]
+    [ValueGetFormat("yyyy-MM-ddTHH:mm")]
+    [ValueSetFormat("yyyy-MM-ddTHH:mm")]
+    public class LocalDateTimeInput<TOwner> : Input<DateTime?, TOwner>
+        where TOwner : PageObject<TOwner>
+    {
+        protected override DateTime? ConvertStringToValueUsingGetFormat(string value)
+        {
+            try
+            {
+                return base.ConvertStringToValueUsingGetFormat(value);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        protected override void SetValue(DateTime? value)
+        {
+            string valueAsString = ConvertValueToStringUsingSetFormat(value);
+
+            Driver.ExecuteScript(
+                $"arguments[0].value = '{valueAsString}';" +
+                $"arguments[0].dispatchEvent(new Event('change'));", Scope);
+        }
+    }
+}
