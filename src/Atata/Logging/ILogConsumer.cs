7c9bea719e0103feab08ac8e1f20de3f88854187
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2020-03-26 15:16:20 +0200

    Add XML comments to ILogConsumer

--- src/Atata/Logging/ILogConsumer.cs
+++ src/Atata/Logging/ILogConsumer.cs
@@ -1,7 +1,14 @@
 ﻿namespace Atata
 {
+    /// <summary>
+    /// Defines a method to log the event information.
+    /// </summary>
     public interface ILogConsumer
     {
+        /// <summary>
+        /// Logs the specified event information.
+        /// </summary>
+        /// <param name="eventInfo">The event information.</param>
         void Log(LogEventInfo eventInfo);
     }
 }
