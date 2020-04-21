6863e950c694add1af078875807b72ebe26d8f47
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2020-04-21 18:52:21 +0300

    Add overloaded Randomizer.GetInt method; Update Randomizer to use unique Random object each time

--- src/Atata/Randomizer.cs
+++ src/Atata/Randomizer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Atata
@@ -26,14 +27,30 @@ namespace Atata
             return string.Format(format, stringBuilder);
         }
 
+        /// <summary>
+        /// Returns a non-negative random integer that is less than the specified maximum.
+        /// </summary>
+        /// <param name="exclusiveMax">The exclusive upper bound of the random number to be generated. Must be greater than or equal to <c>0</c>.</param>
+        /// <returns>The random <see cref="int"/> value.</returns>
+        public static int GetInt(int exclusiveMax)
+        {
+            return CreateRandom().Next(exclusiveMax);
+        }
+
+        /// <summary>
+        /// Returns a random integer that is within a specified range.
+        /// </summary>
+        /// <param name="min">The inclusive lower bound of the random number returned.</param>
+        /// <param name="max">The inclusive upper bound of the random number returned. Must be greater than or equal to <paramref name="min"/>.</param>
+        /// <returns>The random <see cref="int"/> value.</returns>
         public static int GetInt(int min, int max)
         {
-            return new Random().Next(min, max + 1);
+            return CreateRandom().Next(min, max + 1);
         }
 
         public static decimal GetDecimal(decimal min, decimal max, int precision)
         {
-            var next = (decimal)new Random().NextDouble();
+            var next = (decimal)CreateRandom().NextDouble();
             decimal value = min + (next * (max - min));
 
             return Math.Round(value, precision);
@@ -58,7 +75,7 @@ namespace Atata
 
         public static bool GetBool()
         {
-            return new Random().Next(2) == 0;
+            return CreateRandom().Next(2) == 0;
         }
 
         public static T GetOneOf<T>(params T[] values)
@@ -70,7 +87,7 @@ namespace Atata
         {
             values.CheckNotNullOrEmpty(nameof(values));
 
-            int valueIndex = new Random().Next(values.Count());
+            int valueIndex = CreateRandom().Next(values.Count());
             return values.ElementAt(valueIndex);
         }
 
@@ -98,7 +115,7 @@ namespace Atata
             List<T> valuesAsList = values.ToList();
             max.CheckLessOrEqual(nameof(max), valuesAsList.Count, $"Count of {nameof(values)} is {valuesAsList.Count}");
 
-            int count = max == min ? max : (min + new Random().Next(max + 1 - min));
+            int count = max == min ? max : (min + CreateRandom().Next(max + 1 - min));
 
             T[] randomValues = new T[count];
 
@@ -107,12 +124,18 @@ namespace Atata
 
             for (int i = 0; i < count; i++)
             {
-                int valueIndex = new Random().Next(valuesAsList.Count);
+                int valueIndex = CreateRandom().Next(valuesAsList.Count);
                 randomValues[i] = valuesAsList[valueIndex];
                 valuesAsList.Remove(randomValues[i]);
             }
 
             return randomValues;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static Random CreateRandom()
+        {
+            return new Random(Guid.NewGuid().GetHashCode());
         }
     }
 }
