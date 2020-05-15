dded2f9d0e03d8e795391665d5cd15c7c1185fbd
apidhainy <apidhainy@ezlinks.com>
2020-05-15 19:25:31 +0300

    add PressEndAttribute.cs add PressHomeAttribute.cs add PressTriggers.cshtml add PressTriggersPage.cs add PressTriggersTests.cs

---
+++ src/Atata.Tests/Components/PressTriggersPage.cs
@@ -0,0 +1,16 @@
+﻿using _ = Atata.Tests.PressTriggersPage;
+
+namespace Atata.Tests
+{
+    [Url("PressTriggers")]
+    public class PressTriggersPage : Page<_>
+    {
+        [FindById("bottom-text")]
+        [PressEnd(on: TriggerEvents.BeforeClick)]
+        public Text<_> BottomText { get; private set; }
+
+        [FindById("top-text")]
+        [PressHome(on: TriggerEvents.BeforeClick)]
+        public Text<_> TopText { get; private set; }
+    }
+}
---
+++ src/Atata.Tests/PressTriggersTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace Atata.Tests
+{
+    public class PressTriggersTests : UITestFixture
+    {
+        [Test]
+        public void PressEnd_BeforeClick()
+        {
+            var page = Go.To<PressTriggersPage>();
+
+            Assert.That(IsVisibleInViewport(page.BottomText), Is.False);
+
+            page.BottomText.Click();
+
+            Assert.That(IsVisibleInViewport(page.BottomText), Is.True);
+        }
+
+        [Test]
+        public void PressHome_BeforeClick()
+        {
+            var page = Go.To<PressTriggersPage>();
+
+            page.Press(Keys.End);
+
+            Assert.That(IsVisibleInViewport(page.TopText), Is.False);
+
+            page.TopText.Click();
+
+            Assert.That(IsVisibleInViewport(page.TopText), Is.True);
+        }
+
+        private static bool IsVisibleInViewport<T>(Control<T> element)
+            where T : PageObject<T>
+        {
+            return (bool)((IJavaScriptExecutor)AtataContext.Current.Driver).ExecuteScript(
+                "var elem = arguments[0],                 " +
+                "  box = elem.getBoundingClientRect(),    " +
+                "  cx = box.left + box.width / 2,         " +
+                "  cy = box.top + box.height / 2,         " +
+                "  e = document.elementFromPoint(cx, cy); " +
+                "for (; e; e = e.parentElement) {         " +
+                "  if (e === elem)                        " +
+                "    return true;                         " +
+                "}                                        " +
+                "return false;                            ",
+                element.Scope);
+        }
+    }
+}
