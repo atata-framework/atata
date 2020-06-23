fa7a484a05623a04cfa169247dbab5a4c7b903da
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2020-06-23 19:03:08 +0300

    Add XML comments to LogLevel

--- src/Atata/Logging/LogLevel.cs
+++ src/Atata/Logging/LogLevel.cs
@@ -5,11 +5,34 @@
     /// </summary>
     public enum LogLevel
     {
+        /// <summary>
+        /// Trace log level.
+        /// </summary>
         Trace,
+
+        /// <summary>
+        /// Debug log level.
+        /// </summary>
         Debug,
+
+        /// <summary>
+        /// Info log level.
+        /// </summary>
         Info,
+
+        /// <summary>
+        /// Warn log level.
+        /// </summary>
         Warn,
+
+        /// <summary>
+        /// Error log level.
+        /// </summary>
         Error,
+
+        /// <summary>
+        /// Fatal log level.
+        /// </summary>
         Fatal
     }
 }
