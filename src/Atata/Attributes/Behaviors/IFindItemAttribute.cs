655768a47d04d50434b7bfaaa7dd7adf10d1c63f
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2020-10-23 18:45:58 +0300

    Add TODO comment to IFindItemAttribute

--- src/Atata/Attributes/Behaviors/IFindItemAttribute.cs
+++ src/Atata/Attributes/Behaviors/IFindItemAttribute.cs
@@ -1,5 +1,6 @@
 ﻿namespace Atata
 {
+    // TODO: Remove IFindItemAttribute.
     public interface IFindItemAttribute
     {
         IItemElementFindStrategy CreateStrategy(UIComponent component, UIComponentMetadata metadata);
