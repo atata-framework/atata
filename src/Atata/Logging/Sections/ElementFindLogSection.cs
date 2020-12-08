82c8c4f99ff56b2b8a9c0321b6d9e5087778c257
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2020-12-08 17:38:56 +0200

    #443 Add extension methods for ISearchContext that wrap actual methods with log sections

---
+++ src/Atata/Logging/Sections/ElementFindLogSection.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using OpenQA.Selenium;
+
+namespace Atata
+{
+    public class ElementFindLogSection : LogSection
+    {
+        public ElementFindLogSection(ISearchContext searchContext, By by, bool multiple = false)
+        {
+            StringBuilder builder = new StringBuilder("Find ");
+
+            Visibility visibility = by.GetSearchOptionsOrDefault().Visibility;
+
+            if (visibility != Visibility.Any)
+                builder.Append($"{visibility.ToString(TermCase.Lower)} ");
+
+            builder.Append("element");
+
+            if (multiple)
+                builder.Append('s');
+
+            builder.Append($" by {by.ToDescriptiveString()} in {ResolveSearchContextName(searchContext)}");
+
+            Message = builder.ToString();
+            Level = LogLevel.Trace;
+        }
+
+        private static string ResolveSearchContextName(ISearchContext searchContext)
+        {
+            return searchContext is IWebDriver
+                ? searchContext.GetType().Name
+                : Stringifier.ToString(searchContext);
+        }
+    }
+}
