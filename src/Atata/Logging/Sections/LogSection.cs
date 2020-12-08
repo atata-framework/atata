706da8157c11cf13f89e0b5cd5061114e7d41b39
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2020-12-08 16:53:36 +0200

    #442 Add result properties to LogSection

--- src/Atata/Logging/Sections/LogSection.cs
+++ src/Atata/Logging/Sections/LogSection.cs
@@ -2,19 +2,56 @@
 
 namespace Atata
 {
+    /// <summary>
+    /// Represents the log section.
+    /// </summary>
     public class LogSection
     {
+        private object result;
+
         public LogSection(string message = null, LogLevel level = LogLevel.Info)
         {
             Message = message;
             Level = level;
         }
 
+        /// <summary>
+        /// Gets or sets the message.
+        /// </summary>
         public string Message { get; protected set; }
 
+        /// <summary>
+        /// Gets or sets the log level.
+        /// </summary>
         public LogLevel Level { get; protected set; }
 
+        /// <summary>
+        /// Gets the date/time of section start.
+        /// </summary>
         public DateTime StartedAt { get; internal set; }
+
+        /// <summary>
+        /// Gets a value indicating whether the result is set.
+        /// </summary>
+        public bool IsResultSet { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the result.
+        /// </summary>
+        public object Result
+        {
+            get => result;
+            set
+            {
+                result = value;
+                IsResultSet = true;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the exception.
+        /// </summary>
+        public Exception Exception { get; set; }
 
         public TimeSpan GetDuration()
         {
