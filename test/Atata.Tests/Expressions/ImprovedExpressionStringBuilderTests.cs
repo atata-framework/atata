8fc28df4fbf3a30d2b89e7e2c4e8e5ae6217278c
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2021-03-24 15:51:41 +0200

    #485 New ImprovedExpressionStringBuilder class

--- test/Atata.Tests/Expressions/ImprovedExpressionStringBuilderTests.cs
+++ test/Atata.Tests/Expressions/ImprovedExpressionStringBuilderTests.cs
@@ -58,7 +58,6 @@ namespace Atata.Tests.Expressions
             // Variable:
             string itemName = "VarStr";
             TestModel item = new TestModel { Name = "PropStr" };
-            Value<string, object> valueItem = null;
             bool? nullableBool = null;
             bool? nullableBoolIsTrue = true;
 
@@ -68,10 +67,6 @@ namespace Atata.Tests.Expressions
                 .Returns("x => x.Item == \"FldStr\"");
             TestPredicate(x => x.Item != item.Name)
                 .Returns("x => x.Item != item.Name");
-            TestPredicate(x => x.Item == valueItem)
-                .Returns("x => x.Item == valueItem");
-            TestPredicate(x => x.Item == valueItem.Value)
-                .Returns("x => x.Item == valueItem.Value");
             TestPredicate(x => x.Item.Attributes.Checked == nullableBool)
                 .Returns("x => x.Item.Attributes.Checked == null");
             TestPredicate(x => x.Item.Attributes.Checked == nullableBoolIsTrue)
