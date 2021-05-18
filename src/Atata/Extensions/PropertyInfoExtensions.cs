47e108cc54dd1e4d9a023935741c5fa6e945937d
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2021-05-18 18:37:09 +0300

    #495 Add NLogFileConsumer log consumer: fix PropertyInfoExtensions

--- src/Atata/Extensions/PropertyInfoExtensions.cs
+++ src/Atata/Extensions/PropertyInfoExtensions.cs
@@ -5,12 +5,12 @@ namespace Atata
     internal static class PropertyInfoExtensions
     {
         internal static object GetValue(this PropertyInfo property, object obj) =>
-            property.GetValue(obj, new object[0]);
+            property.GetValue(obj, null);
 
         internal static object GetStaticValue(this PropertyInfo property) =>
-            property.GetValue(null, new object[0]);
+            property.GetValue(null, null);
 
         internal static void SetStaticValue(this PropertyInfo property, object value) =>
-            property.SetValue(null, value);
+            property.SetValue(null, value, null);
     }
 }
