75eaf30d6625870d4ecd43db006cbca4bd5eb233
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2021-08-19 13:34:36 +0300

    #530 New `TypesTextUsingScriptAttribute`

---
+++ src/Atata/Attributes/Behaviors/TextType/TypesTextUsingScriptAttribute.cs
@@ -0,0 +1,13 @@
+﻿namespace Atata
+{
+    /// <summary>
+    /// Represents the behavior for control value set by executing
+    /// <c>HTMLElement.value += '{value}'; HTMLElement.dispatchEvent(new Event('change'));</c> JavaScript.
+    /// </summary>
+    public class TypesTextUsingScriptAttribute : TextTypeBehaviorAttribute
+    {
+        /// <inheritdoc/>
+        public override void Execute<TOwner>(IUIComponent<TOwner> component, string value) =>
+            component.Script.AddValueAndDispatchChangeEvent(value);
+    }
+}
---
+++ test/Atata.Tests/Bahaviors/TextTypeBehaviorAttributeTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Atata.Tests.Bahaviors
+{
+    public class TextTypeBehaviorAttributeTests : UITestFixture
+    {
+        private const string InitialValue = "abc";
+
+        private const string SetValue = "def";
+
+        private const string ConcatValue = InitialValue + SetValue;
+
+        private static IEnumerable<TestCaseData> Source =>
+            new[]
+            {
+                new TestCaseData(new TypesTextUsingSendKeysAttribute()).Returns(ConcatValue),
+                new TestCaseData(new TypesTextUsingScriptAttribute()).Returns(ConcatValue)
+            };
+
+        [TestCaseSource(nameof(Source))]
+        public string Execute(TextTypeBehaviorAttribute behavior)
+        {
+            var sut = Go.To<InputPage>().TextInput;
+            sut.Set(InitialValue);
+
+            sut.Metadata.Push(behavior);
+
+            sut.Type(SetValue);
+
+            return sut.Value;
+        }
+    }
+}
