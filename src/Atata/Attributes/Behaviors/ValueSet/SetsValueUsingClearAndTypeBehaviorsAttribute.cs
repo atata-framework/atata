48abb0c63529cf89e9ac5fda8990c9210ec2dba5
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2021-08-19 12:52:01 +0300

    #527 New `SetsValueUsingClearAndTypeBehaviorsAttribute`

---
+++ src/Atata/Attributes/Behaviors/ValueSet/SetsValueUsingClearAndTypeBehaviorsAttribute.cs
@@ -0,0 +1,46 @@
+﻿namespace Atata
+{
+    /// <summary>
+    /// Represents the behavior for control value set by executing <see cref="ValueClearBehaviorAttribute"/> behavior first;
+    /// then, if value to set is not <see langword="null"/> or empty,
+    /// executes <see cref="TextTypeBehaviorAttribute"/> behavior.
+    /// </summary>
+    public class SetsValueUsingClearAndTypeBehaviorsAttribute : ValueSetBehaviorAttribute
+    {
+        private readonly bool _useUIComponentScopeCache;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SetsValueUsingClearAndTypeBehaviorsAttribute"/> class.
+        /// </summary>
+        public SetsValueUsingClearAndTypeBehaviorsAttribute()
+            : this(true)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SetsValueUsingClearAndTypeBehaviorsAttribute"/> class.
+        /// </summary>
+        /// <param name="useUIComponentScopeCache">If set to <c>true</c>, uses UI component scope cache.</param>
+        public SetsValueUsingClearAndTypeBehaviorsAttribute(bool useUIComponentScopeCache)
+        {
+            _useUIComponentScopeCache = useUIComponentScopeCache;
+        }
+
+        /// <inheritdoc/>
+        public override void Execute<TOwner>(IUIComponent<TOwner> component, string value)
+        {
+            void DoExecute()
+            {
+                component.ExecuteBehavior<ValueClearBehaviorAttribute>(x => x.Execute(component));
+
+                if (!string.IsNullOrEmpty(value))
+                    component.ExecuteBehavior<TextTypeBehaviorAttribute>(x => x.Execute(component, value));
+            }
+
+            if (_useUIComponentScopeCache)
+                AtataContext.Current.UIComponentScopeCache.ExecuteWithin(DoExecute);
+            else
+                DoExecute();
+        }
+    }
+}
---
+++ test/Atata.Tests/Bahaviors/ValueSet/ValueSetBehaviorAttributeTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Atata.Tests.Bahaviors
+{
+    public class ValueSetBehaviorAttributeTests : UITestFixture
+    {
+        private const string InitialValue = "abc";
+
+        private const string SetValue = "def";
+
+        private static IEnumerable<TestCaseData> Source =>
+            new[]
+            {
+                new TestCaseData(new SetsValueUsingClearAndTypeBehaviorsAttribute()).Returns(SetValue),
+                new TestCaseData(new SetsValueUsingClearAndSendKeysAttribute()).Returns(SetValue),
+                new TestCaseData(new SetsValueUsingScriptAttribute()).Returns(SetValue),
+                new TestCaseData(new SetsValueUsingCharByCharTypingAttribute()).Returns(InitialValue + SetValue),
+                new TestCaseData(new SetsValueUsingSendKeysAttribute()).Returns(InitialValue + SetValue)
+            };
+
+        [TestCaseSource(nameof(Source))]
+        public string Execute(ValueSetBehaviorAttribute behavior)
+        {
+            var sut = Go.To<InputPage>().TextInput;
+            sut.Set(InitialValue);
+
+            sut.Metadata.Push(behavior);
+
+            sut.Set(SetValue);
+
+            return sut.Value;
+        }
+    }
+}
