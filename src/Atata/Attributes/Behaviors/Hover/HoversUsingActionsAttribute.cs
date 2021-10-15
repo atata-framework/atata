c23713057c77d367d7c9d2bc1d628094e4f7849f
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2021-10-15 17:21:08 +0300

    #558 Add ability to use offsets in `HoversUsingActionsAttribute`

--- src/Atata/Attributes/Behaviors/Hover/HoversUsingActionsAttribute.cs
+++ src/Atata/Attributes/Behaviors/Hover/HoversUsingActionsAttribute.cs
@@ -4,12 +4,34 @@ using OpenQA.Selenium.Interactions;
 namespace Atata
 {
     /// <summary>
-    /// Represents the behavior for control hovering using WebDriver's <see cref="Actions"/>.
-    /// Performs <see cref="Actions.MoveToElement(IWebElement)"/> action.
+    /// Represents the behavior for control hovering by using one of actions:
+    /// <see cref="Actions.MoveToElement(IWebElement)"/> or <see cref="Actions.MoveToElement(IWebElement, int, int, MoveToElementOffsetOrigin)"/>.
     /// </summary>
     public class HoversUsingActionsAttribute : HoverBehaviorAttribute
     {
-        public override void Execute<TOwner>(IUIComponent<TOwner> component) =>
-            component.Owner.Driver.Perform(a => a.MoveToElement(component.Scope));
+        /// <summary>
+        /// Gets or sets the kind of the offset.
+        /// The default value is <see cref="UIComponentOffsetKind.FromCenterInPercents"/>.
+        /// </summary>
+        public UIComponentOffsetKind OffsetKind { get; set; }
+
+        /// <summary>
+        /// Gets or sets the horizontal offset to which to move the mouse.
+        /// </summary>
+        public int OffsetX { get; set; }
+
+        /// <summary>
+        /// Gets or sets the vertical offset to which to move the mouse.
+        /// </summary>
+        public int OffsetY { get; set; }
+
+        /// <inheritdoc/>
+        public override void Execute<TOwner>(IUIComponent<TOwner> component)
+        {
+            var scopeElement = component.Scope;
+
+            component.Owner.Driver.Perform(
+                x => x.MoveToElement(scopeElement, OffsetX, OffsetY, OffsetKind));
+        }
     }
 }
