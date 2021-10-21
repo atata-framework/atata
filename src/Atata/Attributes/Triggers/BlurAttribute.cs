8131db50cc4977d72bb63c04d75ba885fac3c05f
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2021-10-21 11:03:29 +0300

    #565 Add `BlurAttribute` trigger

---
+++ src/Atata/Attributes/Triggers/BlurAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Atata
+{
+    /// <summary>
+    /// Indicates that the control blurring (removing focus) should be performed on the specified event.
+    /// By default occurs after set.
+    /// </summary>
+    public class BlurAttribute : TriggerAttribute
+    {
+        public BlurAttribute(
+            TriggerEvents on = TriggerEvents.AfterSet,
+            TriggerPriority priority = TriggerPriority.Medium)
+            : base(on, priority)
+        {
+        }
+
+        protected internal override void Execute<TOwner>(TriggerContext<TOwner> context)
+        {
+            if (context.Component is Control<TOwner> componentAsControl)
+            {
+                if (context.Event != TriggerEvents.BeforeBlur && context.Event != TriggerEvents.AfterBlur)
+                    componentAsControl.Blur();
+            }
+            else
+            {
+                throw new InvalidOperationException($"{nameof(BlurAttribute)} trigger can be executed only against control. But was: {context.Component.GetType().FullName}.");
+            }
+        }
+    }
+}
