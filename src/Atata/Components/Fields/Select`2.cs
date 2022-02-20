ff7d547790b8d603d3e3e4e91d65076c76db5d45
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-02-20 20:35:03 +0200

    Rename `Select<T, TOwner>` to `Select<TValue, TOwner>`

--- src/Atata/Components/Fields/Select`2.cs
+++ src/Atata/Components/Fields/Select`2.cs
@@ -9,24 +9,24 @@
     /// <see cref="SelectsOptionByLabelAttributeAttribute"/> and <see cref="SelectsOptionByAttributeAttribute"/>.
     /// Default option selection is performed by text using <see cref="SelectsOptionByTextAttribute"/>.
     /// </summary>
-    /// <typeparam name="T">The type of the data.</typeparam>
+    /// <typeparam name="TValue">The type of the control's value.</typeparam>
     /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
     [ControlDefinition("select", IgnoreNameEndings = "Select", ComponentTypeName = "select")]
     [FindByLabel]
     [SelectsOptionByText]
-    public class Select<T, TOwner> : EditableField<T, TOwner>
+    public class Select<TValue, TOwner> : EditableField<TValue, TOwner>
         where TOwner : PageObject<TOwner>
     {
         /// <summary>
         /// Gets the options' <see cref="ControlList{TItem, TOwner}"/> instance.
         /// </summary>
         [TraceLog]
-        public ControlList<Option<T, TOwner>, TOwner> Options { get; private set; }
+        public ControlList<Option<TValue, TOwner>, TOwner> Options { get; private set; }
 
         /// <summary>
         /// Gets the selected option.
         /// </summary>
-        public Option<T, TOwner> SelectedOption => Options[x => x.IsSelected];
+        public Option<TValue, TOwner> SelectedOption => Options[x => x.IsSelected];
 
         /// <summary>
         /// Gets the <see cref="ValueProvider{TValue, TOwner}"/> of the index of the selected option.
@@ -41,10 +41,10 @@
         protected internal SelectOptionBehaviorAttribute SelectOptionBehavior =>
             Metadata.Get<SelectOptionBehaviorAttribute>();
 
-        protected override T GetValue() =>
+        protected override TValue GetValue() =>
             SelectedOption.Value;
 
-        protected override void SetValue(T value)
+        protected override void SetValue(TValue value)
         {
             var option = GetOption(value);
             option.Select();
@@ -57,8 +57,8 @@
         /// Gets the option by the associated value.
         /// </summary>
         /// <param name="value">The value associated with the option.</param>
-        /// <returns>The <see cref="Option{T, TOwner}"/> instance.</returns>
-        public Option<T, TOwner> GetOption(T value)
+        /// <returns>The <see cref="Option{TValue, TOwner}"/> instance.</returns>
+        public Option<TValue, TOwner> GetOption(TValue value)
         {
             string valueAsString = ConvertValueToString(value);
             string xPathCondition = SelectOptionBehavior.FormatOptionXPathCondition(valueAsString);
