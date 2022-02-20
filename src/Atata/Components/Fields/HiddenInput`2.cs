b8f5c868e677e3393edbcf79e8cc1b3eaf2db170
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-02-20 20:21:10 +0200

    Rename `HiddenInput<T, TOwner>` to `HiddenInput<TValue, TOwner>`

--- src/Atata/Components/Fields/HiddenInput`2.cs
+++ src/Atata/Components/Fields/HiddenInput`2.cs
@@ -4,11 +4,11 @@
     /// Represents the hidden input control (<c>&lt;input type="hidden"&gt;</c>).
     /// Default search finds the first occurring <c>&lt;input type="hidden"&gt;</c> element.
     /// </summary>
-    /// <typeparam name="T">The type of the control's data.</typeparam>
+    /// <typeparam name="TValue">The type of the control's value.</typeparam>
     /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
     [ControlDefinition("input[@type='hidden']", Visibility = Visibility.Hidden, ComponentTypeName = "hidden input")]
     [FindFirst]
-    public class HiddenInput<T, TOwner> : Input<T, TOwner>
+    public class HiddenInput<TValue, TOwner> : Input<TValue, TOwner>
         where TOwner : PageObject<TOwner>
     {
     }
