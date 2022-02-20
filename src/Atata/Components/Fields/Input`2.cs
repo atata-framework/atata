ddc38018b9ed3f3fa1bbe8bf87f9a88f6fba8085
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-02-20 20:22:59 +0200

    Rename `Input<T, TOwner>` to `Input<TValue, TOwner>`

--- src/Atata/Components/Fields/Input`2.cs
+++ src/Atata/Components/Fields/Input`2.cs
@@ -4,11 +4,11 @@
     /// Represents the input control (<c>&lt;input&gt;</c>).
     /// Default search is performed by the label.
     /// </summary>
-    /// <typeparam name="T">The type of the control's data.</typeparam>
+    /// <typeparam name="TValue">The type of the control's value.</typeparam>
     /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
     [ControlDefinition("input[not(@type) or (@type!='button' and @type!='submit' and @type!='reset')]", ComponentTypeName = "input")]
     [FindByLabel]
-    public class Input<T, TOwner> : EditableTextField<T, TOwner>
+    public class Input<TValue, TOwner> : EditableTextField<TValue, TOwner>
         where TOwner : PageObject<TOwner>
     {
     }
