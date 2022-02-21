7e2732c57c941ad72816ad56a01ce57df1caa2a3
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-02-21 17:35:30 +0200

    Rename `DisposableSubject<T>` to `DisposableSubject<TObject>`

--- src/Atata/DataProvision/DisposableSubjectObjectExtensions.cs
+++ src/Atata/DataProvision/DisposableSubjectObjectExtensions.cs
@@ -3,50 +3,50 @@
 namespace Atata
 {
     /// <summary>
-    /// Provides a set of object extension methods that wrap the <see cref="IDisposable"/> object with the <see cref="DisposableSubject{T}"/> class.
+    /// Provides a set of object extension methods that wrap the <see cref="IDisposable"/> object with the <see cref="DisposableSubject{TObject}"/> class.
     /// </summary>
     public static class DisposableSubjectObjectExtensions
     {
         /// <summary>
-        /// Creates a new <see cref="DisposableSubject{T}"/> instance that wraps the <paramref name="source"/> with the <c>"sut"</c> provider name.
+        /// Creates a new <see cref="DisposableSubject{TObject}"/> instance that wraps the <paramref name="source"/> with the <c>"sut"</c> provider name.
         /// </summary>
         /// <typeparam name="T">The type of the source.</typeparam>
-        /// <param name="source">The instance to wrap with <see cref="DisposableSubject{T}"/>.</param>
-        /// <returns>A new <see cref="DisposableSubject{T}"/> instance.</returns>
+        /// <param name="source">The instance to wrap with <see cref="DisposableSubject{TObject}"/>.</param>
+        /// <returns>A new <see cref="DisposableSubject{TObject}"/> instance.</returns>
         public static DisposableSubject<T> ToSutDisposableSubject<T>(this T source)
             where T : IDisposable
             =>
             source.ToDisposableSubject("sut");
 
         /// <summary>
-        /// Creates a new <see cref="DisposableSubject{T}"/> instance that wraps the <paramref name="source"/> with the <c>"result"</c> provider name.
+        /// Creates a new <see cref="DisposableSubject{TObject}"/> instance that wraps the <paramref name="source"/> with the <c>"result"</c> provider name.
         /// </summary>
         /// <typeparam name="T">The type of the source.</typeparam>
-        /// <param name="source">The instance to wrap with <see cref="DisposableSubject{T}"/>.</param>
-        /// <returns>A new <see cref="DisposableSubject{T}"/> instance.</returns>
+        /// <param name="source">The instance to wrap with <see cref="DisposableSubject{TObject}"/>.</param>
+        /// <returns>A new <see cref="DisposableSubject{TObject}"/> instance.</returns>
         public static DisposableSubject<T> ToResultDisposableSubject<T>(this T source)
             where T : IDisposable
             =>
             source.ToDisposableSubject("result");
 
         /// <summary>
-        /// Creates a new <see cref="DisposableSubject{T}"/> instance that wraps the <paramref name="source"/> with the default provider name.
+        /// Creates a new <see cref="DisposableSubject{TObject}"/> instance that wraps the <paramref name="source"/> with the default provider name.
         /// </summary>
         /// <typeparam name="T">The type of the source.</typeparam>
-        /// <param name="source">The instance to wrap with <see cref="DisposableSubject{T}"/>.</param>
-        /// <returns>A new <see cref="DisposableSubject{T}"/> instance.</returns>
+        /// <param name="source">The instance to wrap with <see cref="DisposableSubject{TObject}"/>.</param>
+        /// <returns>A new <see cref="DisposableSubject{TObject}"/> instance.</returns>
         public static DisposableSubject<T> ToDisposableSubject<T>(this T source)
             where T : IDisposable
             =>
             new DisposableSubject<T>(source);
 
         /// <summary>
-        /// Creates a new <see cref="DisposableSubject{T}"/> instance that wraps the <paramref name="source"/> with the specified <paramref name="providerName"/>.
+        /// Creates a new <see cref="DisposableSubject{TObject}"/> instance that wraps the <paramref name="source"/> with the specified <paramref name="providerName"/>.
         /// </summary>
         /// <typeparam name="T">The type of the source.</typeparam>
-        /// <param name="source">The instance to wrap with <see cref="DisposableSubject{T}"/>.</param>
+        /// <param name="source">The instance to wrap with <see cref="DisposableSubject{TObject}"/>.</param>
         /// <param name="providerName">Name of the provider.</param>
-        /// <returns>A new <see cref="DisposableSubject{T}"/> instance.</returns>
+        /// <returns>A new <see cref="DisposableSubject{TObject}"/> instance.</returns>
         public static DisposableSubject<T> ToDisposableSubject<T>(this T source, string providerName)
             where T : IDisposable
             =>
--- src/Atata/DataProvision/DisposableSubject`1.cs
+++ src/Atata/DataProvision/DisposableSubject`1.cs
@@ -3,50 +3,50 @@
 namespace Atata
 {
     /// <summary>
-    /// Represents the test subject that wraps <typeparamref name="T"/> object, which should be <see cref="IDisposable"/>.
+    /// Represents the test subject that wraps <typeparamref name="TObject"/> object, which should be <see cref="IDisposable"/>.
     /// </summary>
-    /// <typeparam name="T">The type of the object.</typeparam>
-    public sealed class DisposableSubject<T> : SubjectBase<T, DisposableSubject<T>>, IDisposable
-        where T : IDisposable
+    /// <typeparam name="TObject">The type of the object.</typeparam>
+    public sealed class DisposableSubject<TObject> : SubjectBase<TObject, DisposableSubject<TObject>>, IDisposable
+        where TObject : IDisposable
     {
         private bool _isDisposed;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="DisposableSubject{T}"/> class
+        /// Initializes a new instance of the <see cref="DisposableSubject{TObject}"/> class
         /// with the default <c>"subject"</c> provider name that is taken from <see cref="Subject.DefaultSubjectName"/> property.
         /// </summary>
         /// <param name="source">The source object.</param>
-        public DisposableSubject(T source)
-            : this(new StaticObjectSource<T>(source))
+        public DisposableSubject(TObject source)
+            : this(new StaticObjectSource<TObject>(source))
         {
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="DisposableSubject{T}"/> class.
+        /// Initializes a new instance of the <see cref="DisposableSubject{TObject}"/> class.
         /// </summary>
         /// <param name="source">The source object.</param>
         /// <param name="providerName">Name of the provider.</param>
-        public DisposableSubject(T source, string providerName)
-            : this(new StaticObjectSource<T>(source), providerName)
+        public DisposableSubject(TObject source, string providerName)
+            : this(new StaticObjectSource<TObject>(source), providerName)
         {
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="DisposableSubject{T}"/> class
+        /// Initializes a new instance of the <see cref="DisposableSubject{TObject}"/> class
         /// with the default <c>"subject"</c> provider name that is taken from <see cref="Subject.DefaultSubjectName"/> property.
         /// </summary>
         /// <param name="objectSource">The object source.</param>
-        public DisposableSubject(IObjectSource<T> objectSource)
+        public DisposableSubject(IObjectSource<TObject> objectSource)
             : this(objectSource, Subject.DefaultSubjectName)
         {
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="DisposableSubject{T}"/> class.
+        /// Initializes a new instance of the <see cref="DisposableSubject{TObject}"/> class.
         /// </summary>
         /// <param name="objectSource">The object source.</param>
         /// <param name="providerName">Name of the provider.</param>
-        public DisposableSubject(IObjectSource<T> objectSource, string providerName)
+        public DisposableSubject(IObjectSource<TObject> objectSource, string providerName)
             : base(objectSource, providerName)
         {
         }
