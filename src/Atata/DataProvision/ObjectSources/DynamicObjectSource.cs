ef269996982e161ecf70a2a30463bfbf64499318
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-02-21 12:25:34 +0200

    Refactor `DynamicObjectSource`

--- src/Atata/DataProvision/ObjectSources/DynamicObjectSource.cs
+++ src/Atata/DataProvision/ObjectSources/DynamicObjectSource.cs
@@ -3,17 +3,17 @@
 namespace Atata
 {
     /// <summary>
-    /// Provides a set of methods for <see cref="DynamicObjectSource{TValue}"/> creation.
+    /// Provides a set of methods for <see cref="DynamicObjectSource{TObject}"/> creation.
     /// </summary>
     public static class DynamicObjectSource
     {
         /// <summary>
-        /// Creates a <see cref="DynamicObjectSource{TValue}"/> for the specified <paramref name="valueGetFunction"/>.
+        /// Creates a <see cref="DynamicObjectSource{TObject}"/> for the specified <paramref name="objectGetFunction"/>.
         /// </summary>
-        /// <typeparam name="TValue">The type of the value.</typeparam>
-        /// <param name="valueGetFunction">The value get function.</param>
+        /// <typeparam name="TObject">The type of the object.</typeparam>
+        /// <param name="objectGetFunction">The object get function.</param>
         /// <returns>An instance of <see cref="DynamicObjectSource{TValue}"/>.</returns>
-        public static DynamicObjectSource<TValue> Create<TValue>(Func<TValue> valueGetFunction) =>
-            new DynamicObjectSource<TValue>(valueGetFunction);
+        public static DynamicObjectSource<TObject> Create<TObject>(Func<TObject> objectGetFunction) =>
+            new DynamicObjectSource<TObject>(objectGetFunction);
     }
 }
