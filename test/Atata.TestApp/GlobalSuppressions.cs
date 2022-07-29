5490f81fa4e34290a207020a9ee2e17342d13379
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-07-29 15:52:11 +0200

    Update `Program` to ASP.NET Core 6 approach and delete `Startup` of Atata.TestApp project

--- test/Atata.TestApp/GlobalSuppressions.cs
+++ test/Atata.TestApp/GlobalSuppressions.cs
@@ -1,8 +1,3 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 
-#pragma warning disable S103 // Lines should not be too long
-
-[assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>", Scope = "member", Target = "~M:Atata.TestApp.Startup.ConfigureServices(Microsoft.Extensions.DependencyInjection.IServiceCollection)")]
-[assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>", Scope = "member", Target = "~M:Atata.TestApp.Startup.Configure(Microsoft.AspNetCore.Builder.IApplicationBuilder)")]
-
-#pragma warning restore S103 // Lines should not be too long
+[assembly: SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "<Pending>", Scope = "member", Target = "~M:Atata.TestApp.Program.Main(System.String[])")]
--- test/Atata.TestApp/Program.cs
+++ test/Atata.TestApp/Program.cs
@@ -1,20 +1,31 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Atata.TestApp
 {
     public static class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        public static void Main(string[] args) =>
+            CreateWebApplication(new WebApplicationOptions { Args = args })
+                .Run();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static WebApplication CreateWebApplication(WebApplicationOptions options, Action<WebApplicationBuilder> builderConfiguration = null)
+        {
+            var builder = WebApplication.CreateBuilder(options);
+
+            builder.Services.AddRazorPages();
+            builderConfiguration?.Invoke(builder);
+
+            var app = builder.Build();
+
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.MapRazorPages();
+
+            return app;
+        }
     }
 }
