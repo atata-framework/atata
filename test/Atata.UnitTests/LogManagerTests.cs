a87901c5a22163670c7e4efc9c6f0a7a22a5de68
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-07-30 19:59:42 +0200

    Move `LogManagerTests` to Atata.UnitTests project

---
+++ test/Atata.UnitTests/LogManagerTests.cs
@@ -0,0 +1,25 @@
+﻿namespace Atata.UnitTests;
+
+[TestFixture]
+public class LogManagerTests
+{
+    private LogManager _sut;
+
+    [SetUp]
+    public void SetUp() =>
+        _sut = new LogManager(new BasicLogEventInfoFactory());
+
+    [Test]
+    public void AddSecretStringsToMask()
+    {
+        var logConsumerMock = new Mock<ILogConsumer>();
+        _sut.Use(new LogConsumerConfiguration(logConsumerMock.Object));
+
+        _sut.AddSecretStringsToMask(
+            new[] { new SecretStringToMask("abc123", "***") });
+
+        _sut.Info(@"Set ""abc123"" to something");
+
+        logConsumerMock.Verify(x => x.Log(It.Is<LogEventInfo>(eventInfo => eventInfo.Message == @"Set ""***"" to something")), Times.Once);
+    }
+}
