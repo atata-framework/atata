efaa6c12694f18901d5287ed5b7e201c985c1e05
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-07-30 13:29:36 +0200

    Move `StaticSubjectTests` to Atata.UnitTests project

---
+++ test/Atata.UnitTests/DataProvision/StaticSubjectTests.cs
@@ -0,0 +1,77 @@
+﻿namespace Atata.UnitTests.DataProvision;
+
+[TestFixture]
+public static class StaticSubjectTests
+{
+    [TestFixture]
+    public static class ResultOf
+    {
+        [Test]
+        public static void ProviderName() =>
+            Subject.ResultOf(() => TestClass.GetEntity(10))
+                .ProviderName.Should().Be("StaticSubjectTests.TestClass.GetEntity(10) => result");
+
+        [Test]
+        public static void Returns() =>
+            Subject.ResultOf(() => TestClass.GetEntity(10))
+                .ValueOf(x => x.Id).Should.Equal(10)
+                .ValueOf(x => x.Name).Should.BeNull();
+
+        [Test]
+        public static void Throws()
+        {
+            var result = Subject.ResultOf(() => TestClass.GetEntity(null));
+
+            Assert.Throws<ArgumentNullException>(() =>
+                _ = result.Object);
+        }
+    }
+
+    [TestFixture]
+    public static class SubjectOf
+    {
+        [Test]
+        public static void ProviderName() =>
+            Subject.SubjectOf(() => TestClass.GetEntity(10))
+                .ProviderName.Should().Be("StaticSubjectTests.TestClass.GetEntity(10)");
+    }
+
+    [TestFixture]
+    public static class Invoking
+    {
+        [Test]
+        public static void ProviderName() =>
+            Subject.Invoking(() => TestClass.GetEntity(10))
+                .ProviderName.Should().Be("StaticSubjectTests.TestClass.GetEntity(10)");
+
+        [Test]
+        public static void Function_Should_Throw() =>
+            Subject.Invoking(() => TestClass.GetEntity(null))
+                .Should.Throw<ArgumentNullException>();
+
+        [Test]
+        public static void Function_Should_Not_Throw() =>
+            Subject.Invoking(() => TestClass.GetEntity(10))
+                .Should.Not.Throw();
+    }
+
+    public static class TestClass
+    {
+        public static TestEntity GetEntity(int id) =>
+            new() { Id = id };
+
+        public static TestEntity GetEntity(string name)
+        {
+            if (name is null)
+                throw new ArgumentNullException(nameof(name));
+            return new TestEntity { Name = name };
+        }
+    }
+
+    public class TestEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
