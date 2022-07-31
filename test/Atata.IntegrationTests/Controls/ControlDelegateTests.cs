e1fcae458a7184d8ea8006bcd6ec31f300c0c95e
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-07-31 19:04:40 +0200

    Refactor `ClickableTests` as `ControlDelegateTests` of Atata.IntegrationTests project

---
+++ test/Atata.IntegrationTests/Controls/ControlDelegateTests.cs
@@ -0,0 +1,52 @@
+﻿namespace Atata.IntegrationTests.Controls;
+
+[TestFixture]
+public class ControlDelegateTests : UITestFixture
+{
+    [Test]
+    public void WithoutNavigation()
+    {
+        Go.To<BasicControlsPage>()
+            .RawButton.Should().Exist()
+            .RawButton.Should().BeEnabled()
+            .RawButton.Content().Should.Equal("Raw Button")
+            .RawButton()
+            .InputButton.Should().Exist()
+            .InputButton()
+            .Do(_ => _.LinkButton, x =>
+            {
+                x.Should().Exist();
+                x.Content().Should.Equal("Link Button");
+                x();
+            })
+            .DivButton.Should().Exist()
+            .DivButton.Click()
+            .DisabledButton.Should().Exist()
+            .DisabledButton.Should().BeDisabled()
+            .MissingButton.Should().Not.Exist();
+    }
+
+    [Test]
+    public void WithNavigaton()
+    {
+        Go.To<BasicControlsPage>()
+            .GoToButton.Should().Exist()
+            .GoToButton.Should().BeEnabled()
+            .GoToButton();
+
+        Go.To<BasicControlsPage>()
+            .GoToInputButton.Should().Exist()
+            .GoToInputButton.Should().BeEnabled()
+            .GoToInputButton();
+
+        Go.To<BasicControlsPage>()
+            .GoToLink.Should().Exist()
+            .GoToLink.Should().BeEnabled()
+            .GoToLink();
+
+        Go.To<BasicControlsPage>()
+            .GoToDivButton.Should().Exist()
+            .GoToDivButton.Should().BeEnabled()
+            .GoToDivButton();
+    }
+}
