b3825c4b1298dfc612addac59dd0c6c86d870cf9
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-07-31 18:40:31 +0200

    Refactor ``CheckBoxTests of Atata.IntegrationTests project

---
+++ test/Atata.IntegrationTests/Controls/CheckBoxTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Atata.IntegrationTests.Controls;
+
+public class CheckBoxTests : UITestFixture
+{
+    private CheckBoxListPage _page;
+
+    protected override void OnSetUp() =>
+        _page = Go.To<CheckBoxListPage>();
+
+    [Test]
+    public void Interact()
+    {
+        var sut = _page.OptionA;
+
+        sut.Should.BeFalse();
+        sut.Should.Not.BeChecked();
+        sut.Set(true);
+        sut.Should.BeTrue();
+        sut.Should.BeChecked();
+        sut.Uncheck();
+        sut.Should.Not.BeTrue();
+        sut.Should.BeUnchecked();
+        sut.Check();
+        sut.Should.BeChecked();
+    }
+}
