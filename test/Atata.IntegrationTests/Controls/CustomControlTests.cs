af67dbebfca36e91e496e2db427b7c694613857e
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-07-31 20:58:44 +0200

    Refactor ``CustomControlTests of Atata.IntegrationTests project

---
+++ test/Atata.IntegrationTests/Controls/CustomControlTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Atata.IntegrationTests.Controls;
+
+public class CustomControlTests : UITestFixture
+{
+    [Test]
+    public void CustomDatePicker_WithFormat()
+    {
+        var sut = Go.To<InputPage>().DatePicker;
+
+        sut.Should.BeNull();
+
+        DateTime value = new(2018, 7, 11);
+        sut.Set(value);
+        sut.Should.Equal(value);
+
+        sut.Set(null);
+        sut.Should.BeNull();
+    }
+}
