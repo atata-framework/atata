daf0b3838986b722678aa597d442faaf1b4f5eeb
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-07-31 14:36:10 +0200

    Move `UriUtilsTests` to Atata.UnitTests project

---
+++ test/Atata.UnitTests/Utils/UriUtilsTests.cs
@@ -0,0 +1,25 @@
+﻿namespace Atata.UnitTests;
+
+[TestFixture]
+public class UriUtilsTests
+{
+    [TestCase("http://something.com", true)]
+    [TestCase("https://something.com", true)]
+    [TestCase("ftp://something.com", true)]
+    [TestCase("custom://something.com", true)]
+    [TestCase("http:/something.com", false)]
+    [TestCase("//something", false)]
+    [TestCase("/something", false)]
+    [TestCase("something", false)]
+    [TestCase(null, false)]
+    public void TryCreateAbsoluteUrl(string url, bool isAbsolute)
+    {
+        var isActuallyAbsolute = UriUtils.TryCreateAbsoluteUrl(url, out Uri result);
+        isActuallyAbsolute.Should().Be(isAbsolute);
+
+        if (isAbsolute)
+            result.AbsoluteUri.Should().StartWith(url);
+        else
+            result.Should().BeNull();
+    }
+}
