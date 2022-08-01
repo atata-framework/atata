b0e9cab2d9fd25570ced6fad150e1a618efd4d32
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-08-01 17:39:27 +0200

    Refactor `ImageTests` of Atata.IntegrationTests project

---
+++ test/Atata.IntegrationTests/Controls/ImageTests.cs
@@ -0,0 +1,27 @@
+﻿namespace Atata.IntegrationTests.Controls;
+
+public class ImageTests : UITestFixture
+{
+    private ImagePage _page;
+
+    protected override void OnSetUp() =>
+        _page = Go.To<ImagePage>();
+
+    [Test]
+    public void WhenLoaded()
+    {
+        var sut = _page.LoadedImage;
+
+        sut.Source.Should.EndWith("/images/350x150.png");
+        sut.IsLoaded.Should.BeTrue();
+    }
+
+    [Test]
+    public void WhenNotLoaded()
+    {
+        var sut = _page.NotLoadedImage;
+
+        sut.Source.Should.EndWith("/images/missing.png");
+        sut.IsLoaded.Should.BeFalse();
+    }
+}
