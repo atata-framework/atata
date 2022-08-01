07b7bb18fb9b65d87bc5c0dc2433a31510c537d8
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-08-01 17:49:45 +0200

    Refactor `LabelTests` of Atata.IntegrationTests project

--- test/Atata.IntegrationTests/Controls/LabelTests.cs
+++ test/Atata.IntegrationTests/Controls/LabelTests.cs
@@ -1,25 +1,25 @@
-﻿using NUnit.Framework;
-
-namespace Atata.IntegrationTests
+﻿namespace Atata.IntegrationTests.Controls
 {
     public class LabelTests : UITestFixture
     {
         private LabelPage _page;
 
-        protected override void OnSetUp()
-        {
+        protected override void OnSetUp() =>
             _page = Go.To<LabelPage>();
-        }
 
         [Test]
-        public void Label()
+        public void Default()
         {
             _page.FirstNameLabel.Should.Equal("First Name");
             _page.FirstNameLabel.Attributes.For.Should.Equal("first-name");
-
-            _page.LastNameLabel.Should.Equal("Last Name");
-
-            _page.LastNameByForLabel.Should.Equal("Last Name*");
         }
+
+        [Test]
+        public void WithFindByAttributeAttribute() =>
+            _page.LastNameByForLabel.Should.Equal("Last Name*");
+
+        [Test]
+        public void WithFormatAttribute() =>
+            _page.LastNameLabel.Should.Equal("Last Name");
     }
 }
