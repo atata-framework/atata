f7164b2eae043e4e5bad322603444f1505467479
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-08-01 18:00:10 +0200

    Refactor `RadioButtonTests` of Atata.IntegrationTests project

---
+++ test/Atata.IntegrationTests/Controls/RadioButtonTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Atata.IntegrationTests.Controls;
+
+public class RadioButtonTests : UITestFixture
+{
+    private RadioButtonListPage _page;
+
+    protected override void OnSetUp() =>
+        _page = Go.To<RadioButtonListPage>();
+
+    [Test]
+    public void Interact()
+    {
+        _page
+            .OptionA.Should.BeFalse()
+            .OptionA.Should.Not.BeChecked()
+            .OptionA.Check()
+            .OptionA.Should.BeTrue()
+            .OptionA.Should.BeChecked()
+            .OptionB.Check()
+            .OptionA.Should.Not.BeTrue()
+            .OptionA.Should.Not.BeChecked()
+            .OptionB.Should.Not.BeUnchecked()
+            .OptionA.Check()
+            .OptionA.Should.BeChecked();
+    }
+}
