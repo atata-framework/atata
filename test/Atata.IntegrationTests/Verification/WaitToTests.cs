25d6b09e9793e7af09fdfe2561abf8a5c6c131a6
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-08-01 15:19:34 +0200

    Refactor ``WaitToTests of Atata.IntegrationTests project

---
+++ test/Atata.IntegrationTests/Verification/WaitToTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework.Internal;
+
+namespace Atata.IntegrationTests.Verification;
+
+public class WaitToTests : UITestFixture
+{
+    private StubPage _page;
+
+    protected override void OnSetUp() =>
+        _page = Go.To<StubPage>();
+
+    [Test]
+    public void NoFailure()
+    {
+        var waitTo = _page.IsTrue.WaitTo;
+
+        waitTo.BeTrue();
+    }
+
+    [Test]
+    public void NoFailure_WithRetry()
+    {
+        var waitTo = _page.IsTrueInASecond.WaitTo;
+
+        waitTo.BeTrue();
+    }
+
+    [Test]
+    public void Positive_Failure()
+    {
+        var waitTo = _page.IsTrue.WaitTo.AtOnce;
+
+        var exception = Assert.Throws<TimeoutException>(() =>
+            waitTo.BeFalse());
+
+        Assert.That(exception.Message, Does.StartWith("Timed out waiting for "));
+        Assert.That(exception.InnerException, Is.Null);
+    }
+
+    [Test]
+    public void Negative_Failure()
+    {
+        var waitTo = _page.IsTrue.WaitTo.Not.AtOnce;
+
+        var exception = Assert.Throws<TimeoutException>(() =>
+            waitTo.BeTrue());
+
+        Assert.That(exception.Message, Does.StartWith("Timed out waiting for "));
+        Assert.That(exception.InnerException, Is.Null);
+    }
+}
