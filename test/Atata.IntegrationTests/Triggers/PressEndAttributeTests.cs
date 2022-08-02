8776c2fae357f115b4c614d5bc9092a1572bf66f
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-08-02 14:15:01 +0200

    Refactor `PressTriggerTests` of Atata.IntegrationTests project as `PressHomeAttributeTests` and `PressEndAttributeTests`

---
+++ test/Atata.IntegrationTests/Triggers/PressEndAttributeTests.cs
@@ -0,0 +1,16 @@
+﻿namespace Atata.IntegrationTests.Triggers;
+
+public class PressEndAttributeTests : UITestFixture
+{
+    [Test]
+    public void Execute()
+    {
+        var sut = Go.To<ScrollablePage>().BottomText;
+
+        sut.Should.Not.BeVisibleInViewPort();
+        sut.Metadata.Add(new PressEndAttribute(TriggerEvents.BeforeGet));
+
+        sut.Get(out _);
+        sut.Should.BeVisibleInViewPort();
+    }
+}
---
+++ test/Atata.IntegrationTests/Triggers/PressHomeAttributeTests.cs
@@ -0,0 +1,18 @@
+﻿namespace Atata.IntegrationTests.Triggers;
+
+public class PressHomeAttributeTests : UITestFixture
+{
+    [Test]
+    public void Execute()
+    {
+        var sut = Go.To<ScrollablePage>()
+            .Press(Keys.End)
+            .TopText;
+
+        sut.Should.Not.BeVisibleInViewPort();
+        sut.Metadata.Add(new PressHomeAttribute(TriggerEvents.BeforeGet));
+
+        sut.Get(out _);
+        sut.Should.BeVisibleInViewPort();
+    }
+}
