7500499399527d44d1c57f7300c0e873a1972d97
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-08-03 19:49:06 +0200

    Refactor `TriggerTests` of Atata.IntegrationTests project as `TriggerTests` and `InvokeMethodAttributeTests`

---
+++ test/Atata.IntegrationTests/Triggers/InvokeMethodAttributeTests.cs
@@ -0,0 +1,22 @@
+﻿namespace Atata.IntegrationTests.Triggers;
+
+public class InvokeMethodAttributeTests : UITestFixture
+{
+    private TriggersPage _page;
+
+    protected override void OnSetUp() =>
+        _page = Go.To<TriggersPage>();
+
+    [Test]
+    public void Execute_AtProperty()
+    {
+        _page.Perform.Click();
+
+        Assert.That(_page.IsBeforePerformInvoked, Is.True);
+        Assert.That(_page.IsAfterPerformInvoked, Is.True);
+    }
+
+    [Test]
+    public void Execute_AtComponent() =>
+        Assert.That(TriggersPage.IsOnInitInvoked, Is.True);
+}
