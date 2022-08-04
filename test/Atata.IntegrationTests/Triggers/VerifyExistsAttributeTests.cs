acecf3022981767139dbabf99793e316cadc1180
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-08-04 13:56:13 +0200

    Refactor `VerifyExistsTests` of Atata.IntegrationTests project as `VerifyExistsAttributeTests`

---
+++ test/Atata.IntegrationTests/Triggers/VerifyExistsAttributeTests.cs
@@ -0,0 +1,21 @@
+﻿namespace Atata.IntegrationTests.Triggers;
+
+public class VerifyExistsAttributeTests : UITestFixture
+{
+    [Test]
+    public void Execute_WhenOnInit() =>
+        Go.To(new WaitingOnInitPage { OnInitWaitKind = WaitingOnInitPage.WaitKind.VerifyExists })
+            .VerifyContentBlockIsLoaded();
+
+    [Test]
+    public void Execute_WhenOnInit_AfterGo() =>
+        Go.To(new WaitingPage { NavigatingPageWaitKind = WaitingOnInitPage.WaitKind.VerifyExists })
+            .GoToWaitingOnInitPage.ClickAndGo()
+            .VerifyContentBlockIsLoaded();
+
+    [Test]
+    public void Execute_WhenOnInit_AfterDelayedGo() =>
+        Go.To(new WaitingPage { NavigatingPageWaitKind = WaitingOnInitPage.WaitKind.VerifyExists })
+            .WaitAndGoToWaitingOnInitPage.ClickAndGo()
+            .VerifyContentBlockIsLoaded();
+}
