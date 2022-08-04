c810abbf516a3b05c5c3ff78567089c3916f5e05
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-08-04 14:05:10 +0200

    Refactor `WaitForScriptTests` of Atata.IntegrationTests project as `WaitForScriptAttributeTests`

---
+++ test/Atata.IntegrationTests/Triggers/WaitForScriptAttributeTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Atata.IntegrationTests.Triggers;
+
+public class WaitForScriptAttributeTests : UITestFixture
+{
+    private WaitingPage _page;
+
+    protected override void OnSetUp() =>
+        _page = Go.To<WaitingPage>();
+
+    [Test]
+    public void Execute() =>
+        _page
+            .ButtonWithSuccessfulScriptWait()
+            .ValueBlock.Should.AtOnce.Exist();
+
+    [Test]
+    public void Execute_WithTimeout()
+    {
+        using (StopwatchAsserter.WithinSeconds(1))
+            Assert.Throws<TimeoutException>(
+                () => _page.ButtonWithTimeoutScriptWait());
+    }
+}
