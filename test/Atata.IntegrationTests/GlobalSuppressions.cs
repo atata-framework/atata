43a9003aa7d401175b9ec004c11ceeae324b291a
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-08-08 16:30:07 +0200

    Refactor `RandomizationTests` of Atata.IntegrationTests project

--- test/Atata.IntegrationTests/GlobalSuppressions.cs
+++ test/Atata.IntegrationTests/GlobalSuppressions.cs
@@ -6,5 +6,6 @@
 [assembly: SuppressMessage("Major Code Smell", "S103:Lines should not be too long", Justification = "<Pending>", Scope = "member", Target = "~P:Atata.IntegrationTests.ListPage.ComplexHierarchicalOrderedListWithAnyVisibilityUsingControlDefinition")]
 [assembly: SuppressMessage("Major Code Smell", "S1144:Unused private types or members should be removed", Justification = "<Pending>", Scope = "member", Target = "~M:Atata.IntegrationTests.TriggersPage.WriteTriggerEventAttribute.Execute(Atata.TriggerContext{Atata.IntegrationTests.TriggersPage})")]
 [assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~M:Atata.IntegrationTests.TriggersPage.WriteTriggerEventAttribute.Execute(Atata.TriggerContext{Atata.IntegrationTests.TriggersPage})")]
+[assembly: SuppressMessage("Naming", "CA1720:Identifier contains type name", Justification = "<Pending>", Scope = "member", Target = "~M:Atata.IntegrationTests.RandomizationTests.Int")]
 
 #pragma warning restore S103 // Lines should not be too long
--- test/Atata.IntegrationTests/RandomizationTests.cs
+++ test/Atata.IntegrationTests/RandomizationTests.cs
@@ -1,202 +1,197 @@
-﻿using NUnit.Framework;
+﻿namespace Atata.IntegrationTests;
 
-namespace Atata.IntegrationTests
+public class RandomizationTests : UITestFixture
 {
-    public class RandomizationTests : UITestFixture
+    private const int MaxTriesNumber = 100;
+
+    private RandomizationPage _page;
+
+    protected override void OnSetUp() =>
+        _page = Go.To<RandomizationPage>();
+
+    [Test]
+    public void Enum_Single()
     {
-        private const int MaxTriesNumber = 100;
+        var control = _page.SimpleEnum;
 
-        private RandomizationPage _page;
+        control.SetRandom(out RandomizationPage.CheckBoxOptions value);
 
-        protected override void OnSetUp()
+        for (int i = 0; i < MaxTriesNumber; i++)
         {
-            _page = Go.To<RandomizationPage>();
+            control.SetRandom(out RandomizationPage.CheckBoxOptions newValue);
+            control.Should.Equal(newValue);
+            _page.CheckedItemsCount.Should.BeInRange(0, 1);
+
+            if (newValue != value)
+                return;
         }
 
-        [Test]
-        public void Randomization_Enum_Single()
+        Assert.Fail();
+    }
+
+    [Test]
+    public void Enum_Multiple()
+    {
+        var control = _page.MultipleEnums;
+
+        control.SetRandom(out RandomizationPage.CheckBoxOptions value);
+
+        for (int i = 0; i < MaxTriesNumber; i++)
         {
-            var control = _page.SimpleEnum;
+            control.SetRandom(out RandomizationPage.CheckBoxOptions newValue);
+            control.Should.Equal(newValue);
+            _page.CheckedItemsCount.Should.BeInRange(2, 4);
 
-            control.SetRandom(out RandomizationPage.CheckBoxOptions value);
-
-            for (int i = 0; i < MaxTriesNumber; i++)
-            {
-                control.SetRandom(out RandomizationPage.CheckBoxOptions newValue);
-                control.Should.Equal(newValue);
-                _page.CheckedItemsCount.Should.BeInRange(0, 1);
-
-                if (newValue != value)
-                    return;
-            }
-
-            Assert.Fail();
+            if (newValue != value)
+                return;
         }
 
-        [Test]
-        public void Randomization_Enum_Multiple()
+        Assert.Fail();
+    }
+
+    [Test]
+    public void Enum_MultipleWithExcluding()
+    {
+        var control = _page.MultipleEnumsExcludingNoneBDF;
+
+        control.SetRandom(out RandomizationPage.CheckBoxOptions value);
+
+        for (int i = 0; i < MaxTriesNumber; i++)
         {
-            var control = _page.MultipleEnums;
+            control.SetRandom(out RandomizationPage.CheckBoxOptions newValue);
+            control.Should.Equal(newValue);
+            _page.CheckedItemsCount.Should.Equal(2);
+            control.Should.Not.HaveChecked(
+                RandomizationPage.CheckBoxOptions.None |
+                RandomizationPage.CheckBoxOptions.OptionB |
+                RandomizationPage.CheckBoxOptions.OptionD |
+                RandomizationPage.CheckBoxOptions.OptionF);
 
-            control.SetRandom(out RandomizationPage.CheckBoxOptions value);
-
-            for (int i = 0; i < MaxTriesNumber; i++)
-            {
-                control.SetRandom(out RandomizationPage.CheckBoxOptions newValue);
-                control.Should.Equal(newValue);
-                _page.CheckedItemsCount.Should.BeInRange(2, 4);
-
-                if (newValue != value)
-                    return;
-            }
-
-            Assert.Fail();
+            if (newValue != value)
+                return;
         }
 
-        [Test]
-        public void Randomization_Enum_MultipleWithExcluding()
+        Assert.Fail();
+    }
+
+    [Test]
+    public void Enum_MultipleWithIncluding()
+    {
+        var control = _page.MultipleEnumsIncludingABDEF;
+
+        control.SetRandom(out RandomizationPage.CheckBoxOptions value);
+
+        for (int i = 0; i < MaxTriesNumber; i++)
         {
-            var control = _page.MultipleEnumsExcludingNoneBDF;
+            control.SetRandom(out RandomizationPage.CheckBoxOptions newValue);
+            control.Should.Equal(newValue);
+            _page.CheckedItemsCount.Should.Equal(3);
+            control.Should.Not.HaveChecked(
+                RandomizationPage.CheckBoxOptions.None |
+                RandomizationPage.CheckBoxOptions.OptionC);
 
-            control.SetRandom(out RandomizationPage.CheckBoxOptions value);
-
-            for (int i = 0; i < MaxTriesNumber; i++)
-            {
-                control.SetRandom(out RandomizationPage.CheckBoxOptions newValue);
-                control.Should.Equal(newValue);
-                _page.CheckedItemsCount.Should.Equal(2);
-                control.Should.Not.HaveChecked(
-                    RandomizationPage.CheckBoxOptions.None |
-                    RandomizationPage.CheckBoxOptions.OptionB |
-                    RandomizationPage.CheckBoxOptions.OptionD |
-                    RandomizationPage.CheckBoxOptions.OptionF);
-
-                if (newValue != value)
-                    return;
-            }
-
-            Assert.Fail();
+            if (newValue != value)
+                return;
         }
 
-        [Test]
-        public void Randomization_Enum_MultipleWithIncluding()
+        Assert.Fail();
+    }
+
+    [Test]
+    public void Enum_Nullable()
+    {
+        var control = _page.EnumSelect;
+
+        control.SetRandom(out RandomizationPage.SelectOption value);
+
+        for (int i = 0; i < MaxTriesNumber; i++)
         {
-            var control = _page.MultipleEnumsIncludingABDEF;
+            control.SetRandom(out RandomizationPage.SelectOption newValue);
+            control.Should.Equal(newValue);
 
-            control.SetRandom(out RandomizationPage.CheckBoxOptions value);
-
-            for (int i = 0; i < MaxTriesNumber; i++)
-            {
-                control.SetRandom(out RandomizationPage.CheckBoxOptions newValue);
-                control.Should.Equal(newValue);
-                _page.CheckedItemsCount.Should.Equal(3);
-                control.Should.Not.HaveChecked(
-                    RandomizationPage.CheckBoxOptions.None |
-                    RandomizationPage.CheckBoxOptions.OptionC);
-
-                if (newValue != value)
-                    return;
-            }
-
-            Assert.Fail();
+            if (newValue != value)
+                return;
         }
 
-        [Test]
-        public void Randomization_Enum_Nullable()
+        Assert.Fail();
+    }
+
+    [Test]
+    public void String_WithIncluding()
+    {
+        var control = _page.TextSelect;
+
+        control.SetRandom(out string value);
+
+        for (int i = 0; i < MaxTriesNumber; i++)
         {
-            var control = _page.EnumSelect;
+            control.SetRandom(out string newValue);
+            control.Should.Equal(newValue);
+            control.Should.Not.Equal("Option D");
 
-            control.SetRandom(out RandomizationPage.SelectOption value);
-
-            for (int i = 0; i < MaxTriesNumber; i++)
-            {
-                control.SetRandom(out RandomizationPage.SelectOption newValue);
-                control.Should.Equal(newValue);
-
-                if (newValue != value)
-                    return;
-            }
-
-            Assert.Fail();
+            if (newValue != value)
+                return;
         }
 
-        [Test]
-        public void Randomization_String_WithIncluding()
+        Assert.Fail();
+    }
+
+    [Test]
+    public void Int()
+    {
+        var control = _page.IntSelect;
+
+        control.SetRandom(out int value);
+
+        for (int i = 0; i < MaxTriesNumber; i++)
         {
-            var control = _page.TextSelect;
+            control.SetRandom(out int newValue);
+            control.Should.Equal(newValue);
+            control.Should.BeInRange(1, 2);
 
-            control.SetRandom(out string value);
-
-            for (int i = 0; i < MaxTriesNumber; i++)
-            {
-                control.SetRandom(out string newValue);
-                control.Should.Equal(newValue);
-                control.Should.Not.Equal("Option D");
-
-                if (newValue != value)
-                    return;
-            }
-
-            Assert.Fail();
+            if (newValue != value)
+                return;
         }
 
-        [Test]
-        public void Randomization_Int()
+        Assert.Fail();
+    }
+
+    [Test]
+    public void Int_WithIncluding()
+    {
+        var control = _page.IntSelectUsingInclude;
+
+        control.SetRandom(out int value);
+
+        for (int i = 0; i < MaxTriesNumber; i++)
         {
-            var control = _page.IntSelect;
+            control.SetRandom(out int newValue);
+            control.Should.Equal(newValue);
+            control.Should.BeLessOrEqual(3);
 
-            control.SetRandom(out int value);
-
-            for (int i = 0; i < MaxTriesNumber; i++)
-            {
-                control.SetRandom(out int newValue);
-                control.Should.Equal(newValue);
-                control.Should.BeInRange(1, 2);
-
-                if (newValue != value)
-                    return;
-            }
-
-            Assert.Fail();
+            if (newValue != value)
+                return;
         }
 
-        [Test]
-        public void Randomization_Int_WithIncluding()
+        Assert.Fail();
+    }
+
+    [Test]
+    public void Bool()
+    {
+        var control = _page.OptionA;
+
+        control.SetRandom(out bool value);
+
+        for (int i = 0; i < MaxTriesNumber; i++)
         {
-            var control = _page.IntSelectUsingInclude;
+            control.SetRandom(out bool newValue);
+            control.Should.Equal(newValue);
 
-            control.SetRandom(out int value);
-
-            for (int i = 0; i < MaxTriesNumber; i++)
-            {
-                control.SetRandom(out int newValue);
-                control.Should.Equal(newValue);
-                control.Should.BeLessOrEqual(3);
-
-                if (newValue != value)
-                    return;
-            }
-
-            Assert.Fail();
+            if (newValue != value)
+                return;
         }
 
-        [Test]
-        public void Randomization_Bool()
-        {
-            var control = _page.OptionA;
-
-            control.SetRandom(out bool value);
-
-            for (int i = 0; i < MaxTriesNumber; i++)
-            {
-                control.SetRandom(out bool newValue);
-                control.Should.Equal(newValue);
-
-                if (newValue != value)
-                    return;
-            }
-
-            Assert.Fail();
-        }
+        Assert.Fail();
     }
 }
