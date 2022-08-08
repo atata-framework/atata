00a395f6e74fd2d270c9fbe4661a30073b732f23
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-08-08 15:33:08 +0200

    Refactor `PageObjectTests` of Atata.IntegrationTests project

--- test/Atata.IntegrationTests/PageObjectTests.cs
+++ test/Atata.IntegrationTests/PageObjectTests.cs
@@ -1,164 +1,137 @@
-﻿using System;
-using NUnit.Framework;
-using OpenQA.Selenium;
+﻿namespace Atata.IntegrationTests;
 
-namespace Atata.IntegrationTests
+public class PageObjectTests : UITestFixture
 {
-    public class PageObjectTests : UITestFixture
+    [Test]
+    public void RefreshPage() =>
+        Go.To<BasicControlsPage>()
+            .ById.FirstName.Set("test")
+            .ById.FirstName.Should.Equal("test")
+            .RefreshPage()
+            .ById.FirstName.Should.BeEmpty();
+
+    [Test]
+    public void RefreshPageUntil() =>
+        Go.To<WaitingPage>()
+            .CurrentTime.Get(out TimeSpan? time)
+            .RefreshPageUntil(x => x.CurrentTime.Value > time.Value.Add(TimeSpan.FromSeconds(15)), timeout: 20, retryInterval: 2);
+
+    [Test]
+    public void RefreshPageUntil_WithTimeout()
     {
-        [Test]
-        public void PageObject_RefreshPage()
-        {
-            Go.To<BasicControlsPage>().
-                ById.FirstName.Set("test").
-                ById.FirstName.Should.Equal("test").
-                RefreshPage().
-                ById.FirstName.Should.BeEmpty();
-        }
+        var page = Go.To<WaitingPage>()
+            .CurrentTime.Get(out TimeSpan? time);
 
-        [Test]
-        public void PageObject_RefreshPageUntil()
-        {
-            Go.To<WaitingPage>().
-                CurrentTime.Get(out TimeSpan? time).
-                RefreshPageUntil(x => x.CurrentTime.Value > time.Value.Add(TimeSpan.FromSeconds(15)), timeout: 20, retryInterval: 2);
-        }
-
-        [Test]
-        public void PageObject_RefreshPageUntil_Timeout()
-        {
-            var page = Go.To<WaitingPage>().
-                CurrentTime.Get(out TimeSpan? time);
-
-            using (StopwatchAsserter.WithinSeconds(1))
-                Assert.Throws<TimeoutException>(() =>
-                    page.RefreshPageUntil(x => x.CurrentTime.Value > time.Value.Add(TimeSpan.FromSeconds(15)), timeout: 1));
-        }
-
-        [Test]
-        public void PageObject_IgnoreInit()
-        {
-            Assert.That(Go.To<BasicControlsPage>().IgnoredInput, Is.Null);
-        }
-
-        [Test]
-        public void PageObject_GoBack()
-        {
-            Go.To<GoTo1Page>().
-                GoTo2().
-                    GoBack<GoTo1Page>().
-                    Should.Exist();
-        }
-
-        [Test]
-        public void PageObject_GoBack_Fail()
-        {
-            var page2 = Go.To<GoTo1Page>().
-                GoTo2();
-
-            Assert.Throws<AssertionException>(
-                () => page2.GoBack<GoTo3Page>());
-        }
-
-        [Test]
-        public void PageObject_GoForward()
-        {
-            Go.To<GoTo1Page>().
-                GoTo2().
-                    GoBack<GoTo1Page>().
-                GoForward<GoTo2Page>().
-                    Should.Exist();
-        }
-
-        [Test]
-        public void PageObject_GoForward_Failed()
-        {
-            var page1 = Go.To<GoTo1Page>().
-                GoTo2().
-                    GoBack<GoTo1Page>();
-
-            Assert.Throws<AssertionException>(
-                () => page1.GoForward<GoTo3Page>());
-        }
-
-        [Test]
-        public void PageObject_Press()
-        {
-            Go.To<InputPage>().
-                TextInput.Focus().
-                Press("abc").
-                Press("d").
-                Press(Keys.Tab).
-                Press("e").
-                TextInput.Should.Equal("abcd");
-        }
-
-        [Test]
-        public void PageObject_ActiveControl()
-        {
-            Go.To<InputPage>().
-                TelInput.Set("123").
-                ActiveControl.Attributes.Value.Should.Equal("123").
-                IntTextInput.Focus().
-                ActiveControl.Attributes.Id.Should.Equal("int-text-input");
-        }
-
-        [Test]
-        public void PageObject_AggregateAssert()
-        {
-            AggregateAssertionException exception = Assert.Throws<AggregateAssertionException>(() =>
-            {
-                Go.To<StubPage>().
-                    AggregateAssert(x => x.
-                        IsTrue.Should.AtOnce.BeFalse().
-                        IsTrue.Should.AtOnce.BeTrue().
-                        IsTrue.Should.AtOnce.BeFalse());
-            });
-
-            Assert.That(exception.Results, Has.Count.EqualTo(2));
-        }
-
-        [Test]
-        public void PageObject_AggregateAssert_Component()
-        {
-            AggregateAssertionException exception = Assert.Throws<AggregateAssertionException>(() =>
-            {
-                Go.To<StubPage>().
-                    AggregateAssert(_ => _.IsTrue, x =>
-                    {
-                        x.Should.AtOnce.BeFalse();
-                        x.Should.AtOnce.BeTrue();
-                        x.Should.AtOnce.BeFalse();
-                    });
-            });
-
-            Assert.That(exception.Results, Has.Count.EqualTo(2));
-        }
-
-        [Test]
-        public void PageObject_PageSource()
-        {
-            Go.To<BasicControlsPage>().
-                PageSource.Should.ContainAll("<head>", "</body>", "<input", "<button");
-        }
-
-        [Test]
-        public void PageObject_ScrollDown()
-        {
-            Go.To<ScrollablePage>().
-                BottomText.Should.Not.BeVisibleInViewPort().
-                ScrollDown().
-                BottomText.Should.BeVisibleInViewPort();
-        }
-
-        [Test]
-        public void PageObject_ScrollUp()
-        {
-            Go.To<ScrollablePage>().
-                Press(Keys.End).
-                TopText.Should.Not.BeVisibleInViewPort().
-                ScrollUp().
-                TopText.Should.BeVisibleInViewPort();
-        }
+        using (StopwatchAsserter.WithinSeconds(1))
+            Assert.Throws<TimeoutException>(() =>
+                page.RefreshPageUntil(x => x.CurrentTime.Value > time.Value.Add(TimeSpan.FromSeconds(15)), timeout: 1));
     }
+
+    [Test]
+    public void WhenIgnoreInitAttributeOnProperty() =>
+        Assert.That(Go.To<BasicControlsPage>().IgnoredInput, Is.Null);
+
+    [Test]
+    public void GoBack() =>
+        Go.To<GoTo1Page>()
+            .GoTo2()
+                .GoBack<GoTo1Page>()
+                .Should.Exist();
+
+    [Test]
+    public void GoBack_WithInappropriateTOther()
+    {
+        var page2 = Go.To<GoTo1Page>()
+            .GoTo2();
+
+        Assert.Throws<AssertionException>(
+            () => page2.GoBack<GoTo3Page>());
+    }
+
+    [Test]
+    public void GoForward()
+    {
+        Go.To<GoTo1Page>()
+            .GoTo2()
+                .GoBack<GoTo1Page>()
+            .GoForward<GoTo2Page>()
+                .Should.Exist();
+    }
+
+    [Test]
+    public void GoForward_WithInappropriateTOther()
+    {
+        var page1 = Go.To<GoTo1Page>()
+            .GoTo2()
+                .GoBack<GoTo1Page>();
+
+        Assert.Throws<AssertionException>(
+            () => page1.GoForward<GoTo3Page>());
+    }
+
+    [Test]
+    public void Press() =>
+        Go.To<InputPage>()
+            .TextInput.Focus()
+            .Press("abc")
+            .Press("d")
+            .Press(Keys.Tab)
+            .Press("e")
+            .TextInput.Should.Equal("abcd");
+
+    [Test]
+    public void ActiveControl() =>
+        Go.To<InputPage>()
+            .TelInput.Set("123")
+            .ActiveControl.Attributes.Value.Should.Equal("123")
+            .IntTextInput.Focus()
+            .ActiveControl.Attributes.Id.Should.Equal("int-text-input");
+
+    [Test]
+    public void AggregateAssert()
+    {
+        var exception = Assert.Throws<AggregateAssertionException>(() =>
+            Go.To<StubPage>().
+                AggregateAssert(x => x
+                    .IsTrue.Should.AtOnce.BeFalse()
+                    .IsTrue.Should.AtOnce.BeTrue()
+                    .IsTrue.Should.AtOnce.BeFalse()));
+
+        Assert.That(exception.Results, Has.Count.EqualTo(2));
+    }
+
+    [Test]
+    public void AggregateAssert_WithComponentSelector()
+    {
+        var exception = Assert.Throws<AggregateAssertionException>(() =>
+           Go.To<StubPage>().
+               AggregateAssert(_ => _.IsTrue, x =>
+               {
+                   x.Should.AtOnce.BeFalse();
+                   x.Should.AtOnce.BeTrue();
+                   x.Should.AtOnce.BeFalse();
+               }));
+
+        Assert.That(exception.Results, Has.Count.EqualTo(2));
+    }
+
+    [Test]
+    public void PageSource() =>
+        Go.To<BasicControlsPage>()
+            .PageSource.Should.ContainAll("<head>", "</body>", "<input", "<button");
+
+    [Test]
+    public void ScrollDown() =>
+        Go.To<ScrollablePage>()
+            .BottomText.Should.Not.BeVisibleInViewPort()
+            .ScrollDown()
+            .BottomText.Should.BeVisibleInViewPort();
+
+    [Test]
+    public void ScrollUp() =>
+        Go.To<ScrollablePage>()
+            .Press(Keys.End)
+            .TopText.Should.Not.BeVisibleInViewPort()
+            .ScrollUp()
+            .TopText.Should.BeVisibleInViewPort();
 }
