41a3df8e7ee6c711b894df0f49d7d8a815209208
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-08-08 16:39:27 +0200

    Refactor `StopwatchAsserter` of Atata.IntegrationTests project

--- test/Atata.IntegrationTests/StopwatchAsserter.cs
+++ test/Atata.IntegrationTests/StopwatchAsserter.cs
@@ -1,34 +1,29 @@
-﻿using System;
-using System.Diagnostics;
-using NUnit.Framework;
+﻿using System.Diagnostics;
 
-namespace Atata.IntegrationTests
+namespace Atata.IntegrationTests;
+
+public sealed class StopwatchAsserter : IDisposable
 {
-    public sealed class StopwatchAsserter : IDisposable
+    private readonly TimeSpan _expectedTime;
+
+    private readonly TimeSpan _upperToleranceTime;
+
+    private readonly Stopwatch _watch;
+
+    public StopwatchAsserter(TimeSpan expectedTime, TimeSpan upperToleranceTime)
     {
-        private readonly TimeSpan _expectedTime;
+        _expectedTime = expectedTime;
+        _upperToleranceTime = upperToleranceTime;
 
-        private readonly TimeSpan _upperToleranceTime;
+        _watch = Stopwatch.StartNew();
+    }
 
-        private readonly Stopwatch _watch;
+    public static StopwatchAsserter WithinSeconds(double seconds, double upperToleranceSeconds = 1.5) =>
+        new(TimeSpan.FromSeconds(seconds), TimeSpan.FromSeconds(upperToleranceSeconds));
 
-        public StopwatchAsserter(TimeSpan expectedTime, TimeSpan upperToleranceTime)
-        {
-            _expectedTime = expectedTime;
-            _upperToleranceTime = upperToleranceTime;
-
-            _watch = Stopwatch.StartNew();
-        }
-
-        public static StopwatchAsserter WithinSeconds(double seconds, double upperToleranceSeconds = 1.5)
-        {
-            return new StopwatchAsserter(TimeSpan.FromSeconds(seconds), TimeSpan.FromSeconds(upperToleranceSeconds));
-        }
-
-        public void Dispose()
-        {
-            _watch.Stop();
-            Assert.That(_watch.Elapsed, Is.InRange(_expectedTime, _expectedTime + _upperToleranceTime));
-        }
+    public void Dispose()
+    {
+        _watch.Stop();
+        Assert.That(_watch.Elapsed, Is.InRange(_expectedTime, _expectedTime + _upperToleranceTime));
     }
 }
