283dcac7ea1daa3ff67b2f548d60feb53ba29a98
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-08-09 12:15:17 +0200

    Refactor `VerifyHeadingTests` of Atata.IntegrationTests project as `VerifyH1H6AttributeTests`

---
+++ test/Atata.IntegrationTests/Triggers/VerifyH1H6AttributeTests.cs
@@ -0,0 +1,24 @@
+﻿namespace Atata.IntegrationTests.Triggers;
+
+public class VerifyH1H6AttributeTests : UITestFixture
+{
+    [Test]
+    public void Execute()
+    {
+        Go.To<HeadingPage>();
+
+        string[] logMessages = LogEntries.Select(x => x.Message).ToArray();
+
+        string[] expectedLogMessageTexts =
+        {
+            "Heading 1_2",
+            "Heading 2.1.2.1.1.1",
+            "Heading 2.2",
+            "Assert: \"4th\" <h2> heading content should equal \"Heading 2.2\"",
+            "Assert: \"Heading_2/Heading 2\" <h1> heading should be present"
+        };
+
+        foreach (var text in expectedLogMessageTexts)
+            Assert.That(logMessages, Has.Some.Contains(text));
+    }
+}
