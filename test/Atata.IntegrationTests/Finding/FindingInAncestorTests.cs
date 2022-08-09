2fa95b3d014866b93caf29a457009b3f6768ab5b
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-08-09 16:12:37 +0200

    Refactor `FindingInAncestorTests` of Atata.IntegrationTests project

---
+++ test/Atata.IntegrationTests/Finding/FindingInAncestorTests.cs
@@ -0,0 +1,25 @@
+﻿namespace Atata.IntegrationTests.Finding;
+
+public class FindingInAncestorTests : UITestFixture
+{
+    private FindingInAncestorPage _page;
+
+    protected override void OnSetUp() =>
+        _page = Go.To<FindingInAncestorPage>();
+
+    [Test]
+    public void First_Visible() =>
+        _page.LegendInOneLayer.Should.Equal("Radio Buttons");
+
+    [Test]
+    public void ThreeLayers() =>
+        _page.LegendInThreeLayers.Should.Equal("Radio Buttons");
+
+    [Test]
+    public void ThreeLayers_AtParentAndDeclared() =>
+        _page.LegendInThreeLayersAtParentAndDeclared.Should.Equal("Radio Buttons");
+
+    [Test]
+    public void ThreeLayers_AtParentAndDeclaredAndComponent() =>
+        _page.LegendInThreeLayersAtParentAndDeclaredAndComponent.Should.Equal("Radio Buttons");
+}
