4b6b1159c39b9e4d4ef82b41710c03f976eb19ad
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-08-09 15:20:20 +0200

    Refactor `FindingWithSettingsTests` of Atata.IntegrationTests project as `FindingWithFindSettingsAttributeTests`

---
+++ test/Atata.IntegrationTests/Finding/FindingWithFindSettingsAttributeTests.cs
@@ -0,0 +1,25 @@
+﻿namespace Atata.IntegrationTests.Finding;
+
+public class FindingWithFindSettingsAttributeTests : UITestFixture
+{
+    private FindingWithSettingsPage _page;
+
+    protected override void OnSetUp() =>
+        _page = Go.To<FindingWithSettingsPage>();
+
+    [Test]
+    public void WhenAtPageObject() =>
+        _page
+            .OptionA.Should.Not.Exist()
+            .OptionB.Should.Exist()
+            .OptionC.Should.Exist()
+            .OptionD.Should.Not.Exist();
+
+    [Test]
+    public void WhenAtParentControl() =>
+        _page
+            .RadioSet.OptionA.Should.Not.Exist()
+            .RadioSet.OptionB.Should.Exist()
+            .RadioSet.OptionC.Should.Not.Exist()
+            .RadioSet.OptionD.Should.Exist();
+}
