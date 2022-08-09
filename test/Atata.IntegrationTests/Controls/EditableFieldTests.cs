f390e04212209ae135a2407dd95353405135c9e8
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-08-09 14:55:07 +0200

    Refactor `FormatTests` of Atata.IntegrationTests project as `EditableFieldTests`

---
+++ test/Atata.IntegrationTests/Controls/EditableFieldTests.cs
@@ -0,0 +1,79 @@
+﻿namespace Atata.IntegrationTests.Controls;
+
+public static class EditableFieldTests
+{
+    public class GetSet : UITestFixture
+    {
+        [Test]
+        public void WithValueGetFormatAttribute()
+        {
+            var sut = Go.To<InputPage>().TextInput;
+
+            sut.Metadata.Push(
+                new ValueGetFormatAttribute("INDICATOR {0}"));
+
+            sut.Set("INDICATOR abc");
+
+            sut.Should.AtOnce.Equal("abc");
+            sut.Attributes.Value.Should.AtOnce.Equal("INDICATOR abc");
+        }
+
+        [Test]
+        public void WithValueSetFormatAttribute()
+        {
+            var sut = Go.To<InputPage>().TextInput;
+
+            sut.Metadata.Push(
+                new ValueSetFormatAttribute("INDICATOR {0}"));
+
+            sut.Set("abc");
+
+            sut.Should.AtOnce.Equal("INDICATOR abc");
+            sut.Attributes.Value.Should.AtOnce.Equal("INDICATOR abc");
+        }
+
+        [Test]
+        public void WithValueGetFormatAttribute_AndValueSetFormatAttribute()
+        {
+            var sut = Go.To<InputPage>().TextInput;
+
+            sut.Metadata.Push(
+                new ValueSetFormatAttribute("SET {0}"),
+                new ValueGetFormatAttribute("SET INDICATOR {0}"));
+
+            sut.Set("INDICATOR abc");
+
+            sut.Should.AtOnce.Equal("abc");
+            sut.Attributes.Value.Should.AtOnce.Equal("SET INDICATOR abc");
+        }
+
+        [Test]
+        public void WithFormatAttribute_AndValueGetFormatAttribute_AndValueSetFormatAttribute()
+        {
+            var sut = Go.To<InputPage>().TextInputWithFormat;
+
+            sut.Metadata.Push(
+                new ValueSetFormatAttribute("SET {0}"),
+                new ValueGetFormatAttribute("SET INDICATOR {0}"));
+
+            sut.Set("INDICATOR abc");
+
+            sut.Should.AtOnce.Equal("abc");
+            sut.Attributes.Value.Should.AtOnce.Equal("SET INDICATOR abc");
+        }
+
+        [Test]
+        public void WithFormatAttribute_AndValueSetFormatAttribute()
+        {
+            var sut = Go.To<InputPage>().TextInputWithFormat;
+
+            sut.Metadata.Push(
+                new ValueSetFormatAttribute("!SET {0}!"));
+
+            sut.Set("abc");
+
+            sut.Should.AtOnce.Equal("SET abc");
+            sut.Attributes.Value.Should.AtOnce.Equal("!SET abc!");
+        }
+    }
+}
