be79ac0b4651c8ed20d26857fc480374fc667e26
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-08-09 12:57:09 +0200

    Refactor `VerifyMissingTests` of Atata.IntegrationTests project as `VerifyMissingAttributeTests`

---
+++ test/Atata.IntegrationTests/Triggers/VerifyMissingAttributeTests.cs
@@ -0,0 +1,21 @@
+﻿namespace Atata.IntegrationTests.Triggers;
+
+public class VerifyMissingAttributeTests : UITestFixture
+{
+    [Test]
+    public void Execute_WhenOnInit() =>
+        Go.To(new WaitingOnInitPage { OnInitWaitKind = WaitingOnInitPage.WaitKind.VerifyMissing })
+            .VerifyContentBlockIsLoaded();
+
+    [Test]
+    public void Execute_WhenOnInit_AfterGo() =>
+        Go.To(new WaitingPage { NavigatingPageWaitKind = WaitingOnInitPage.WaitKind.VerifyMissing })
+            .GoToWaitingOnInitPage.ClickAndGo()
+            .VerifyContentBlockIsLoaded();
+
+    [Test]
+    public void Execute_WhenOnDeInit() =>
+        Go.To(new WaitingPage { NavigatingWaitKind = WaitingPage.WaitKind.VerifyMissing })
+            .WaitAndGoToWaitingOnInitPage.ClickAndGo()
+            .PageUrl.Should.AtOnce.EndWith(WaitingOnInitPage.Url);
+}
