e799929b348dcb2e55d8fd8b50ea3aac767e5ce6
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-08-09 14:37:34 +0200

    Refactor `FrameSetTests` of Atata.IntegrationTests project as `FrameSetPageTests`

---
+++ test/Atata.IntegrationTests/FrameSetPageTests.cs
@@ -0,0 +1,24 @@
+﻿namespace Atata.IntegrationTests;
+
+public class FrameSetPageTests : UITestFixture
+{
+    private FrameSetPage _page;
+
+    protected override void OnSetUp() =>
+        _page = Go.To<FrameSetPage>();
+
+    [Test]
+    public void Interact() =>
+        _page
+            .Frame1.SwitchTo()
+                .Header.Should.Equal("Frame Inner 1")
+                .TextInput.Set("123")
+                .TextInput.Should.Equal("123")
+                .SwitchToRoot<FrameSetPage>()
+            .Frame2.DoWithin(x => x
+                .Header.Should.Equal("Frame Inner 2")
+                .Select.Should.Equal(1))
+            .Frame3.SwitchTo()
+                .Header.Should.Equal("Frame Inner 1")
+                .TextInput.Should.BeNullOrEmpty();
+}
