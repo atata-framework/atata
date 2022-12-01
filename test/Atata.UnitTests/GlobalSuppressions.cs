90bcb6b32e46e08c0218dde4ab03c2f4bb4da1bb
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-12-01 17:53:00 +0100

    #712 Add `ContainAny` verification extension methods; Update `DataVerificationProviderExtensionMethodTests` functionality and tests

--- test/Atata.UnitTests/GlobalSuppressions.cs
+++ test/Atata.UnitTests/GlobalSuppressions.cs
@@ -3,8 +3,6 @@
 #pragma warning disable S103 // Lines should not be too long
 
 [assembly: SuppressMessage("Major Code Smell", "S2743:Static fields should not be used in generic types", Justification = "<Pending>", Scope = "member", Target = "~F:Atata.UnitTests.DataProvision.DataVerificationProviderExtensionMethodTests.ExtensionMethodTestFixture`2.s_testSuiteData")]
-[assembly: SuppressMessage("Design", "CA1000:Do not declare static members on generic types", Justification = "<Pending>", Scope = "member", Target = "~M:Atata.UnitTests.DataProvision.DataVerificationProviderExtensionMethodTests.ExtensionMethodTestFixture`2.GetPassFunctionsTestCases(System.String)~System.Collections.Generic.IEnumerable{NUnit.Framework.TestCaseData}")]
-[assembly: SuppressMessage("Design", "CA1000:Do not declare static members on generic types", Justification = "<Pending>", Scope = "member", Target = "~M:Atata.UnitTests.DataProvision.DataVerificationProviderExtensionMethodTests.ExtensionMethodTestFixture`2.GetFailFunctionsTestCases(System.String)~System.Collections.Generic.IEnumerable{NUnit.Framework.TestCaseData}")]
 [assembly: SuppressMessage("Naming", "CA1720:Identifier contains type name", Justification = "<Pending>", Scope = "member", Target = "~P:Atata.UnitTests.DataProvision.EnumerableProviderTests.TestOwner.Object")]
 [assembly: SuppressMessage("Minor Code Smell", "S1125:Boolean literals should not be redundant", Justification = "<Pending>", Scope = "member", Target = "~M:Atata.UnitTests.Expressions.ObjectExpressionStringBuilderTests.GetExpressionTestCases~System.Collections.Generic.IEnumerable{NUnit.Framework.TestCaseData}")]
 [assembly: SuppressMessage("Performance", "CA1802:Use literals where appropriate", Justification = "<Pending>", Scope = "member", Target = "~F:Atata.UnitTests.Expressions.ImprovedExpressionStringBuilderTests.s_testFieldValue")]
@@ -14,5 +12,6 @@
 [assembly: SuppressMessage("Design", "CA1024:Use properties where appropriate", Justification = "<Pending>", Scope = "member", Target = "~M:Atata.UnitTests.Expressions.ImprovedExpressionStringBuilderTests.StaticClass.GetInt~System.Int32")]
 [assembly: SuppressMessage("Major Code Smell", "S2326:Unused type parameters should be removed", Justification = "<Pending>", Scope = "type", Target = "~T:Atata.UnitTests.Utils.TypeFinderTests.StaticSubClass.GenericSubClass`1")]
 [assembly: SuppressMessage("Design", "CA1000:Do not declare static members on generic types", Justification = "<Pending>", Scope = "member", Target = "~P:Atata.UnitTests.GenericCollectionAssertionsExtensions.ReferenceEqualityComparer`1.Default")]
+[assembly: SuppressMessage("Design", "CA1000:Do not declare static members on generic types", Justification = "<Pending>", Scope = "member", Target = "~M:Atata.UnitTests.DataProvision.DataVerificationProviderExtensionMethodTests.ExtensionMethodTestFixture`2.GetTestActions~System.Collections.Generic.IEnumerable{NUnit.Framework.TestCaseData}")]
 
 #pragma warning restore S103 // Lines should not be too long
