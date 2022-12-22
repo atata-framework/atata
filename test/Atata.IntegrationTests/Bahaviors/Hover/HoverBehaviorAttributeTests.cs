14e234ad450d5a65490f519c6657e26296b84ab1
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2022-12-22 11:37:15 +0100

    Add `HoverBehaviorAttributeTests`

---
+++ test/Atata.IntegrationTests/Bahaviors/Hover/HoverBehaviorAttributeTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+
+namespace Atata.IntegrationTests.Bahaviors;
+
+public class HoverBehaviorAttributeTests : UITestFixture
+{
+    private static IEnumerable<TestCaseData> Source =>
+        new[]
+        {
+            new TestCaseData(new HoversUsingActionsAttribute())
+        };
+
+    protected override bool ReuseDriver => false;
+
+    [TestCaseSource(nameof(Source))]
+    public void Execute(HoverBehaviorAttribute behavior)
+    {
+        static object GetPageYOffset() =>
+            AtataContext.Current.Driver.AsScriptExecutor().ExecuteScript("return window.pageYOffset;");
+
+        AtataContext.Current.Driver.Manage().Window.Size = new Size(400, 400);
+
+        ScrollablePage page = Go.To<ScrollablePage>();
+        Assume.That(GetPageYOffset(), Is.EqualTo(0));
+
+        var sut = page.BottomText;
+        sut.Metadata.Push(behavior);
+
+        sut.Hover();
+
+        Assert.That(GetPageYOffset(), Is.GreaterThan(200));
+    }
+}
