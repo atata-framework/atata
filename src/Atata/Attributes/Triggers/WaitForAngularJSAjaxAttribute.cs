1dc5ff5ccbc63e421b34ff8d53a47b9e02786263
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2023-01-09 14:30:17 +0100

    #732 Replace `WaitForAngularJSAjaxAttribute` with `WaitForAngularJSAttribute`

--- src/Atata/Attributes/Triggers/WaitForAngularJSAjaxAttribute.cs
+++ src/Atata/Attributes/Triggers/WaitForAngularJSAjaxAttribute.cs
@@ -1,35 +1,13 @@
-﻿namespace Atata
+﻿using System;
+
+namespace Atata
 {
-    /// <summary>
-    /// Indicates that the waiting should be performed until the AngularJS (v1.*) AJAX is completed.
-    /// By default occurs after the click.
-    /// </summary>
-    public class WaitForAngularJSAjaxAttribute : WaitForScriptAttribute
+    [Obsolete("Use " + nameof(WaitForAngularJSAttribute) + " instead.")] // Obsolete since v2.6.0.
+    public class WaitForAngularJSAjaxAttribute : WaitForAngularJSAttribute
     {
         public WaitForAngularJSAjaxAttribute(TriggerEvents on = TriggerEvents.AfterClick, TriggerPriority priority = TriggerPriority.Medium)
             : base(on, priority)
         {
         }
-
-        protected override string BuildScript<TOwner>(TriggerContext<TOwner> context)
-            => @"
-try {
-    if (document.readyState !== 'complete') {
-        return false;
-    }
-    if (window.angular) {
-        var injector = window.angular.element('body').injector();
-    
-        var $rootScope = injector.get('$rootScope');
-        var $http = injector.get('$http');
-
-        if ($rootScope.$$phase === '$apply' || $rootScope.$$phase === '$digest' || $http.pendingRequests.length !== 0) {
-            return false;
-        }
-    }
-    return true;
-} catch (err) {
-  return false;
-}";
     }
 }
---
+++ src/Atata/Attributes/Triggers/WaitForAngularJSAttribute.cs
@@ -0,0 +1,35 @@
+﻿namespace Atata
+{
+    /// <summary>
+    /// Indicates to wait until Angular (v1) has finished rendering and has no outstanding HTTP calls.
+    /// By default occurs after the click.
+    /// </summary>
+    public class WaitForAngularJSAttribute : WaitForScriptAttribute
+    {
+        public WaitForAngularJSAttribute(TriggerEvents on = TriggerEvents.AfterClick, TriggerPriority priority = TriggerPriority.Medium)
+            : base(on, priority)
+        {
+        }
+
+        protected override string BuildScript<TOwner>(TriggerContext<TOwner> context)
+            => @"
+try {
+    if (document.readyState !== 'complete') {
+        return false;
+    }
+    if (window.angular) {
+        var injector = window.angular.element('body').injector();
+    
+        var $rootScope = injector.get('$rootScope');
+        var $http = injector.get('$http');
+
+        if ($rootScope.$$phase === '$apply' || $rootScope.$$phase === '$digest' || $http.pendingRequests.length !== 0) {
+            return false;
+        }
+    }
+    return true;
+} catch (err) {
+  return false;
+}";
+    }
+}
