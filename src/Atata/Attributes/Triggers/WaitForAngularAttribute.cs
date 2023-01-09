56c649d353829cf20a59150ab8d6b0ce85dd87d1
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2023-01-09 14:08:35 +0100

    #731 Add `WaitForAngularAttribute` trigger attribute

---
+++ src/Atata/Attributes/Triggers/WaitForAngularAttribute.cs
@@ -0,0 +1,24 @@
+﻿namespace Atata
+{
+    /// <summary>
+    /// Indicates that the waiting should be performed until the Angular (v2+) AJAX is completed.
+    /// By default occurs after the click.
+    /// </summary>
+    public class WaitForAngularAttribute : TriggerAttribute
+    {
+        public WaitForAngularAttribute(TriggerEvents on = TriggerEvents.AfterClick, TriggerPriority priority = TriggerPriority.Medium)
+            : base(on, priority)
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets the Angular root selector.
+        /// The default value is taken from <see cref="AngularSettings.RootSelector"/>,
+        /// which is <c>"[ng-app]"</c> by default.
+        /// </summary>
+        public string RootSelector { get; set; } = AngularSettings.RootSelector;
+
+        protected internal override void Execute<TOwner>(TriggerContext<TOwner> context) =>
+            context.Component.Script.WaitForAngular(RootSelector);
+    }
+}
