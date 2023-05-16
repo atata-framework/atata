ea7f97356305e3a5bf5e082cc039732ad27b78ea
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2023-05-16 23:02:00 +0200

    Upgrade SonarAnalyzer.CSharp package to v9.0.0.68202 and fix code analysis warnings

--- test/Atata.IntegrationTests/GlobalSuppressions.cs
+++ test/Atata.IntegrationTests/GlobalSuppressions.cs
@@ -7,5 +7,6 @@
 [assembly: SuppressMessage("Major Code Smell", "S1144:Unused private types or members should be removed", Justification = "<Pending>", Scope = "member", Target = "~M:Atata.IntegrationTests.TriggersPage.WriteTriggerEventAttribute.Execute(Atata.TriggerContext{Atata.IntegrationTests.TriggersPage})")]
 [assembly: SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>", Scope = "member", Target = "~M:Atata.IntegrationTests.TriggersPage.WriteTriggerEventAttribute.Execute(Atata.TriggerContext{Atata.IntegrationTests.TriggersPage})")]
 [assembly: SuppressMessage("Naming", "CA1720:Identifier contains type name", Justification = "<Pending>", Scope = "member", Target = "~M:Atata.IntegrationTests.RandomizationTests.Int")]
+[assembly: SuppressMessage("Minor Code Smell", "S2094:Classes should not be empty", Justification = "<Pending>", Scope = "type", Target = "~T:Atata.IntegrationTests.TriggersPage.HierarchyControl.LevelItem1.LevelItem2.LevelItem3.LevelItem4")]
 
 #pragma warning restore S103 // Lines should not be too long
