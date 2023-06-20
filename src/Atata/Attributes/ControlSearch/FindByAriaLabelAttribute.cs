4dcb7f811f10713a08d9de0e76d588e0924f129f
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2023-06-20 17:53:52 +0200

    #751 Add `FindByAriaLabelAttribute`

---
+++ src/Atata/Attributes/ControlSearch/FindByAriaLabelAttribute.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Atata
+{
+    /// <summary>
+    /// Specifies that a control should be found by the <c>aria-label</c> attribute.
+    /// Finds the control that has the <c>aria-label</c> attribute matching the specified term(s).
+    /// Uses <see cref="TermCase.Sentence"/> as the default term case.
+    /// </summary>
+    public class FindByAriaLabelAttribute : TermFindAttribute
+    {
+        public FindByAriaLabelAttribute(TermCase termCase)
+            : base(termCase)
+        {
+        }
+
+        public FindByAriaLabelAttribute(TermMatch match, TermCase termCase)
+            : base(match, termCase)
+        {
+        }
+
+        public FindByAriaLabelAttribute(TermMatch match, params string[] values)
+            : base(match, values)
+        {
+        }
+
+        public FindByAriaLabelAttribute(params string[] values)
+            : base(values)
+        {
+        }
+
+        protected override TermCase DefaultCase => TermCase.Sentence;
+
+        protected override Type DefaultStrategy => typeof(FindByAttributeStrategy);
+
+        protected override IEnumerable<object> GetStrategyArguments()
+        {
+            yield return "aria-label";
+        }
+    }
+}
