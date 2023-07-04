00107b4b301d943d641a70fb88a7921c617ecd06
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2023-07-04 13:45:21 +0200

    #761 Add `FindByPrecedingSiblingContentAttribute`

---
+++ src/Atata/Attributes/ControlSearch/FindByPrecedingSiblingContentAttribute.cs
@@ -0,0 +1,43 @@
+﻿namespace Atata;
+
+/// <summary>
+/// Specifies that a control should be found by the content text of the preceding sibling.
+/// Uses <see cref="TermCase.Title"/> as the default term case.
+/// </summary>
+public class FindByPrecedingSiblingContentAttribute : TermFindAttribute
+{
+    public FindByPrecedingSiblingContentAttribute(TermCase termCase)
+        : base(termCase)
+    {
+    }
+
+    public FindByPrecedingSiblingContentAttribute(TermMatch match, TermCase termCase)
+        : base(match, termCase)
+    {
+    }
+
+    public FindByPrecedingSiblingContentAttribute(TermMatch match, params string[] values)
+        : base(match, values)
+    {
+    }
+
+    public FindByPrecedingSiblingContentAttribute(params string[] values)
+        : base(values)
+    {
+    }
+
+    /// <summary>
+    /// Gets or sets the sibling XPath.
+    /// The default value is <c>"*"</c>.
+    /// </summary>
+    public string SiblingXPath { get; set; } = "*";
+
+    protected override TermCase DefaultCase => TermCase.Title;
+
+    protected override Type DefaultStrategy => typeof(FindByRelativeElementContentStrategy);
+
+    protected override IEnumerable<object> GetStrategyArguments()
+    {
+        yield return $"preceding-sibling::{SiblingXPath}";
+    }
+}
---
+++ test/Atata.IntegrationTests/Finding/FindByPrecedingSiblingContentAttributeTests.cs
@@ -0,0 +1,22 @@
+﻿namespace Atata.IntegrationTests.Finding;
+
+public sealed class FindByPrecedingSiblingContentAttributeTests : UITestFixture
+{
+    [Test]
+    public void WithValue() =>
+        Go.To<FindingPage>()
+           .Find<TextInput<FindingPage>>(new FindByPrecedingSiblingContentAttribute("User Name"))
+               .DomProperties.Id.Should.Be("user-name");
+
+    [Test]
+    public void WithValueAndIndex() =>
+        Go.To<FindingPage>()
+           .Find<Label<FindingPage>>(new FindByPrecedingSiblingContentAttribute("Radio Buttons") { Index = 2 })
+               .Content.Should.Be("Option C");
+
+    [Test]
+    public void WithValueAndSiblingXPath() =>
+        Go.To<FindingPage>()
+           .Find<Label<FindingPage>>(new FindByPrecedingSiblingContentAttribute("Radio Buttons") { SiblingXPath = "legend[1]" })
+               .Content.Should.Be("Option A");
+}
