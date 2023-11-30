f82c63846c23b75b4903f9b41a146a226379a1fd
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2023-11-30 17:54:18 +0100

    Add `FakeWebDriver` and `UseFakeDriver` extension method to Atata.IntegrationTests project

---
+++ test/Atata.IntegrationTests/Extensions/AtataContextBuilderExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace Atata.IntegrationTests;
+
+internal static class AtataContextBuilderExtensions
+{
+    internal static CustomDriverAtataContextBuilder UseFakeDriver(this AtataContextBuilder builder) =>
+        builder.UseDriver(FakeWebDriver.Create());
+}
---
+++ test/Atata.IntegrationTests/FakeWebDriver.cs
@@ -0,0 +1,16 @@
+﻿namespace Atata.IntegrationTests;
+
+internal static class FakeWebDriver
+{
+    public static IWebDriver Create()
+    {
+        var driverMock = new Mock<IWebDriver>();
+        var driverManageMock = new Mock<IOptions>();
+        var driverTimeoutsMock = new Mock<ITimeouts>();
+
+        driverMock.Setup(x => x.Manage()).Returns(driverManageMock.Object);
+        driverManageMock.Setup(x => x.Timeouts()).Returns(driverTimeoutsMock.Object);
+
+        return driverMock.Object;
+    }
+}
