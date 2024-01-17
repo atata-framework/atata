0e71670fde1bf41f42e0ebfb8da09d75a341b9b2
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2024-01-17 17:20:32 +0100

    #813 Remove obsolete attributes

--- src/Atata/Attributes/Behaviors/Scroll/ScrollsUsingActionsAttribute.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Atata;
-
-[Obsolete("Use " + nameof(ScrollsUsingMoveToElementActionAttribute) + " or " + nameof(ScrollsUsingScrollToElementActionAttribute) + " instead.")] // Obsolete since v2.1.0.
-public class ScrollsUsingActionsAttribute : ScrollsUsingMoveToElementActionAttribute
-{
-}
