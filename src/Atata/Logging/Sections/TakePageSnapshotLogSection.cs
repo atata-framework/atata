4e269165f0c401ca63992446f073c33aeed83899
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2024-02-07 20:47:57 +0100

    #819 Change `TakePageSnapshotLogSection` log level from `Info` to `Trace`

--- src/Atata/Logging/Sections/TakePageSnapshotLogSection.cs
+++ src/Atata/Logging/Sections/TakePageSnapshotLogSection.cs
@@ -4,9 +4,10 @@ public class TakePageSnapshotLogSection : LogSection
 {
     public TakePageSnapshotLogSection(int snapshotNumber, string title = null)
     {
+        Level = LogLevel.Trace;
         Message = $"Take page snapshot #{snapshotNumber:D2}";
 
         if (!string.IsNullOrWhiteSpace(title))
-            Message += $" - {title}";
+            Message += $" {title}";
     }
 }
