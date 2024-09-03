b7702b7107d7968ebbafdeac492d55d08e79226a
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2024-09-03 12:04:02 +0200

    Restore `DriverAliases` as obsolete

---
+++ src/Atata/WebDriver/DriverAliases.cs
@@ -0,0 +1,17 @@
+﻿namespace Atata;
+
+[Obsolete("Use WebDriverAliases instead.")] // Obsolete since v4.0.0.
+public static class DriverAliases
+{
+    public const string Remote = "remote";
+
+    public const string Chrome = "chrome";
+
+    public const string Firefox = "firefox";
+
+    public const string InternetExplorer = "internetexplorer";
+
+    public const string Safari = "safari";
+
+    public const string Edge = "edge";
+}
