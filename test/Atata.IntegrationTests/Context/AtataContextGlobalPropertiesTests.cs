1b09545d699d7ee2250ed0751216c275b70c7225
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2024-09-21 15:35:11 +0200

    Rename `AtataContextArtifactsRootTests` to `AtataContextGlobalPropertiesTests.UseArtifactsRootPathTemplate`

---
+++ test/Atata.IntegrationTests/Context/AtataContextGlobalPropertiesTests.cs
@@ -0,0 +1,22 @@
+﻿namespace Atata.IntegrationTests.Context;
+
+public static class AtataContextGlobalPropertiesTests
+{
+    [Parallelizable(ParallelScope.None)]
+    public class UseArtifactsRootPathTemplate : SessionlessTestSuite
+    {
+        private readonly string _rootPath = $"{AppDomain.CurrentDomain.BaseDirectory}artifacts{Path.DirectorySeparatorChar}{Guid.NewGuid()}";
+
+        [OneTimeSetUp]
+        public void SetUpFixture() =>
+            AtataContext.GlobalProperties.UseArtifactsRootPathTemplate(_rootPath);
+
+        [OneTimeTearDown]
+        public void TearDownFixture() =>
+            AtataContext.GlobalProperties.UseArtifactsRootPathTemplate(AtataContextGlobalProperties.DefaultArtifactsRootPathTemplate);
+
+        [Test]
+        public void AtataContext_Artifacts() =>
+            AtataContext.Current.Artifacts.FullName.Should.StartWith(_rootPath + Path.DirectorySeparatorChar);
+    }
+}
