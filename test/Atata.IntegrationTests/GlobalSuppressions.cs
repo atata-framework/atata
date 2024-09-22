47e901097c0b806dafab0ecdf087f8eeeba29fb3
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2024-09-22 12:41:15 +0200

    Update GlobalSuppressions.cs files

--- test/Atata.IntegrationTests/GlobalSuppressions.cs
+++ test/Atata.IntegrationTests/GlobalSuppressions.cs
@@ -9,5 +9,6 @@
 [assembly: SuppressMessage("Naming", "CA1720:Identifier contains type name", Justification = "<Pending>", Scope = "member", Target = "~M:Atata.IntegrationTests.RandomizationTests.Int")]
 [assembly: SuppressMessage("Minor Code Smell", "S2094:Classes should not be empty", Justification = "<Pending>", Scope = "type", Target = "~T:Atata.IntegrationTests.TriggersPage.HierarchyControl.LevelItem1.LevelItem2.LevelItem3.LevelItem4")]
 [assembly: SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>", Scope = "member", Target = "~P:Atata.IntegrationTests.Verification.MixedVerificationTests.ThrowingSut.ThrowingProperty")]
+[assembly: SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1204:Static elements should appear before instance elements", Justification = "<Pending>", Scope = "member", Target = "~M:Atata.IntegrationTests.WebDriverSessionTestSuiteBase.SetAndVerifyValues``2(Atata.EditableField{``0,``1},``0[])")]
 
 #pragma warning restore S103 // Lines should not be too long
