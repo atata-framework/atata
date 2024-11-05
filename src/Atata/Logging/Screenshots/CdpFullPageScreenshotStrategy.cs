c539e375fda91399bf860f95c7dbfa9601cb4461
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2024-11-05 15:58:02 +0100

    #844 Upgrade Selenium.WebDriver package reference to v4.26.1

--- src/Atata/Logging/Screenshots/CdpFullPageScreenshotStrategy.cs
+++ src/Atata/Logging/Screenshots/CdpFullPageScreenshotStrategy.cs
@@ -1,4 +1,4 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using System.Text.Json.Nodes;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.DevTools;
 using OpenQA.Selenium.Edge;
@@ -23,7 +23,7 @@ public sealed class CdpFullPageScreenshotStrategy : IScreenshotStrategy
         var devTools = context.Driver.As<IDevTools>();
         var devToolsSession = devTools.GetDevToolsSession();
 
-        var commandParameters = new JObject
+        var commandParameters = new JsonObject
         {
             ["captureBeyondViewport"] = true
         };
