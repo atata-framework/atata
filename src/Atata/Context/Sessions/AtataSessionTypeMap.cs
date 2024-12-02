30f88792e1a59194b0eb27fcd5e194fbc4d869cd
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2024-12-02 10:20:55 +0100

    Add `AtataSessionTypeMap`

---
+++ src/Atata/Context/Sessions/AtataSessionTypeMap.cs
@@ -0,0 +1,23 @@
+﻿#nullable enable
+
+namespace Atata;
+
+internal static class AtataSessionTypeMap
+{
+    private static readonly ConcurrentDictionary<Type, Type?> s_builderSessionTypeMap = [];
+
+    internal static Type ResolveSessionTypeByBuilderType(Type builderType)
+    {
+        Type? sessionType = s_builderSessionTypeMap.GetOrAdd(builderType, DoResolveSessionTypeByBuilderType);
+
+        return sessionType
+            ?? throw new TypeNotFoundException($"{builderType} type doesn't inherit {typeof(AtataSessionBuilder<,>)} type.");
+    }
+
+    private static Type? DoResolveSessionTypeByBuilderType(Type builderType)
+    {
+        Type concreteBuilderType = builderType.GetBaseTypeOfRawGeneric(typeof(AtataSessionBuilder<,>));
+
+        return concreteBuilderType?.GetGenericArguments()[0];
+    }
+}
