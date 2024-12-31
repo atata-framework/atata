b350f7c06c1f97d866d5330bb3674e2486fe7a52
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2024-12-31 13:51:19 +0100

    Add `WebDriverSessionAtataSessionCollectionExtensions`

---
+++ src/Atata/WebDriver/WebDriverSessionAtataSessionCollectionExtensions.cs
@@ -0,0 +1,9 @@
+﻿#nullable enable
+
+namespace Atata;
+
+public static class WebDriverSessionAtataSessionCollectionExtensions
+{
+    public static WebDriverSessionBuilder AddWebDriver(this AtataSessionCollection collection, Action<WebDriverSessionBuilder>? configure = null) =>
+        collection.Add(configure);
+}
