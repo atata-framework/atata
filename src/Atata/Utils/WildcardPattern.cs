3ded64d37ef98518dec0a0f5d3346fb47313bf98
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-01-05 18:38:25 +0100

    Add `WildcardPattern`

---
+++ src/Atata/Utils/WildcardPattern.cs
@@ -0,0 +1,120 @@
+﻿#nullable enable
+
+namespace Atata;
+
+/// <summary>
+/// Represents a wildcard pattern.
+/// Wildcards: <c>?</c> matches exactly one occurrence of any character;
+/// <c>*</c> matches arbitrary many (including zero) occurrences of any character.
+/// To escape <c>?</c> or <c>*</c> prepend it with <c>\</c> character.
+/// </summary>
+public class WildcardPattern
+{
+    private readonly string _pattern;
+
+    private readonly StringComparison _stringComparison;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WildcardPattern"/> class.
+    /// </summary>
+    /// <param name="pattern">The pattern.</param>
+    public WildcardPattern(string pattern)
+        : this(pattern, StringComparison.Ordinal)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WildcardPattern"/> class.
+    /// </summary>
+    /// <param name="pattern">The pattern.</param>
+    /// <param name="stringComparison">The string comparison type.</param>
+    public WildcardPattern(string pattern, StringComparison stringComparison)
+    {
+        _pattern = pattern.CheckNotNull(nameof(pattern));
+        _stringComparison = stringComparison;
+    }
+
+    /// <inheritdoc cref="IsMatch(string)"/>
+    /// <param name="input">The text to search for a match.</param>
+    /// <param name="pattern">The pattern.</param>
+    public static bool IsMatch(string input, string pattern) =>
+        IsMatch(input.CheckNotNull(nameof(input)).AsSpan(), pattern, StringComparison.Ordinal);
+
+    /// <inheritdoc cref="IsMatch(string, string)"/>
+    public static bool IsMatch(ReadOnlySpan<char> input, string pattern) =>
+        IsMatch(input, pattern, StringComparison.Ordinal);
+
+    /// <inheritdoc cref="IsMatch(string)"/>
+    /// <param name="input">The text to search for a match.</param>
+    /// <param name="pattern">The pattern.</param>
+    /// <param name="stringComparison">The string comparison type.</param>
+    public static bool IsMatch(string input, string pattern, StringComparison stringComparison) =>
+        IsMatch(input.CheckNotNull(nameof(input)).AsSpan(), pattern, stringComparison);
+
+    /// <inheritdoc cref="IsMatch(string, string, StringComparison)"/>
+    public static bool IsMatch(ReadOnlySpan<char> input, string pattern, StringComparison stringComparison) =>
+        new WildcardPattern(pattern, stringComparison).IsMatch(input);
+
+    /// <summary>
+    /// Determines whether the specified <paramref name="input"/> matches the pattern.
+    /// </summary>
+    /// <param name="input">The text to search for a match.</param>
+    /// <returns>
+    /// <see langword="true"/> if the specified input matches the pattern; otherwise, <see langword="false"/>.
+    /// </returns>
+    public bool IsMatch(string input) =>
+        IsMatch(input.CheckNotNull(nameof(input)).AsSpan());
+
+    /// <inheritdoc cref="IsMatch(string)"/>
+    public bool IsMatch(ReadOnlySpan<char> input)
+    {
+        ReadOnlySpan<char> patternSpan = _pattern.AsSpan();
+
+        int inputIndex = 0;
+        int patternIndex = 0;
+        int patternLength = patternSpan.Length;
+        int patternStarIndex = -1;
+        int inputOnStarIndex = -1;
+
+        while (inputIndex < input.Length)
+        {
+            if (patternIndex + 1 < patternLength
+                && patternSpan[patternIndex] == '\\'
+                && patternSpan[patternIndex + 1] is '*' or '?'
+                && input[inputIndex] == patternSpan[patternIndex + 1])
+            {
+                inputIndex++;
+                patternIndex += 2;
+            }
+            else if (patternIndex < patternLength
+                && (patternSpan[patternIndex] == '?' || input.Slice(inputIndex, 1).Equals(patternSpan.Slice(patternIndex, 1), _stringComparison)))
+            {
+                inputIndex++;
+                patternIndex++;
+            }
+            else if (patternIndex < patternLength && patternSpan[patternIndex] == '*')
+            {
+                patternStarIndex = patternIndex;
+                inputOnStarIndex = inputIndex;
+                patternIndex++;
+            }
+            else if (patternStarIndex != -1)
+            {
+                patternIndex = patternStarIndex + 1;
+                inputOnStarIndex++;
+                inputIndex = inputOnStarIndex;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        while (patternIndex < patternLength && patternSpan[patternIndex] == '*')
+        {
+            patternIndex++;
+        }
+
+        return patternIndex == patternLength;
+    }
+}
---
+++ test/Atata.UnitTests/Utils/WildcardPatternTests.cs
@@ -0,0 +1,58 @@
+﻿namespace Atata.UnitTests.Utils;
+
+public static class WildcardPatternTests
+{
+    public sealed class IsMatch
+    {
+        [TestCase("", "", ExpectedResult = true)]
+        [TestCase("word", "", ExpectedResult = false)]
+        [TestCase("", "word", ExpectedResult = false)]
+        [TestCase("word", "*", ExpectedResult = true)]
+        [TestCase("word", "**", ExpectedResult = true)]
+        [TestCase("word", "*****", ExpectedResult = true)]
+        [TestCase("word", "", ExpectedResult = false)]
+        [TestCase("word", "word", ExpectedResult = true)]
+        [TestCase("word", "word*", ExpectedResult = true)]
+        [TestCase("word", "w*d", ExpectedResult = true)]
+        [TestCase("word", "w***d", ExpectedResult = true)]
+        [TestCase("word", "wor*", ExpectedResult = true)]
+        [TestCase("word", "*rd", ExpectedResult = true)]
+        [TestCase("word", "w*r", ExpectedResult = false)]
+        [TestCase("word*", "word*", ExpectedResult = true)]
+        [TestCase("word", "w?r?", ExpectedResult = true)]
+        [TestCase("word", "word?", ExpectedResult = false)]
+        [TestCase("wo*d", "w**d", ExpectedResult = true)]
+        [TestCase("word do", "wo*do", ExpectedResult = true)]
+        [TestCase("word do word do done", "wo*do*done", ExpectedResult = true)]
+
+        [TestCase("a*b?c", "a\\*b\\?c", ExpectedResult = true)]
+        [TestCase("aXb?c", "a\\*b\\?c", ExpectedResult = false)]
+        [TestCase("a*bXc", "a\\*b\\?c", ExpectedResult = false)]
+        public bool With(string input, string pattern) =>
+            WildcardPattern.IsMatch(input, pattern);
+
+        [Test]
+        public void WithNullInput()
+        {
+            Action action = () => WildcardPattern.IsMatch(null, "some");
+
+            action.Should().Throw<ArgumentNullException>().WithMessage("Value cannot be null. (Parameter 'input')");
+        }
+
+        [Test]
+        public void WithNullPattern()
+        {
+            Action action = () => WildcardPattern.IsMatch("some", null);
+
+            action.Should().Throw<ArgumentNullException>().WithMessage("Value cannot be null. (Parameter 'pattern')");
+        }
+    }
+
+    public sealed class IsMatch_WithStringComparison
+    {
+        [TestCase("word", "WORD", StringComparison.InvariantCulture, ExpectedResult = false)]
+        [TestCase("word", "WORD", StringComparison.InvariantCultureIgnoreCase, ExpectedResult = true)]
+        public bool With(string input, string pattern, StringComparison stringComparison) =>
+            WildcardPattern.IsMatch(input, pattern, stringComparison);
+    }
+}
