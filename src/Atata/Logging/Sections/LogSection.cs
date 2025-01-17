7b5f89c4f050c786278661daff8d48b9260fbdb6
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-01-17 19:37:56 +0100

    Add `#nullable enable` to `LogSection`

--- src/Atata/Logging/Sections/LogSection.cs
+++ src/Atata/Logging/Sections/LogSection.cs
@@ -1,22 +1,28 @@
-﻿namespace Atata;
+﻿#nullable enable
+
+namespace Atata;
 
 /// <summary>
 /// Represents the log section.
 /// </summary>
 public class LogSection
 {
-    private object _result;
+    private object? _result;
 
-    public LogSection(string message = null, LogLevel level = LogLevel.Info)
+    public LogSection(string message, LogLevel level = LogLevel.Info)
     {
         Message = message;
         Level = level;
     }
 
+    protected LogSection()
+    {
+    }
+
     /// <summary>
     /// Gets or sets the message.
     /// </summary>
-    public string Message { get; protected set; }
+    public string? Message { get; protected set; }
 
     /// <summary>
     /// Gets or sets the log level.
@@ -31,7 +37,7 @@ public class LogSection
     /// <summary>
     /// Gets the date/time of section start.
     /// </summary>
-    internal Stopwatch Stopwatch { get; } = new Stopwatch();
+    internal Stopwatch Stopwatch { get; } = new();
 
     /// <summary>
     /// Gets a value indicating whether the result is set.
@@ -41,7 +47,7 @@ public class LogSection
     /// <summary>
     /// Gets or sets the result.
     /// </summary>
-    public object Result
+    public object? Result
     {
         get => _result;
         set
@@ -52,9 +58,9 @@ public class LogSection
     }
 
     /// <summary>
-    /// Gets or sets the exception.
+    /// Gets the exception.
     /// </summary>
-    public Exception Exception { get; set; }
+    public Exception? Exception { get; internal set; }
 
     /// <summary>
     /// Gets the elapsed time of section execution.
