253d208fcf303914df5e24e4c627ccd5ca9a85a1
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-02-06 10:32:26 +0100

    Fix `Atata.IntegrationTests.NUnit.GlobalFixture`

--- test/Atata.IntegrationTests.NUnit/GlobalFixture.cs
+++ test/Atata.IntegrationTests.NUnit/GlobalFixture.cs
@@ -12,6 +12,6 @@ public sealed class GlobalFixture : AtataGlobalFixture
     protected override void ConfigureAtataContextGlobalProperties(AtataContextGlobalProperties globalProperties) =>
         globalProperties.UseRootNamespaceOf<GlobalFixture>();
 
-    protected override void ConfigureAtataContextBaseConfiguration(AtataContextBuilder builder)
+    protected override void ConfigureAtataContextBaseConfiguration(AtataContextBuilder builder) =>
         builder.LogConsumers.AddNLogFile();
 }
