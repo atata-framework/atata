57479a50dd1542786856c35dc04b5b9327c87d37
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-02-06 10:38:07 +0100

    Update `Atata.Benchmarks.Program`

--- test/Atata.Benchmarks/Program.cs
+++ test/Atata.Benchmarks/Program.cs
@@ -1,5 +1,18 @@
 ﻿Console.WriteLine(
-    """
-    Use:
-    BenchmarkRunner.Run<SomeBenchmarks>();
-    """);
+"""
+Use template:
+using Atata;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+BenchmarkRunner.Run<SomeBenchmarks>();
+
+[MemoryDiagnoser]
+public class SomeBenchmarks
+{
+    [Benchmark]
+    public void SomeBenchmark()
+    {
+    }
+}
+""");
