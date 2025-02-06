c7d37d06d3027787494e562d200bd8924156555c
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-02-06 10:30:05 +0100

    Add missing `ContextIdBasedArtifactsPathFactory`

---
+++ src/Atata/Context/Artifacts/ContextIdBasedArtifactsPathFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace Atata;
+
+public sealed class ContextIdBasedArtifactsPathFactory : IArtifactsPathFactory
+{
+    public string Create(AtataContext context) =>
+        Sanitize(context.Id);
+
+    private static string Sanitize(string value) =>
+        value.SanitizeForFileName('_');
+}
