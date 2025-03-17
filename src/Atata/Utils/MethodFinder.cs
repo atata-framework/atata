f6d2d428e67e449b7c9df72ff2b6923a48f9bca4
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-03-17 17:26:30 +0100

    Add `MethodFinder` static class with `FindAllWithAttribute` method

---
+++ src/Atata/Utils/MethodFinder.cs
@@ -0,0 +1,18 @@
+﻿#nullable enable
+
+namespace Atata;
+
+public static class MethodFinder
+{
+    public static IEnumerable<(MethodInfo Method, TAttribute Attribute)> FindAllWithAttribute<TAttribute>(Type type, BindingFlags bindingFlags)
+        where TAttribute : notnull, Attribute
+    {
+        MethodInfo[] methods = type.GetMethods(bindingFlags);
+
+        foreach (MethodInfo method in methods)
+        {
+            if (method.TryGetCustomAttribute(out TAttribute? attribute))
+                yield return (method, attribute);
+        }
+    }
+}
