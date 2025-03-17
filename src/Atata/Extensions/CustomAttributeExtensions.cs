0952c4a5151ee9100a398a6ca5aa9eebe515da5b
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-03-17 17:18:47 +0100

    Add `#nullable enable` to `CustomAttributeExtensions`

--- src/Atata/Extensions/CustomAttributeExtensions.cs
+++ src/Atata/Extensions/CustomAttributeExtensions.cs
@@ -1,15 +1,17 @@
-﻿namespace Atata;
+﻿#nullable enable
+
+namespace Atata;
 
 public static class CustomAttributeExtensions
 {
-    internal static T GetCustomAttribute<T>(this MemberInfo element, bool inherit = true)
-        where T : Attribute =>
+    internal static T? GetCustomAttribute<T>(this MemberInfo element, bool inherit = true)
+        where T : notnull, Attribute =>
         element.GetCustomAttributes(typeof(T), inherit).Cast<T>().FirstOrDefault();
 
-    public static bool TryGetCustomAttribute<T>(this MemberInfo element, out T attribute, bool inherit = true)
-        where T : Attribute
+    public static bool TryGetCustomAttribute<T>(this MemberInfo element, [NotNullWhen(true)] out T? attribute, bool inherit = true)
+        where T : notnull, Attribute
     {
         attribute = element.GetCustomAttribute<T>(inherit);
-        return attribute != null;
+        return attribute is not null;
     }
 }
