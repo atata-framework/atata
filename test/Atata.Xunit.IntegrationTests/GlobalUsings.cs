9bc7b2adebf777cb7c56bed7d1e7337dc3ace3b2
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-03-26 15:31:44 +0100

    Update GlobalUsings.cs of Atata.Xunit.IntegrationTests project

--- test/Atata.Xunit.IntegrationTests/GlobalUsings.cs
+++ test/Atata.Xunit.IntegrationTests/GlobalUsings.cs
@@ -1,5 +1,4 @@
-﻿global using System.Diagnostics.CodeAnalysis;
-global using FluentAssertions;
+﻿global using FluentAssertions;
 global using FluentAssertions.Execution;
 global using Xunit;
 global using Xunit.Abstractions;
