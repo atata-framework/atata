11622de9407753bc846da006b00f5142841530be
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-03-28 11:59:29 +0100

    Add `#nullable enable` to `AssertionResult`

--- src/Atata/Verification/AssertionResult.cs
+++ src/Atata/Verification/AssertionResult.cs
@@ -1,45 +1,40 @@
-﻿namespace Atata;
+﻿#nullable enable
+
+namespace Atata;
 
 /// <summary>
 /// Represents the result of assertion.
 /// </summary>
-public class AssertionResult
+public sealed class AssertionResult
 {
+    private AssertionResult(AssertionStatus status, string message, string? stackTrace = null)
+    {
+        Status = status;
+        Message = message;
+        StackTrace = stackTrace;
+    }
+
     /// <summary>
     /// Gets the status of assertion.
     /// </summary>
-    public AssertionStatus Status { get; private set; }
+    public AssertionStatus Status { get; }
 
     /// <summary>
     /// Gets the failure message.
     /// </summary>
-    public string Message { get; private set; }
+    public string Message { get; }
 
     /// <summary>
     /// Gets the stack trace of assertion.
     /// </summary>
-    public string StackTrace { get; private set; }
+    public string? StackTrace { get; }
 
     public static AssertionResult ForFailure(string message, string stackTrace) =>
-        new()
-        {
-            Status = AssertionStatus.Failed,
-            Message = message,
-            StackTrace = stackTrace
-        };
+        new(AssertionStatus.Failed, message, stackTrace);
 
     public static AssertionResult ForWarning(string message, string stackTrace) =>
-        new()
-        {
-            Status = AssertionStatus.Warning,
-            Message = message,
-            StackTrace = stackTrace
-        };
+        new(AssertionStatus.Warning, message, stackTrace);
 
     public static AssertionResult ForException(Exception exception) =>
-        new()
-        {
-            Status = AssertionStatus.Exception,
-            Message = exception.ToString()
-        };
+        new(AssertionStatus.Exception, exception.ToString());
 }
