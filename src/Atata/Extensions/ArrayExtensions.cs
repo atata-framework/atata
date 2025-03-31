eb5d37235e6379bdb08b0514b4b6dd3fbee74ada
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-03-31 16:27:43 +0200

    Add `#nullable enable` to `ArrayExtensions`

--- src/Atata/Extensions/ArrayExtensions.cs
+++ src/Atata/Extensions/ArrayExtensions.cs
@@ -1,8 +1,10 @@
-﻿namespace Atata;
+﻿#nullable enable
+
+namespace Atata;
 
 internal static class ArrayExtensions
 {
-    internal static TSource FirstOrDefault<TSource>(this TSource[] source, Predicate<TSource> predicate) =>
+    internal static TSource? FirstOrDefault<TSource>(this TSource[] source, Predicate<TSource> predicate) =>
         Array.Find(source, predicate);
 
     internal static bool Any<TSource>(this TSource[] source, Predicate<TSource> predicate) =>
