21fe4b4a83a6fecd57f22d0b1349f2a7d882fd2d
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-03-31 16:27:17 +0200

    Add `#nullable enable` to `UIComponentDefinitionAttribute`

--- src/Atata/Attributes/Definitions/UIComponentDefinitionAttribute.cs
+++ src/Atata/Attributes/Definitions/UIComponentDefinitionAttribute.cs
@@ -1,10 +1,14 @@
-﻿namespace Atata;
+﻿#nullable enable
+
+namespace Atata;
 
 /// <summary>
-/// Represents the base attribute class for UI component (page object, control) definition.
+/// A base attribute class for UI component (page object, control) definition.
 /// </summary>
 public abstract class UIComponentDefinitionAttribute : ScopeDefinitionAttribute
 {
+    private static readonly char[] s_ignoreNameEndingValueSeparator = [','];
+
     protected UIComponentDefinitionAttribute(string scopeXPath = DefaultScopeXPath)
         : base(scopeXPath)
     {
@@ -13,22 +17,25 @@ public abstract class UIComponentDefinitionAttribute : ScopeDefinitionAttribute
     /// <summary>
     /// Gets or sets the name of the component type.
     /// It is used in report log messages to describe the component type.
+    /// The default value is <see langword="null"/>,
+    /// meaning that the component type name will be resolved automatically.
     /// </summary>
-    public string ComponentTypeName { get; set; }
+    public string? ComponentTypeName { get; set; }
 
     /// <summary>
     /// Gets or sets the property name endings to ignore/truncate.
     /// Accepts a string containing a set of values separated by comma, for example <c>"Button,Link"</c>.
+    /// The default value is <see langword="null"/>.
     /// </summary>
-    public string IgnoreNameEndings { get; set; }
+    public string? IgnoreNameEndings { get; set; }
 
     /// <summary>
     /// Gets the values of property name endings to ignore.
     /// </summary>
     /// <returns>An array of name endings to ignore.</returns>
     public string[] GetIgnoreNameEndingValues() =>
-        IgnoreNameEndings != null
-            ? [.. IgnoreNameEndings.Split(',').Where(x => !string.IsNullOrWhiteSpace(x))]
+        IgnoreNameEndings is not null
+            ? IgnoreNameEndings.Split(s_ignoreNameEndingValueSeparator, StringSplitOptions.RemoveEmptyEntries)
             : [];
 
     /// <summary>
@@ -38,10 +45,10 @@ public abstract class UIComponentDefinitionAttribute : ScopeDefinitionAttribute
     /// <returns>Normalized name.</returns>
     public string NormalizeNameIgnoringEnding(string name)
     {
-        string endingToIgnore = GetIgnoreNameEndingValues()
+        string? endingToIgnore = GetIgnoreNameEndingValues()
             .FirstOrDefault(x => name.EndsWith(x, StringComparison.Ordinal) && name.Length > x.Length);
 
-        return endingToIgnore != null
+        return endingToIgnore is not null
             ? name[..^endingToIgnore.Length].TrimEnd()
             : name;
     }
