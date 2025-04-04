90ab8e1017f8b858998e7ca679d8bf21543809fa
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-04-04 09:52:09 +0200

    Add `#nullable enable` to extension classes

--- src/Atata/Extensions/EditableFieldExtensions.cs
+++ src/Atata/Extensions/EditableFieldExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace Atata;
+﻿#nullable enable
+
+namespace Atata;
 
 public static class EditableFieldExtensions
 {
@@ -48,18 +50,18 @@ public static class EditableFieldExtensions
     /// <see cref="RandomizeStringSettingsAttribute" />, <see cref="RandomizeNumberSettingsAttribute" />, <see cref="RandomizeIncludeAttribute" />, etc.
     /// Also executes <see cref="TriggerEvents.BeforeSet" /> and <see cref="TriggerEvents.AfterSet" /> triggers.
     /// </summary>
-    /// <typeparam name="TData">The type of the control's data.</typeparam>
+    /// <typeparam name="TValue">The type of the control's data.</typeparam>
     /// <typeparam name="TOwner">The type of the owner page object.</typeparam>
     /// <param name="field">The editable field control.</param>
     /// <param name="value">The generated value.</param>
     /// <returns>The instance of the owner page object.</returns>
-    public static TOwner SetRandom<TData, TOwner>(this EditableField<TData?, TOwner> field, out TData value)
-        where TData : struct
+    public static TOwner SetRandom<TValue, TOwner>(this EditableField<TValue?, TOwner> field, out TValue value)
+        where TValue : struct
         where TOwner : PageObject<TOwner>
     {
-        field.SetRandom(out TData? nullableValue);
+        field.SetRandom(out TValue? nullableValue);
 
-        value = (TData)nullableValue;
+        value = (TValue)nullableValue;
 
         return field.Owner;
     }
--- src/Atata/Extensions/IEnumerableExtensions.cs
+++ src/Atata/Extensions/IEnumerableExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace Atata;
+﻿#nullable enable
+
+namespace Atata;
 
 public static class IEnumerableExtensions
 {
