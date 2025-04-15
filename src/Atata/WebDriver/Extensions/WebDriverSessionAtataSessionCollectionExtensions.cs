0050192c15041b259d9fd8c8fc3a7ef2da1cc0c3
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-04-15 18:10:46 +0200

    Add XML comments to `WebDriverSessionAtataSessionCollectionExtensions`

--- src/Atata/WebDriver/Extensions/WebDriverSessionAtataSessionCollectionExtensions.cs
+++ src/Atata/WebDriver/Extensions/WebDriverSessionAtataSessionCollectionExtensions.cs
@@ -2,6 +2,12 @@
 
 public static class WebDriverSessionAtataSessionCollectionExtensions
 {
+    /// <summary>
+    /// Creates a new <see cref="WebDriverSessionBuilder"/> and adds it to the collection.
+    /// </summary>
+    /// <param name="collection">The session collection.</param>
+    /// <param name="configure">An action delegate to configure the <see cref="WebDriverSessionBuilder"/>.</param>
+    /// <returns>The created <see cref="WebDriverSessionBuilder"/> instance.</returns>
     public static WebDriverSessionBuilder AddWebDriver(this AtataSessionCollection collection, Action<WebDriverSessionBuilder>? configure = null) =>
         collection.Add(configure);
 }
