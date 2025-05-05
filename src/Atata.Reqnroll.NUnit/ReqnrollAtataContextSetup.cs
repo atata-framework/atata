f26bb6d06fe644d9bd750c0f52b079f049da5cc9
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-05-05 11:00:36 +0200

    Add XML comments to Atata.Reqnroll.NUnit `ReqnrollAtataContextSetup` class

--- src/Atata.Reqnroll.NUnit/ReqnrollAtataContextSetup.cs
+++ src/Atata.Reqnroll.NUnit/ReqnrollAtataContextSetup.cs
@@ -1,7 +1,15 @@
 ﻿namespace Atata.Reqnroll.NUnit;
 
+/// <summary>
+/// Provides setup and tear-down methods for <see cref="AtataContext"/> in the context of Reqnroll.NUnit features and scenarios.
+/// </summary>
 public static class ReqnrollAtataContextSetup
 {
+    /// <summary>
+    /// Sets up the <see cref="AtataContext"/> for the feature.
+    /// </summary>
+    /// <param name="featureContext">The context of the feature.</param>
+    /// <param name="configure">An action delegate to configure the feature <see cref="AtataContextBuilder"/>.</param>
     public static void SetUpFeature(
         FeatureContext featureContext,
         Action<AtataContextBuilder, FeatureContext>? configure = null)
@@ -17,12 +25,22 @@ public static class ReqnrollAtataContextSetup
         featureContext.Set(featureAtataContext);
     }
 
+    /// <summary>
+    /// Tears down the <see cref="AtataContext"/> for the feature.
+    /// </summary>
+    /// <param name="featureContext">The context of the feature.</param>
     public static void TearDownFeature(FeatureContext featureContext)
     {
         if (featureContext.TryGetValue(out AtataContext featureAtataContext))
             NUnitAtataContextCompletionHandler.Complete(featureAtataContext);
     }
 
+    /// <summary>
+    /// Sets up the <see cref="AtataContext"/> for the scenario.
+    /// </summary>
+    /// <param name="featureContext">The context of the feature.</param>
+    /// <param name="scenarioContext">The context of the scenario.</param>
+    /// <param name="configure">An action delegate to configure the scenario <see cref="AtataContextBuilder"/>.</param>
     public static void SetUpScenario(
         FeatureContext featureContext,
         ScenarioContext scenarioContext,
@@ -37,6 +55,10 @@ public static class ReqnrollAtataContextSetup
         scenarioContext.Set(scenarioAtataContext);
     }
 
+    /// <summary>
+    /// Tears down the <see cref="AtataContext"/> for the scenario.
+    /// </summary>
+    /// <param name="scenarioContext">The context of the scenario.</param>
     public static void TearDownScenario(ScenarioContext scenarioContext)
     {
         if (scenarioContext.TryGetValue(out AtataContext atataContext))
