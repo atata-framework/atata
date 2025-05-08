451b1ed167cfea17b206828f4570a9d94f3fa515
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-05-08 14:14:50 +0200

    Add extensions methods to `ContainerSessionAtataSessionsBuilderExtensions`

--- src/Atata.Testcontainers/Extensions/ContainerSessionAtataSessionsBuilderExtensions.cs
+++ src/Atata.Testcontainers/Extensions/ContainerSessionAtataSessionsBuilderExtensions.cs
@@ -13,7 +13,24 @@ public static class ContainerSessionAtataSessionsBuilderExtensions
     /// <param name="builder">The sessions builder.</param>
     /// <param name="configure">An action delegate to configure the provided <see cref="ContainerSessionBuilder"/>.</param>
     /// <returns>The <see cref="AtataContextBuilder"/> instance.</returns>
-    public static AtataContextBuilder AddContainer(this AtataSessionsBuilder builder, Action<ContainerSessionBuilder>? configure = null) =>
+    public static AtataContextBuilder AddContainer(
+        this AtataSessionsBuilder builder,
+        Action<ContainerSessionBuilder>? configure = null)
+        =>
+        builder.Add(configure);
+
+    /// <summary>
+    /// Adds a new instance of <see cref="ContainerSessionBuilder{TContainer}"/> builder.
+    /// </summary>
+    /// <typeparam name="TContainer">The type of the container.</typeparam>
+    /// <param name="builder">The sessions builder.</param>
+    /// <param name="configure">An action delegate to configure the provided <see cref="ContainerSessionBuilder{TContainer}"/>.</param>
+    /// <returns>The <see cref="AtataContextBuilder"/> instance.</returns>
+    public static AtataContextBuilder AddContainer<TContainer>(
+        this AtataSessionsBuilder builder,
+        Action<ContainerSessionBuilder<TContainer>>? configure = null)
+        where TContainer : IContainer
+        =>
         builder.Add(configure);
 
     /// <summary>
@@ -23,7 +40,27 @@ public static class ContainerSessionAtataSessionsBuilderExtensions
     /// <param name="configure">An action delegate to configure the provided <see cref="ContainerSessionBuilder"/>.</param>
     /// <param name="mode">The configuration mode, which is <see cref="ConfigurationMode.ConfigureOrThrow"/> by default.</param>
     /// <returns>The <see cref="AtataContextBuilder"/> instance.</returns>
-    public static AtataContextBuilder ConfigureContainer(this AtataSessionsBuilder builder, Action<ContainerSessionBuilder> configure, ConfigurationMode mode = default) =>
+    public static AtataContextBuilder ConfigureContainer(
+        this AtataSessionsBuilder builder,
+        Action<ContainerSessionBuilder> configure,
+        ConfigurationMode mode = default)
+        =>
+        builder.Configure(configure, mode);
+
+    /// <summary>
+    /// Configures existing nameless <see cref="ContainerSessionBuilder{TContainer}"/> session builder.
+    /// </summary>
+    /// <typeparam name="TContainer">The type of the container.</typeparam>
+    /// <param name="builder">The sessions builder.</param>
+    /// <param name="configure">An action delegate to configure the provided <see cref="ContainerSessionBuilder{TContainer}"/>.</param>
+    /// <param name="mode">The configuration mode, which is <see cref="ConfigurationMode.ConfigureOrThrow"/> by default.</param>
+    /// <returns>The <see cref="AtataContextBuilder"/> instance.</returns>
+    public static AtataContextBuilder ConfigureContainer<TContainer>(
+        this AtataSessionsBuilder builder,
+        Action<ContainerSessionBuilder<TContainer>> configure,
+        ConfigurationMode mode = default)
+        where TContainer : IContainer
+        =>
         builder.Configure(configure, mode);
 
     /// <summary>
@@ -34,6 +71,29 @@ public static class ContainerSessionAtataSessionsBuilderExtensions
     /// <param name="configure">An action delegate to configure the provided <see cref="ContainerSessionBuilder"/>.</param>
     /// <param name="mode">The configuration mode, which is <see cref="ConfigurationMode.ConfigureOrThrow"/> by default.</param>
     /// <returns>The <see cref="AtataContextBuilder"/> instance.</returns>
-    public static AtataContextBuilder ConfigureContainer(this AtataSessionsBuilder builder, string? name, Action<ContainerSessionBuilder> configure, ConfigurationMode mode = default) =>
+    public static AtataContextBuilder ConfigureContainer(
+        this AtataSessionsBuilder builder,
+        string? name,
+        Action<ContainerSessionBuilder> configure,
+        ConfigurationMode mode = default)
+        =>
+        builder.Configure(name, configure, mode);
+
+    /// <summary>
+    /// Configures existing <see cref="ContainerSessionBuilder{TContainer}"/> session builder that has the specified <paramref name="name"/>.
+    /// </summary>
+    /// <typeparam name="TContainer">The type of the container.</typeparam>
+    /// <param name="builder">The sessions builder.</param>
+    /// <param name="name">The session name.</param>
+    /// <param name="configure">An action delegate to configure the provided <see cref="ContainerSessionBuilder{TContainer}"/>.</param>
+    /// <param name="mode">The configuration mode, which is <see cref="ConfigurationMode.ConfigureOrThrow"/> by default.</param>
+    /// <returns>The <see cref="AtataContextBuilder"/> instance.</returns>
+    public static AtataContextBuilder ConfigureContainer<TContainer>(
+        this AtataSessionsBuilder builder,
+        string? name,
+        Action<ContainerSessionBuilder<TContainer>> configure,
+        ConfigurationMode mode = default)
+        where TContainer : IContainer
+        =>
         builder.Configure(name, configure, mode);
 }
--- test/Atata.Testcontainers.IntegrationTests/ContainerSessionBuilderTests.cs
+++ test/Atata.Testcontainers.IntegrationTests/ContainerSessionBuilderTests.cs
@@ -4,7 +4,7 @@
 public sealed class ContainerSessionBuilderTests : AtataTestSuite
 {
     [Test]
-    public async Task NonGenericContainerStarts()
+    public async Task NonGenericContainerStarts_AddToAtataSessionCollection()
     {
         // Act
         var containerSession = await Context.Sessions.AddContainer()
@@ -18,13 +18,46 @@ public sealed class ContainerSessionBuilderTests : AtataTestSuite
     }
 
     [Test]
-    public async Task GenericContainerStarts()
+    public async Task NonGenericContainerStarts_AddToAtataSessionsBuilder()
+    {
+        // Act
+        await using AtataContext context = await AtataContext.CreateDefaultNonScopedBuilder()
+            .Sessions.AddContainer(x => x
+                .Use(x => x.WithImage("hello-world:latest")))
+            .BuildAsync();
+        var containerSession = context.Sessions.Get<ContainerSession>();
+
+        // Assert
+        containerSession.Container.ToResultSubject()
+            .Should.Not.BeNull()
+            .ValueOf(x => x.Name).Should.Not.BeNullOrEmpty();
+    }
+
+    [Test]
+    public async Task GenericContainerStarts_AddToAtataSessionCollection()
     {
         // Act
         var containerSession = await Context.Sessions.AddContainer<WebDriverContainer>()
             .Use(() => new WebDriverBuilder()
                 .WithBrowser(new WebDriverBrowser("selenium/standalone-chrome:latest")))
             .BuildAsync();
+
+        // Assert
+        containerSession.Container.ToResultSubject()
+            .Should.Not.BeNull()
+            .ValueOf(x => x.Name).Should.Not.BeNullOrEmpty();
+    }
+
+    [Test]
+    public async Task GenericContainerStarts_AddToAtataSessionsBuilder()
+    {
+        // Act
+        await using AtataContext context = await AtataContext.CreateDefaultNonScopedBuilder()
+            .Sessions.AddContainer<WebDriverContainer>(x => x
+                .Use(() => new WebDriverBuilder()
+                    .WithBrowser(new WebDriverBrowser("selenium/standalone-chrome:latest"))))
+            .BuildAsync();
+        var containerSession = context.Sessions.Get<ContainerSession<WebDriverContainer>>();
 
         // Assert
         containerSession.Container.ToResultSubject()
