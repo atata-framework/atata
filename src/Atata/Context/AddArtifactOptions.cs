c6f13c26c36df4d65b02e5a3caacba1559f71804
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-05-20 15:53:36 +0200

    Update XML comments of `AddArtifactOptions`

--- src/Atata/Context/AddArtifactOptions.cs
+++ src/Atata/Context/AddArtifactOptions.cs
@@ -1,25 +1,27 @@
 ﻿namespace Atata;
 
+#pragma warning disable SA1623 // Property summary documentation should match accessors
+
 /// <summary>
 /// Represents the options for adding an artifact.
 /// </summary>
 public readonly struct AddArtifactOptions
 {
     /// <summary>
-    /// Gets the type of the artifact.
+    /// Gets or sets the type of the artifact.
     /// The default value is <see langword="null"/>.
     /// The value can be present in <see cref="ArtifactTypes"/>, be a custom one, or just be <see langword="null"/>.
     /// </summary>
     public string? ArtifactType { get; init; }
 
     /// <summary>
-    /// Gets the artifact title.
+    /// Gets or sets the artifact title.
     /// The default value is <see langword="null"/>.
     /// </summary>
     public string? ArtifactTitle { get; init; }
 
     /// <summary>
-    /// Gets a value indicating whether to prepend artifact number to file name in a form of "001-{file name}".
+    /// Gets or sets a value indicating whether to prepend artifact number to file name in a form of "001-{file name}".
     /// The default value is <see langword="false"/>.
     /// </summary>
     public bool PrependArtifactNumberToFileName { get; init; }
