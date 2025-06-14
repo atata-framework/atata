d2b3860891197b1477d4b17e07390a00e129fe75
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-06-14 10:11:05 +0200

    #856 Add `ClickWithHold(double pressSeconds)` method to `Control<TOwner>`

---
+++ src/Atata/Logging/Sections/ClickWithHoldLogSection.cs
@@ -0,0 +1,8 @@
+﻿namespace Atata;
+
+public class ClickWithHoldLogSection : UIComponentLogSection
+{
+    public ClickWithHoldLogSection(UIComponent component, TimeSpan interval)
+        : base(component) =>
+        Message = $"Click {component.ComponentFullName} with {interval.ToShortIntervalString()} hold";
+}
