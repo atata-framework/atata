1aa30496d5ee6a3d1e72e87f811f31249744e702
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-11-14 09:37:49 +0100

    In `StartSessionAndShareAttribute` add constructor with `string sessionName` parameter only

--- src/Atata/Attributes/Context/StartSessionAndShareAttribute.cs
+++ src/Atata/Attributes/Context/StartSessionAndShareAttribute.cs
@@ -9,6 +9,17 @@ public class StartSessionAndShareAttribute : TestSuiteAtataContextConfigurationA
     /// <summary>
     /// Initializes a new instance of the <see cref="StartSessionAndShareAttribute"/> class.
     /// </summary>
+    /// <param name="sessionName">Name of the session.</param>
+    public StartSessionAndShareAttribute(string sessionName)
+    {
+        Guard.ThrowIfNull(sessionName);
+
+        SessionName = sessionName;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="StartSessionAndShareAttribute"/> class.
+    /// </summary>
     /// <param name="sessionType">Type of the session.</param>
     /// <param name="sessionName">Name of the session. Can be <see langword="null"/>.</param>
     public StartSessionAndShareAttribute(Type sessionType, string? sessionName = null)
@@ -22,7 +33,7 @@ public class StartSessionAndShareAttribute : TestSuiteAtataContextConfigurationA
     /// <summary>
     /// Gets the type of the session.
     /// </summary>
-    public Type SessionType { get; }
+    public Type? SessionType { get; }
 
     /// <summary>
     /// Gets the name of the session.
@@ -37,7 +48,7 @@ public class StartSessionAndShareAttribute : TestSuiteAtataContextConfigurationA
 
     protected internal override void ConfigureAtataContext(AtataContextBuilder builder, object? testSuite)
     {
-        builder.Sessions.DisableAllBySessionType(SessionType, SessionName);
+        builder.Sessions.DisableAll(SessionType, SessionName);
 
         builder.Sessions.Configure(
             SessionType,
@@ -52,7 +63,7 @@ public class StartSessionAndShareAttribute : TestSuiteAtataContextConfigurationA
 
     protected internal override void ConfigureTestAtataContext(AtataContextBuilder builder, object? testSuite)
     {
-        builder.Sessions.DisableAllBySessionType(SessionType, SessionName);
+        builder.Sessions.DisableAll(SessionType, SessionName);
 
         builder.Sessions.Borrow(
             SessionType,
