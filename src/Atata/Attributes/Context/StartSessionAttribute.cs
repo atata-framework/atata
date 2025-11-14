272f07479744303c7e2d868aac92a319056d5a8e
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2025-11-14 09:38:13 +0100

    In `StartSessionAttribute` add constructor with `string sessionName` parameter only

--- src/Atata/Attributes/Context/StartSessionAttribute.cs
+++ src/Atata/Attributes/Context/StartSessionAttribute.cs
@@ -9,6 +9,17 @@ public class StartSessionAttribute : AtataContextConfigurationAttribute
     /// <summary>
     /// Initializes a new instance of the <see cref="StartSessionAttribute"/> class.
     /// </summary>
+    /// <param name="sessionName">Name of the session.</param>
+    public StartSessionAttribute(string sessionName)
+    {
+        Guard.ThrowIfNull(sessionName);
+
+        SessionName = sessionName;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="StartSessionAttribute"/> class.
+    /// </summary>
     /// <param name="sessionType">Type of the session.</param>
     /// <param name="sessionName">Name of the session. Can be <see langword="null"/>.</param>
     public StartSessionAttribute(Type sessionType, string? sessionName = null)
@@ -22,7 +33,7 @@ public class StartSessionAttribute : AtataContextConfigurationAttribute
     /// <summary>
     /// Gets the type of the session.
     /// </summary>
-    public Type SessionType { get; }
+    public Type? SessionType { get; }
 
     /// <summary>
     /// Gets the name of the session.
@@ -37,7 +48,7 @@ public class StartSessionAttribute : AtataContextConfigurationAttribute
 
     protected internal override void ConfigureAtataContext(AtataContextBuilder builder, object? testSuite)
     {
-        builder.Sessions.DisableAllBySessionType(SessionType, SessionName);
+        builder.Sessions.DisableAll(SessionType, SessionName);
 
         builder.Sessions.Configure(
             SessionType,
