6ac6481157602ec4bd8f7a667fea08ef099b4173
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2026-06-12 13:53:42 +0200

    Ignore test `Atata.IntegrationTests.Controls.FrameWrappedContentEditorTests.Interact`

--- test/Atata.IntegrationTests/Controls/FrameWrappedContentEditorTests.cs
+++ test/Atata.IntegrationTests/Controls/FrameWrappedContentEditorTests.cs
@@ -3,6 +3,7 @@
 public class FrameWrappedContentEditorTests : WebDriverSessionTestSuite
 {
     [Test]
+    [Ignore("Started to fail on 2026-06-12 due to the latest Chrome.")] // TODO: Investigate and fix the issue.
     public void Interact()
     {
         var sut = Go.To<TinyMcePage>().EditorAsFrameWrappedContentEditor;
