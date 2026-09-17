198ac11693dc3b1acde74246b1b2af11338b5f22
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2026-09-17 17:37:53 +0200

    In Atata.TestApp project remove GlobalSuppressions.cs file

--- test/Atata.TestApp/GlobalSuppressions.cs
+++
@@ -1,3 +0,0 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Reliability", "CA2000:Dispose objects before losing scope", Justification = "<Pending>", Scope = "member", Target = "~M:Atata.TestApp.Program.Main(System.String[])")]
